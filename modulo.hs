baae9292d5b72d4b9f4d20e78c896f04acd35170
Jonathan Zingale <jonzingale@gmail.com>
2016-02-26 19:44:13 -0700

    filled out modulo class

--- modulo.hs
+++ modulo.hs
@@ -1,21 +1,30 @@
+{-# OPTIONS_GHC -fno-warn-tabs #-}
+
 module Modulo where
--- :set -XFlexibleContexts
 
-data I_I m = I_I { modulus :: m, value :: m } | BAD deriving (Show)
+data Mod m = Mod { modulus :: m, value :: m } | BAD deriving (Show)
 
-incl :: (Integral a) => a -> a -> I_I a
-incl n a = I_I n a
+incl :: (Integral a) => a -> a -> Mod a
+incl n a = Mod n a
 
-rep :: (Integral a) => I_I a -> a
-rep (I_I n a) = mod a n
+rep :: (Integral a) => Mod a -> a
+rep (Mod n a) = mod a n
 
-instance (Eq a) => Eq (I_I a) where
-	I_I a b == I_I c d | a == c && b == d = True
+instance (Eq a) => Eq (Mod a) where
+	Mod a b == Mod c d | a == c && b == d = True
 										 | otherwise = False
 
-instance (Num a, Integral a) => Num (I_I a) where
-	I_I a b + I_I c d  | a == c = I_I a $ (b + d) `mod` a
+instance (Num a, Integral a) => Num (Mod a) where
+	Mod a b + Mod c d  | a == c = Mod a $ (b + d) `mod` a
 										 | otherwise = BAD
 
-this = I_I 7 5
-that = I_I 3 11
+	Mod a b * Mod c d | a == c = Mod a $ (b * d) `mod` a
+										| otherwise = BAD
+
+	negate (Mod a b) = Mod a $ (negate b) `mod` a
+	fromInteger a = incl (fromIntegral a) 0
+	abs (Mod a b) = Mod a $ b `mod` a
+	signum (Mod a b) = 1
+
+this = Mod 7 5
+that = Mod 3 11
