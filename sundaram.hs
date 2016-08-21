9fe2dce5f5318af3f5619473c84b941dcf18ba4e
Jon Zingale <jon@jackrabbitsystems.com>
2016-08-20 22:41:47 -0600

    some ideas toward sieving

--- sundaram.hs
+++ sundaram.hs
@@ -21,23 +21,20 @@
 
 import Data.List
 -- :set +s
-odds :: [Integer]
-odds = [ 2*k + 1 | k<-[1..]]
-
-nums = [1..]
-
-limit lim = (div lim 2) - 2
-j_limit lim = div (((limit lim) - 1)) 3
-
-composite_cores :: Integer -> [Integer]
-composite_cores lim = [i+j+2*i*j | j<-[1..j_limit lim], i<-[1..j]]
+--nums = [1..]
+--odds = [ 2*k + 1 | k<-[1..]]
 
 oddify x = 2*x + 1
 
-sundaram lim =  takeWhile (<=  lim) $
-                odds \\ ((map oddify).composite_cores) lim
+limit lim = (div lim 2) - 2
+j_limit lim = div (limit lim - 1) 3
 
-sundaram2 lim = map oddify $ [1..limit lim] \\ (composite_cores lim)
+-- fast but not that fast. try recusion
+composite_cores :: Int -> [Int]
+composite_cores lim = [i+j+2*i*j | j<-[1..j_limit lim], i<-[1..j]]
+
+-- sorting composite_cores is just a bit faster!!
+sundaram lim = map oddify $ [1..limit lim] \\ composite_cores lim
 
 main = putStrLn $ show $ sundaram (10^3)
 
