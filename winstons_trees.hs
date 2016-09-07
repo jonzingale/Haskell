46387aff50acb2fada0c3f1a313d3e4a55d729ec
Jon Zingale <jon@jackrabbitsystems.com>
2016-09-07 00:14:06 -0600

    a variation on winstons splits method

--- winstons_trees.hs
+++ winstons_trees.hs
@@ -10,13 +10,17 @@ trees [x] = [Leaf x]
 trees xs = do
     (branchL, branchR) <- splits xs
     Fork <$> (trees branchL) <*> (trees branchR)
- 
+
 splits xs = filter noEmpty $ map aux (powerset xs)
     where aux = \ set -> (set, xs \\ set)
-          noEmpty = \(p, p') -> p /= [] && p' /= []
- 
+          noEmpty = \ (p, p') -> p /= [] && p' /= []
  
 powerset ::[a] -> [[a]]
 powerset [] = [[]]
 powerset (x:xs) = (map (x:) xs') ++ xs' 
     where xs' = powerset xs
+
+splits' (x:xs) = let first = map (x:) (powerset xs) in
+			  	 let second = (reverse.powerset) xs in
+			  	 let comb = \ x -> (tail.zip x) in
+			  	 comb first second ++ comb second first
