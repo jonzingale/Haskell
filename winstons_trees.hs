d8880796c23ff9afff4182b3c6ddbd9173f458f2
Jon Zingale <jon@jackrabbitsystems.com>
2016-09-07 14:18:41 -0600

    more play

--- winstons_trees.hs
+++ winstons_trees.hs
@@ -17,10 +17,15 @@ splits xs = filter noEmpty $ map aux (powerset xs)
  
 powerset ::[a] -> [[a]]
 powerset [] = [[]]
-powerset (x:xs) = (map (x:) xs') ++ xs' 
+powerset (x:xs) = xs' ++ map (x:) xs'
     where xs' = powerset xs
 
-splits' (x:xs) = let first = map (x:) (powerset xs) in
-			  	 let second = (reverse.powerset) xs in
-			  	 let comb = \ x -> (tail.zip x) in
-			  	 comb first second ++ comb second first
+splits' (x:xs) = let comb = \ x -> (tail.zip x) in
+			  	 			 let first = (reverse.map (x:)) (powerset xs) in
+			  	 			 comb first (powerset xs) ++ comb (powerset xs) first
+
+-- cleaner but slower.
+--splits' xs =
+--	let it = powerset xs in
+--	let them = zip it (reverse it) in
+--	(tail.reverse.tail) them
