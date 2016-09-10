3484490dc75930037ed3e0bb8dbd66fda8171076
Jon Zingale <jon@jackrabbitsystems.com>
2016-09-10 17:55:01 -0600

    steps toward Lulz. first a List monad

---
+++ Lulz.hs
@@ -0,0 +1,26 @@
+import Control.Monad
+import Control.Applicative
+
+newtype List a = List { getList :: [a] } deriving (Show)
+
+instance Functor List where
+	fmap f (List []) = List []
+	fmap f (List xs) = List $ map f xs
+
+instance Applicative List where
+	pure n = List [n]
+	(<*>) = ap
+
+incl :: a -> List a
+incl = pure
+
+mu :: List(List a) -> List a
+mu mm = List $ concat.getList.fmap getList $ mm
+
+instance Monad List where
+	return n = incl n
+	m >>= f = (mu.fmap f) m
+
+m1 = incl 4
+m2 = (incl.incl) 4
+mix = List[List [5], List[6]]
