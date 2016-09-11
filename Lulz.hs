8b77dff6bce715a9a19e3462be058801e9a93062
Jonathan Zingale <jonzingale@gmail.com>
2016-09-10 20:44:11 -0600

    closer ?

--- Lulz.hs
+++ Lulz.hs
@@ -11,17 +11,19 @@ instance Applicative List where
 	pure n = List [n]
 	(<*>) = ap
 
-incl :: a -> List a
-incl = pure
-
--- not quite right as id :: a -> a not a -> ma
--- yet i can pass id as f to >>=
-mu :: List(List a) -> List a
-mu mm = List $ concat.getList.fmap getList $ mm
-
 instance Monad List where
 	return n = incl n
 	m >>= f = (mu.fmap f) m
+
+incl :: a -> List a
+incl = pure
+
+mu :: List(List a) -> List a
+mu (List ls) = List $ concat.fmap cc $ ls
+	where
+		cc (List []) = []
+		cc (List (x:xs)) = x : cc (List xs)
+--mu mm = List $ concat.getList.fmap getList $ mm
 
 m1 = incl 4
 m2 = (incl.incl) 4
