7a1cc3523998af68595671bc0cb058c63c6ab8a5
Jonathan Zingale <jonzingale@gmail.com>
2016-09-10 22:46:11 -0600

    a list monad

--- Lulz.hs
+++ Lulz.hs
@@ -1,29 +1,27 @@
 import Control.Monad
-import Control.Applicative
 
-newtype List a = List { getList :: [a] } deriving (Show)
+data List a = List [a] deriving (Show)
 
 instance Functor List where
-	fmap f (List []) = List []
-	fmap f (List xs) = List $ map f xs
+  fmap f (List []) = List []
+  fmap f (List xs) = List $ map f xs
 
 instance Applicative List where
-	pure n = List [n]
-	(<*>) = ap
+  pure n = List [n]
+  (<*>) = ap
 
 instance Monad List where
-	return n = incl n
-	m >>= f = (mu.fmap f) m
+  return n = incl n
+  m >>= f = (mu.fmap f) m
 
 incl :: a -> List a
 incl = pure
 
 mu :: List(List a) -> List a
 mu (List ls) = List $ concat.fmap cc $ ls
-	where
-		cc (List []) = []
-		cc (List (x:xs)) = x : cc (List xs)
---mu mm = List $ concat.getList.fmap getList $ mm
+  where
+    cc (List []) = []
+    cc (List (x:xs)) = x : cc (List xs)
 
 m1 = incl 4
 m2 = (incl.incl) 4
