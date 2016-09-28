201b98ac424dc561dffc4742812e05e1be5d7a9a
Jon Zingale <jon@jackrabbitsystems.com>
2016-09-28 05:59:13 -0600

    the ontv monad

--- ontv.hs
+++ ontv.hs
@@ -1,49 +1,33 @@
-module OnTV where
-import System.Random
-import Data.Char
+module OnTv where
+import Prelude hiding (Monad,(>>=))
 
-type N = Integer
+{-- 
+    The goal here is to flesh out
+    an idea about being on tv.
+    Namely, once one is on tv, one
+    is always on tv.
+--}
 
--- The goal here is to flesh out
--- some ideas about being on tv.
+data Tv s = Tv s deriving (Show)
 
---data Tree a = Nil 
---            | Node { left  :: Tree a,
---                     value :: a,
---                     right :: Tree a }
+instance Functor Tv where
+  fmap f (Tv s) = Tv (f s)
 
---three_number_tree :: Tree Integer
---three_number_tree = Node (Node Nil 1 Nil) 2 (Node Nil 3 Nil)
+instance Monad Tv where
+  unit b = Tv b
+  join (Tv (Tv a)) = Tv a
+  x >>= f = join $ fmap f x
 
-it = TV 4
-
---data TV s = Nil | Famous { me :: TV s}
---						| Pr { left :: TV s,
---									 value :: s,
---									 right :: TV s
---									}
-
-data TV s = TV s
---pr1 (TV a b) = a
---pr2 (TV a b) = b
-
-instance Show a => Show (TV a) where
-  show (TV a) = show a
-
-instance Functor TV where
-  fmap f (TV s) = TV (f s)
-
-class Functor m => Gonad m where
-  (>>>=) :: m a -> (a -> m b) -> m b
+class Functor m => Monad m where
+  (>>=) :: m a -> (a -> m b) -> m b
+  join :: m (m a) -> m a
   unit :: a -> m a
-  mult :: m (m a) -> m a
-  x >>>= f = mult $ fmap f x
 
-instance Gonad TV where
-  unit b = TV b
-  mult (TV (TV a)) = TV a
+-- Helpers
 
+camera :: String -> Tv String
+camera person = Tv person
 
--- note the f :: a -> m b
--- :t pr1(lit >>>= unit)
--- :t lit
+hipster = Tv "dude"
+
+main = putStrLn.show $ hipster >>= camera >>= camera
