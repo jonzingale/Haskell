5242dbbb00f1f5d2ee573b62fdfe8b9094223e8d
Jonathan Zingale <jonzingale@gmail.com>
2016-10-12 22:30:40 -0600

    another good idea about quantifiers and fermat primes

--- advanced_functional_programming/quantifiers/quantifiers_4.hs
+++ advanced_functional_programming/quantifiers/quantifiers_4.hs
@@ -1,14 +1,16 @@
 -- an attempt to encode the quantifiers
 import Control.Monad
+import qualified Data.Set as S
 
 data UxV u = P u [u] deriving (Show)
 type Fibers u = [UxV u]
 type Z = Integer
 
-another = P 1 [2,3,4]
 them = p_star [7,7,7] [1,2,3]
 up_two = lift another them
+another = P 1 [2,3,4]
 
+numbers :: Fibers Z
 numbers = p_star [2..10] [2..20]
 
 eval (P u v) = [ (u, y) | y <- v]
@@ -23,10 +25,24 @@ p_star vs us = map lift us <*> [vs]
 lift u vs = P u vs
 
 fermat :: Integral n => (n, n) -> Bool
-fermat (p, a) | a == mod (a^p) p = True
+fermat (p, a) | mod a p == mod (a^p) p = True
               | otherwise = False
 
-_S :: ((Z, Z) -> Bool) -> [(Z, Z)]
-_S f = filter f $ eval_fibers numbers
+_S :: ((t, t) -> Bool) -> [UxV t] -> [(t, t)]
+_S f fibered = filter f $ eval_fibers fibered
 
-fermat_S = _S fermat
+fermat_S = _S fermat numbers
+
+-- direct_image
+exists :: Ord a => [(a, b)] -> [a]
+exists someS = S.toList $ S.fromList $ map fst someS
+
+-- glb cylinder in S
+forall :: (Eq b, Eq t) => [(b, t)] -> Fibers t -> [b]
+forall someS (fibs:xs) = hh someS
+  where
+    roundup [] = []
+    roundup ss = ff ss : roundup (gg ss)
+    ff ((x,y):tt) = (x, y : ((snd.unzip.takeWhile ((== x).fst)) tt))
+    gg ((x,y):tt) = dropWhile ((== x).fst) tt
+    hh = (map fst . filter (\(x,y)->y == pr2 fibs) . roundup) 
