7d37fba13bd4ff67e9184cb48032073dafc5594a
Jonathan Zingale <jonzingale@gmail.com>
2016-10-13 21:29:14 -0600

    clean up

--- advanced_functional_programming/classroom_exercise_3.hs
+++ advanced_functional_programming/classroom_exercise_3.hs
@@ -1,28 +1,23 @@
--- State Monad
+import Control.Monad.State
 
 -- runState :: State s a -> s -> (a, s)
-newtype State s a = State { runState :: s -> (a,s) }
-
-get = State $ \s -> (s,s) -- return
-put s = State $ \_ -> ((),s)
+-- newtype State s a = State { runState :: s -> (a,s) }
 
 fromStoAandS :: Int -> (String, Int)
 fromStoAandS c | c `mod` 5 == 0 = ("foo",c+1)
                | otherwise = ("bar",c+1)
 
-stateIntString :: State Int String
-stateIntString = State fromStoAandS
-
-this = runState stateIntString 7
-that = runState stateIntString 1
-
 data Tree a = Unary (Tree a) | Binary (Tree a) (Tree a)
-    | Ternary (Tree a) (Tree a) (Tree a) | A a | G a | C a | T a
-    deriving (Show, Eq)
+              | Ternary (Tree a) (Tree a) (Tree a)
+              | A a | G a | C a | T a deriving (Show, Eq)
 
 foo :: Tree ()
 foo = Ternary (Binary (A ()) (G ())) (C ()) (Ternary (G ()) (G ())
               (Binary (Unary (C ())) (T ())))
+
+food :: Tree Int
+food = Ternary (Binary (A 0) (G 0)) (C 0) (Ternary (G 0) (G 0)
+              (Binary (Unary (C 0)) (T 0)))
 
 instance Functor Tree where
   fmap f (Ternary a b c) = Ternary (fmap f a) (fmap f b) (fmap f c)
@@ -36,15 +31,5 @@ instance Functor Tree where
 fromStoTreeandS :: Int -> (Tree (), Int)
 fromStoTreeandS c = (foo, c+1)
 
-stateIntTree :: State Int (Tree ())
-stateIntTree = State fromStoTreeandS
--- (runState (State fromStoTreeandS)) 3
-
 treeBlanktoInt :: Tree a -> Tree Int
 treeBlanktoInt tree = fmap (\x-> 0) tree
-
-updateG tree | tree == G 0 = G 1
-             | otherwise = tree
-
--- label :: Tree a -> Tree Int
--- label Tree t | 
