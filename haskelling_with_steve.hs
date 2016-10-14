05fa71f963801d9e438b5f810e1edcc4e1822224
Jonathan Zingale <jonzingale@gmail.com>
2016-10-13 21:07:54 -0600

    yeah steve, we coded

---
+++ haskelling_with_steve.hs
@@ -0,0 +1,13 @@
+module HaskellingWithSteve where
+
+array = [1,2,3,4]
+chars = ['a','v','c']
+
+wax :: [String]
+wax = ["fluke", "endgame"]
+
+
+int2char x = show x
+
+fun :: [Integer -> Integer]
+fun = [(+2), (+3)]
