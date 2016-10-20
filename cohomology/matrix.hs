0cefe11587173776f823a7330e28165e2c6e9a2a
Jonathan Zingale <jonzingale@gmail.com>
2016-10-20 12:27:29 -0600

    is_cycle :: Chain -> Bool, ideas on boundary via determinants

--- cohomology/matrix.hs
+++ cohomology/matrix.hs
@@ -3,9 +3,16 @@ import qualified Numeric.Matrix as M
 import qualified Data.Vector as V
 import Data.Matrix
 
-type MMatrix = M.Matrix Float
+type ZMatrix = M.Matrix Integer
+type RMatrix = M.Matrix Float
 
-m2 :: MMatrix
+tetra :: ZMatrix
+tetra = M.fromList [[0,1,1,1],[1,0,1,1],[1,1,0,1],[1,1,1,0]]
+
+m1 :: ZMatrix
+m1 = M.fromList [[1,2,3],[4,5,6],[7,8,8]]
+
+m2 :: RMatrix
 m2 = M.fromList [[1.0,2.0,3.0],[4.0,5.0,6.0],[7.0,8.0,8.0]]
 
 rank2 = M.rank m2
