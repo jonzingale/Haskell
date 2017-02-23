2e524122dec0ed14bba24c50d380490cda5c82f6
Jon Zingale <jon@jackrabbitsystems.com>
2017-02-23 10:55:06 -0700

    separated number class from number arrays

--- tournament/NumberArray.hs
+++ tournament/NumberArray.hs
@@ -1,31 +1,6 @@
 module NumberArray where
 import Prelude hiding (head, (++), tail, length, push, filter)
 
-data NumArray n = N n | BAD deriving (Show, Eq, Ord)
-
-incl :: (Integral a) => a -> NumArray a
-incl n | n < 0 = BAD
-       | otherwise = N n
-
-eval :: (Integral a) => NumArray a -> a
-eval (N a) = a
-eval BAD = -1
-
-instance (Num a, Integral a) => Num (NumArray a) where
-  x + y | x == BAD || y == BAD = BAD
-        | otherwise = incl (eval x + eval y)
-
-  x * y | x == BAD || y == BAD = BAD
-        | otherwise = incl (eval x * eval y)
-
-  x - y | x == BAD || y == BAD = BAD
-        | otherwise = incl (eval x - eval y)
-
--- instance (Num a, Integral a) => Integral (NumArray a) where
---   div x y | x == BAD || y == BAD = BAD
---           | otherwise = N (mod x y)
-
--- type N = NumArray Integer
 type N = Integer
 
 head :: N -> N
