0bb12f4639b828f509cd39c35a09599c0772b80f
Jonathan Zingale <jonzingale@gmail.com>
2017-03-21 00:17:17 -0600

    a Sortable class generalizing sorting on arrays and digits

--- number_theory/Sortable.hs
+++ number_theory/Sortable.hs
@@ -1,41 +1,49 @@
-{-# OPTIONS_GHC -fno-warn-tabs #-}
--- # LANGUAGE FlexibleInstances #
-
+-- {-# OPTIONS_GHC -fno-warn-tabs #-}
 module Sortable where
 import Prelude hiding (head, (++), tail, length, push, filter)
-import Data.Monoid
 
-instance Monoid Integer where
-  mconcat = foldr mappend mempty
-  mappend a b = a + b
-  mempty = 0
+ex_1 :: Integer
+ex_1 = qsort 2937452435798
 
-instance ArrayLike Integer where
-  head n = mod n 10
-  tail n = div n 10
-  length 0 = 0
-  length n = 1 + (length.tail) n
+ex_2 :: [Integer]
+ex_2 = qsort [2,9,3,7,4,5,2,4,3,5,7,9,8]
+
+instance Sortable Integer where
   (++) n m = m + n * 10^(length m)
   push n ns = n + ns * 10
-  filter b ns = f b ns 0
-    where
-      f b 0 accum  = accum
-      f b js accum | (b.head) js = f b (tail js) $ push (head js) accum
-                   | otherwise   = f b (tail js) accum
+  head n = mod n 10
+  tail n = div n 10
+  mm = 0
 
--- qsort :: (ArrayLike a, Ord a) => a -> a
-qsort 0 = mempty
-qsort ns = branch smaller ns ++ head ns ++ branch larger ns
-  where
-    branch f xs = qsort.f (head xs) $ tail xs
-    smaller n ns = filter (<= n) ns
-    larger n ns  = filter (>  n) ns
+instance (Ord a, Show a) => Sortable [a] where
+  (++) [] b = b
+  (++) [a] [b] = [a,b]
+  (++) (a:as) bs = a : (as ++ bs)
+  push (a:[]) bs = a : bs
+  head (n:ns) = [n]
+  tail (n:ns) = ns
+  mm = []
 
-class (Ord s, Eq s, Show s, Monoid s) => ArrayLike s where
-  head :: s -> s
-  tail :: s -> s
-  length :: s -> Int
+class (Ord s, Show s) => Sortable s where
+  filter :: (s -> Bool) -> s -> s
+  head, tail, qsort :: s -> s
   (++) :: s -> s -> s
   push :: s -> s -> s
-  filter :: (s -> Bool) -> s -> s
+  length :: s -> Int
+  mm :: s
 
+  length ns | ns == mm = 0 
+            | otherwise = 1 + (length.tail) ns
+
+  filter b ns = f b ns mm
+    where
+      f b js accum | js == mm = accum
+                   | (b.head) js = f b (tail js) $ push (head js) accum
+                   | otherwise   = f b (tail js) accum
+
+  qsort ns | ns == mm = mm
+           | otherwise = branch smaller ns ++ head ns ++ branch larger ns
+    where
+      branch f xs = qsort.f (head xs) $ tail xs
+      smaller n ns = filter (<= n) ns
+      larger n ns  = filter (>  n) ns
