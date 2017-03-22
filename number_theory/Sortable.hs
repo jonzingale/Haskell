0ce8810aa796bfa299bf818dda54901b853be366
Jonathan Zingale <jonzingale@gmail.com>
2017-03-22 11:47:24 -0600

    towards sortable trees

--- number_theory/Sortable.hs
+++ number_theory/Sortable.hs
@@ -1,6 +1,6 @@
 -- {-# OPTIONS_GHC -fno-warn-tabs #-}
 module Sortable where
-import Prelude hiding (head, (++), tail, length, push, filter)
+import Prelude hiding (head, (++), tail, length, filter)
 import Data.Tree
 
 ex_1 :: Integer
@@ -11,16 +11,13 @@ ex_2 = qsort [2,9,3,7,4,5,2,4,3,5,7,9,8]
 
 instance Sortable Integer where
   (++) n m = m + n * 10^(length m)
-  push n ns = n + ns * 10
   head n = mod n 10
   tail n = div n 10
   unit = 0
 
 instance (Ord a, Show a) => Sortable [a] where
   (++) [] b = b
-  (++) [a] [b] = [a,b]
   (++) (a:as) bs = a : (as ++ bs)
-  push (a:[]) bs = a : bs
   head (n:ns) = [n]
   tail (n:ns) = ns
   unit = []
@@ -29,11 +26,10 @@ class (Ord s, Show s) => Sortable s where
   filter :: (s -> Bool) -> s -> s
   head, tail, qsort :: s -> s
   (++) :: s -> s -> s
-  push :: s -> s -> s
   length :: s -> Int
   unit :: s
 
-  length ns | ns == unit = 0 
+  length ns | ns == unit = 0
             | otherwise = 1 + (length.tail) ns
 
   filter b ns = f b ns unit
--- number_theory/SortableTree.hs
+++ number_theory/SortableTree.hs
@@ -1,11 +1,11 @@
 -- {-# OPTIONS_GHC -fno-warn-tabs #-}
-module Sortable where
+module SortableTrees where
 import Prelude hiding (head, (++), tail, length, filter)
 
 ex_2 :: [Integer]
 ex_2 = qsort [2,9,3,7,4,5,2,4,3,5,7,9,8]
 
-t1 = Fork (Leaf 1) (Leaf 4)
+t1 = Fork (Leaf 4) (Leaf 1)
 t2 = Fork (Leaf 3) t1
 t3 = t1 ++ t2
 
@@ -23,16 +23,19 @@ instance (Ord a, Show a) => Ord (Btree a) where
   (<=) (Fork l r) (Fork s t) = l <= s
 
 instance (Ord a, Show a) => Sortable (Btree a) where
+  (++) Empty tree = tree
+  (++) tree Empty = tree
   (++) left right = Fork left right
   tail Empty = Empty
   tail (Leaf a) = Empty
-  tail (Fork l r) = tail l
+  tail (Fork _ r) = r
+  head Empty = Empty
+  head (Leaf a) = Leaf a
+  head (Fork l _) = l
   unit = Empty
-  head n = n
 
 instance (Ord a, Show a) => Sortable [a] where
   (++) [] b = b
-  (++) [a] [b] = [a,b]
   (++) (a:as) bs = a : (as ++ bs)
   head (n:ns) = [n]
   tail (n:ns) = ns
@@ -45,14 +48,14 @@ class (Ord s, Show s) => Sortable s where
   length :: s -> Int
   unit :: s
 
-  length ns | ns == unit = 0 
+  length ns | ns == unit = 0
             | otherwise = 1 + (length.tail) ns
 
-  filter b ns = f b ns unit
+  filter bool ns = f bool ns unit
     where
-      f b js accum | js == unit = accum
-                   | (b.head) js = f b (tail js) $  head js ++ accum
-                   | otherwise   = f b (tail js) accum
+      f bool js accum | js == unit = accum
+                      | (bool.head) js = f bool (tail js) $  head js ++ accum
+                      | otherwise   = f bool (tail js) accum
 
   qsort ns | ns == unit = unit
            | otherwise = branch smaller ns ++ head ns ++ branch larger ns
