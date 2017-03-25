9ccfc164d7f4101887c5bcaf541e51ad9a79b9b0
Jonathan Zingale <jonzingale@gmail.com>
2017-03-25 00:51:21 -0600

    added vector_fields

---
+++ vector_fields/SmoothManifolds.hs
@@ -0,0 +1,7 @@
+module SmoothManifolds where
+import VectorFields
+{--
+The thing to do here is to begin by defining the
+directional derivative and then to extend toward
+cohomology via exterior algebra and tensors.
+--} 
