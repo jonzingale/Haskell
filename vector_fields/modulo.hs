52a62b32f359bc6ce43b1ada341104a49e975faf
Jonathan Zingale <jonzingale@gmail.com>
2017-04-09 18:33:04 -0600

    copy of modulo module extended for inverses

---
+++ vector_fields/modulo.hs
@@ -0,0 +1,37 @@
+{-# OPTIONS_GHC -fno-warn-tabs #-}
+
+module Modulo where
+
+data Mod m = Mod { modulus :: m, value :: m } | BAD deriving (Show)
+
+incl :: (Integral a) => a -> a -> Mod a
+incl n a = Mod n a
+
+rep :: (Integral a) => Mod a -> a
+rep (Mod n a) = mod a n
+
+instance (Eq a) => Eq (Mod a) where
+	Mod a b == Mod c d | a == c && b == d = True
+										 | otherwise = False
+
+instance (Num a, Integral a) => Num (Mod a) where
+	Mod a b + Mod c d  | a == c = Mod a $ (b + d) `mod` a
+										 | otherwise = BAD
+
+	Mod a b * Mod c d | a == c = Mod a $ (b * d) `mod` a
+										| otherwise = BAD
+
+	negate (Mod a b) = Mod a $ (negate b) `mod` a
+	fromInteger a = incl (fromIntegral a) 0
+	abs (Mod a b) = Mod a $ b `mod` a
+	signum (Mod a b) = 1
+
+inv :: Integral k => Mod k -> Mod k
+inv k | null.f $ k = BAD
+			| otherwise = head.f $ k 
+	where
+		f (Mod n k) = [Mod n i | i<-[1..n-1], (Mod n k) * (Mod n i) == Mod n 1]
+
+this = Mod 7 5
+that = Mod 3 11
+
