8d72536cad055b3519809f3a063cea5921f48e55
Jon Zingale <jon@jackrabbitsystems.com>
2017-04-10 22:55:25 -0600

    correction

--- vector_fields/SymbolicExteriorAlgebra.hs
+++ vector_fields/SymbolicExteriorAlgebra.hs
@@ -5,20 +5,20 @@ data Ext f s = Rval f String |
                Del (Ext f String) |
                Wedge (Ext f String) (Ext f String)
 
-type OneForm = Ext (Integer->Integer) String
-type TwoForm = Ext (Integer->Integer->Integer) String
+type V1 = Ext (Integer->Integer) String
+type V2 = Ext (Integer->Integer->Integer) String
 
 instance Show (Ext f s) where
   show (Wedge dx dy) = show dx ++ " ^ " ++ show dy
   show (Del dx) = "d" ++ show dx
   show (Rval f s) = s
 
-df1, df2 :: OneForm
+df1, df2 :: V1
 df1 = Del $ Rval (\n -> 1) "f1"
 df2 = Del $ Rval (\n -> n*2) "f2"
 wedge = Wedge df1 df2
 
-df3 :: TwoForm
+df3 :: V2
 df3 = Del $ Rval (\x y -> x*2) "f3"
 
 {--
