672d704d602de86e34adf0d70fc8acad0bbbc7ce
Jonathan Zingale <jonzingale@gmail.com>
2017-04-12 16:16:25 -0600

    comments

--- vector_fields/Functionals.hs
+++ vector_fields/Functionals.hs
@@ -8,11 +8,14 @@ module Functionals where
   are zero. For typographical simplicity, k = 3.
 --}
 
-f3 :: Functional Integer
-f3 = Lf $ \x y z -> x + 10 * y + 100 * z
+f1, f2, f3 :: Functional Integer
+f1 = Lf $ \x y z -> x + 10 * y + 100 * z
+f2 = Lf $ \x y z -> x + x * y + 2 * z
+f3 = Lf $ \x y z -> x * z + x * y + z^2
 
-test1 = eval (3 * (f3 + f3)) 5 2 1
-test2 = eval1 (pr1 f3) 1 + eval1 (pr2 f3) 2 + eval1 (pr3 f3) 3 == eval f3 1 2 3
+test1 = eval (3 * (f3 + f2)) 5 2 1
+test2 f = eval1 (pr1 f) 1 + eval1 (pr2 f) 2 + eval1 (pr3 f) 3 == eval f 1 2 3
+-- test2 is only generally true when linearly independent?
 
 data Functional a = C a | Lf (a -> a -> a -> a) | Pi (a -> a)
 
