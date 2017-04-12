44834f316a16a6978312d0e650ca3a9af46f60c9
Jon Zingale <jon@jackrabbitsystems.com>
2017-04-12 10:08:55 -0600

    k defaults to 3 for functional

--- vector_fields/Functionals.hs
+++ vector_fields/Functionals.hs
@@ -1,19 +1,26 @@
 module Functionals where
 
-data Functional a = Lf (a -> a) | C a
+{--
+  Linear Functionals are here defined by a finite list
+  of inputs, ie a function R^k -> R for some number k
+  and ring R. Functionals R^2 -> R, with 2 < k for instance,
+  is treated as an inclusion of R^k -> R where all 2 < i < k
+  are zero. For typographical simplicity, k = 3.
+--}
 
-eval :: Num a => (Functional a) -> a -> a
-eval (Lf f) a = f a
+data Functional a = C a | Lf (a -> a -> a -> a) 
 
-c1 = 2
-f1 = Lf $ \x -> (10 * x)^2
-test = eval (3 * (f1 + f1)) 5
+eval :: Num a => (Functional a) -> a -> a -> a -> a
+eval (Lf f) = f
+
+f3 = Lf $ \x y z -> (10 * x)^2 + 2*y + z
+test = eval (3 * (f3 + f3)) 5 2 1
 
 -- ‘abs’, ‘signum’
 instance (Num a) => Num (Functional a) where
-  (+) (Lf f) (Lf g) = Lf $ \x -> f x + g x
-  (-) (Lf f) (Lf g) = Lf $ \x -> f x - g x
-  (*) (Lf f) (Lf g) = Lf $ \x -> f x * g x
-  (*) (C a)  (Lf f) = Lf $ \x -> a * f x
+  (+) (Lf f) (Lf g) = Lf $ \x y z -> f x y z + g x y z
+  (-) (Lf f) (Lf g) = Lf $ \x y z -> f x y z - g x y z
+  (*) (Lf f) (Lf g) = Lf $ \x y z -> f x y z * g x y z
+  (*) (C a)  (Lf f) = Lf $ \x y z -> a * f x y z
   fromInteger = C . fromIntegral
   negate f = C (-1) * f
