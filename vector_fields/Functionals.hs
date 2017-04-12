04bd8675ca1918f9a55dfdd8432ae5f53690ee74
Jon Zingale <jon@jackrabbitsystems.com>
2017-04-12 15:44:54 -0600

    now with projection maps

--- vector_fields/Functionals.hs
+++ vector_fields/Functionals.hs
@@ -8,13 +8,24 @@ module Functionals where
   are zero. For typographical simplicity, k = 3.
 --}
 
-data Functional a = C a | Lf (a -> a -> a -> a) 
+f3 :: Functional Integer
+f3 = Lf $ \x y z -> x + 10 * y + 100 * z
+
+test1 = eval (3 * (f3 + f3)) 5 2 1
+test2 = eval1 (pr1 f3) 1 + eval1 (pr2 f3) 2 + eval1 (pr3 f3) 3 == eval f3 1 2 3
+
+data Functional a = C a | Lf (a -> a -> a -> a) | Pi (a -> a)
 
 eval :: Num a => (Functional a) -> a -> a -> a -> a
 eval (Lf f) = f
 
-f3 = Lf $ \x y z -> (10 * x)^2 + 2*y + z
-test = eval (3 * (f3 + f3)) 5 2 1
+eval1 :: Num a => (Functional a) -> a -> a
+eval1 (Pi f) = f
+
+pr1, pr2, pr3 :: Num a => Functional a -> Functional a
+pr1 (Lf f) = Pi $ (\f i -> f i 0 0) f
+pr2 (Lf f) = Pi $ (\f i -> f 0 i 0) f
+pr3 (Lf f) = Pi $ (\f i -> f 0 0 i) f
 
 -- ‘abs’, ‘signum’
 instance (Num a) => Num (Functional a) where
