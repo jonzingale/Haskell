3e700aac499586ff4d55996dabfe7ac59c79e22c
Jon Zingale <jon@jackrabbitsystems.com>
2017-04-20 16:27:17 -0600

    clean up

--- vector_fields/Functional.hs
+++ vector_fields/Functional.hs
@@ -12,8 +12,6 @@ instance (Ord t, Num t) => Num (Functional t) where
   signum (T f) | f 1 1 1 < 0 = fromInteger (-1) -- arbitrary
                | otherwise = fromInteger 1
   
-
-
 pr1 (T f) = (\f i -> f i 0 0) f
 pr2 (T f) = (\f i -> f 0 i 0) f
 pr3 (T f) = (\f i -> f 0 0 i) f
