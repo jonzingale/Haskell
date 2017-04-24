65eded350c57ae39daf0c4bac0be58e5611e2957
Jonathan Zingale <jonzingale@gmail.com>
2017-04-23 20:36:24 -0600

    clean up

--- vector_fields/LinearAlgebra.hs
+++ vector_fields/LinearAlgebra.hs
@@ -23,10 +23,10 @@ scalar = S 5
 v1 = V3 2 3 5
 v3 = V3 1 2 1
 f1 = T $ \i j k-> 2*i + 3*j + 5*k
+idf = T $ \i j k-> i + j + k
 
-this = (tr_dn.tr_up) v3
-that = (tr_up v3) <|> v3
-thap = (tr_up v3) <|> v1
+test1 = (tr_dn.tr_up) v3
+test2 = (tr_up v3) <|> v3 == idf <|> (v3 * v3)
 
 tr_up :: Num v => Vector v -> Functional v
 tr_up (V3 x y z) = T $ \i j k -> i*x + j*y + k*z
@@ -38,4 +38,4 @@ instance Num v => Linear (Functional v) (Vector v) where
   (<|>) f (V3 x y z) = S $ pr1 f x + pr2 f y + pr3 f z
 
 class Linear f v | v -> f where
-  (<|>) :: f -> v -> v -- eval
+  (<|>) :: f -> v -> v -- eval, think 2-tensor.
