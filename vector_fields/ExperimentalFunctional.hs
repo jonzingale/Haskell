60c966cc16ccac998d20035f7620c6fb7fb1c1a6
Jonathan Zingale <jonzingale@gmail.com>
2017-04-24 18:51:27 -0600

    clean up

--- vector_fields/ExperimentalFunctional.hs
+++ vector_fields/ExperimentalFunctional.hs
@@ -8,7 +8,7 @@ f1 :: L (Integer -> Integer -> Integer -> Integer)
 f1 = Fun (\i j k -> i+j+k)
 
 instance (Ord t, Ord s, Ord r, Ord b, Num b, Num t, Num s, Num r) => Num (Fun t s r b) where
-  (+) (Fun f) (Fun g) = Fun $ \x y z -> f x y z + g x y z
+  (+) (T f) (T g) = T $ \x y z -> f x y z + g x y z
 
 instance Functor (Fun t s r) where
   fmap f (T g) = T $ (\i j -> (f.(g i j)))
