9287a2bdaf9d2496e307101a18eda10bdf860de2
Jon Zingale <jonzingale@gmail.com>
2017-08-24 14:31:40 -0600

    checking vector sizes

--- diagonals_problem/diagonals2.hs
+++ diagonals_problem/diagonals2.hs
@@ -19,12 +19,12 @@ bot (n:ns) | n == 1 = 1 * 10^length(ns) + bot ns
            | otherwise = bot ns
 
 join :: [N] -> [N] -> Bool
-join ts bs = validV $ bot ts + top bs
+join ts bs = valid_pairing $ bot ts + top bs
 
-validV :: N -> Bool
-validV n | n < 2 = True
+valid_pairing :: N -> Bool
+valid_pairing n | n < 2 = True
          | mod n 10 == 2 = False
-         | otherwise = validV $ div n 10
+         | otherwise = valid_pairing $ div n 10
 
 vand :: Bool -> Bool -> Bool
 vand n m = and [n, m]
@@ -40,7 +40,37 @@ processList (n:m:ps) = vand (join n m) $ processList (m:ps)
 To count maxima, covered_corners/2.
 3^7 Vectors thus (3^7)^7 == 3^49
 239299329230617529590083 matrices.
-but only 2^64 == 18446744073709551616 ?
-That can't be right.
+
+perhaps start with the occupancy
+problem of the individual vectors.
+2^7 with only ones, 2^7 only twos,
+and some number with a mixture.
+
+still only 3^7 = 2187 vectors so,
+how many less are valid?
+
+pairwise sums to 3 are out. effectively,
+these are powers of 11 in ternary.
+
+5,7 are good to test.
 --}
+
+jt = (vects 7)!!42
+
+validV :: N -> Bool
+validV n | n < 10 = True
+         | mod (mod n 100) 3 == 0 = False 
+         | otherwise = validV.div n $ 10
+
+vects n = map tern [0..3^n-1] -- only n digit numbers.
+
+goodVs = [vv | vv<-vects 7, validV vv] -- 255 == 2^8 - 1
+
+tern :: N -> N
+tern n = f n 0
+  where
+    f n i | n < 3 = n * 10^i
+          | otherwise = (mod n 3) * 10^i + f (div n 3) (i+1)
+
+
 
