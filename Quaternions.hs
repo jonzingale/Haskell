966822164ec29178a848fe7bda6eaa559057c72c
Jon Zingale <jonzingale@gmail.com>
2017-08-31 15:44:36 -0600

    further into typeclass

--- Quaternions.hs
+++ Quaternions.hs
@@ -2,6 +2,9 @@ module Quaternions where
 
 data Q a = Q a a a a deriving (Eq, Show)
 
+conj :: Num a => Q a -> Q a
+conj (Q a b c d) = Q a (-b) (-c) (-d)
+
 -- a + bi + cj + dk
 -- i^2 = j^2 = k^2 = ijk = −1
 
@@ -11,8 +14,9 @@ data Q a = Q a a a a deriving (Eq, Show)
 
 -- perhaps another instance Eq a for the 2 to 1 projection.
 
--- ‘*’, ‘abs’, ‘signum’, ‘fromInteger’, and (either ‘negate’ or ‘-’)
+-- ‘abs’, ‘signum’, and (either ‘negate’ or ‘-’)
 instance (Num a, Integral a) => Num (Q a) where
+  fromInteger n = Q (fromInteger n) 0 0 0
   Q a b c d + Q e f g h = Q (a+e) (b+f) (c+g) (d+h)
   Q a b c d * Q e f g h =
       let u = a*e-b*f-c*g-d*h in
@@ -20,5 +24,6 @@ instance (Num a, Integral a) => Num (Q a) where
       let j = a*g-b*h+c*e+d*f in
       let k = a*h+b*g-c*f+d*e in
       Q u i j k
-  fromInteger n = Q (fromInteger n) 0 0 0
-  -- Q a b c d + Q e f g h = Q (a+e) (b+f) (c+g) (d+h) 
+
+-- as this will not be an Integer value.
+abs (Q a b c d) = Q ((a*a + b*b + c*c + d*d)**0.5) 0 0 0
