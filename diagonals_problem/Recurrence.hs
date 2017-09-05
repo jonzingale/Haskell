b3b0942f2c4c1abb74c3a8b2f10e796a1d6bb3e5
Jon Zingale <jonzingale@gmail.com>
2017-09-05 10:37:10 -0600

    corecursion for diagonals defined

--- diagonals_problem/Recurrence.hs
+++ diagonals_problem/Recurrence.hs
@@ -7,7 +7,18 @@ choose n 0 = 1
 choose n k | n < k = 0
            | otherwise = choose (n-1) (k-1) + choose (n-1) k
 
--- no Show.
+-- no Show. corecusion.
 ff :: N -> N
 ff 0 = ff 0 + ff 1
 ff 1 = ff 0
+
+fib :: N -> N
+fib 0 = 1
+fib 1 = 1
+fib n = fib (n-1) + fib (n-2)
+
+-- Diagonals
+dd :: N -> N -- lotka-like
+dd 0 = dd 1 + dd 0 + dd 2
+dd 1 = dd 1 + dd 0
+dd 2 = dd 0 + dd 2
