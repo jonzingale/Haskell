427e5cf5dbd143636d92311cf135ff54f78c772d
Jon Zingale <jonzingale@gmail.com>
2017-09-11 20:31:18 -0600

    quaternions clean up

--- Quaternions.hs
+++ Quaternions.hs
@@ -2,22 +2,20 @@ module Quaternions where
 
 data Q a = Q a a a a deriving (Eq, Show)
 
-conj :: Num a => Q a -> Q a
-conj (Q a b c d) = Q a (-b) (-c) (-d)
-
 -- a + bi + cj + dk
 -- i^2 = j^2 = k^2 = ijk = −1
 
--- instance (Eq a) => Eq (Mod a) where
---   Q a b c d == Q e f g h | a == c && b == d = True
---                          | otherwise = False
+qq = Q 1 2 3 4 
+uu = Q 5 2 3 1
 
--- perhaps another instance Eq a for the 2 to 1 projection.
+conj :: Num a => Q a -> Q a
+conj (Q a b c d) = Q a (-b) (-c) (-d)
 
 -- ‘abs’, ‘signum’, and (either ‘negate’ or ‘-’)
 instance (Num a, Integral a) => Num (Q a) where
-  fromInteger n = Q (fromInteger n) 0 0 0
   Q a b c d + Q e f g h = Q (a+e) (b+f) (c+g) (d+h)
+  fromInteger n = Q (fromInteger n) 0 0 0
+  negate qq = qq * Q (-1) 0 0 0
   Q a b c d * Q e f g h =
       let u = a*e-b*f-c*g-d*h in
       let i = a*f+b*e+c*h-d*g in
