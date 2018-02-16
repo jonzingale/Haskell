784326478db1bfbf99e6155c6b5d5e8434eabdaa
Jon Zingale <jonzingale@gmail.com>
2018-02-15 22:14:57 -0700

    worked through details for 3 dimensional complex vectors

--- HaskellStudy/LinearAlgebra/ComplexVector/Complex.hs
+++ HaskellStudy/LinearAlgebra/ComplexVector/Complex.hs
@@ -3,28 +3,29 @@
 module Complex where
 import Text.Printf
 
+c1 :: Complex
+c1  = C 3 1
+
 data Complex = C {real::Double, imag::Double} deriving (Eq)
 
 instance Show Complex where
   show (C a b) | b < 0 = printf "%f%fi" a b
                | otherwise = printf "%f+%fi" a b
 
-c1 :: Complex
-c1  = C 3 1
+class Comp c where
+  conj :: c -> c
+  (<|>) :: c -> c -> c
 
-conj :: Complex -> Complex
-conj (C a b) = C a (-b)
-
-(<|>) :: Complex -> Complex -> Complex
-(<|>) a b = a * conj b
-
+instance Comp Complex where
+  conj (C a b) = C a (-b)
+  (<|>) a b = a * conj b
 
 instance Num Complex where
   fromInteger x = C (fromInteger x) 0
   (+) (C a b) (C c d) = C (a+c) (b+d)
   (*) (C a b) (C c d) = C (a*c-b*d) (a*d + b*c)
   (-) (C a b) (C c d) = C (a-c) (b-d)
-  abs cc = C (sqrt.real $ cc <|> cc) 0
+  abs cc = C (sqrt.real $ cc * conj cc) 0
   signum (C a b) | and [a>0, b>0] = 1
                  | and [a>0, b<0] = 4
                  | and [a<0, b>0] = 2
--- HaskellStudy/LinearAlgebra/ComplexVector/ComplexVector.hs
+++ HaskellStudy/LinearAlgebra/ComplexVector/ComplexVector.hs
@@ -1,26 +1,38 @@
 module ComplexVector where
 import Complex
 
-data Vector x = S x | V2 x x | V3 x x x deriving (Show, Eq)
+cv1 = V3 (C 1 (-1)) (C 2 3) (C 5 0) -- 2 * sqrt 10
 
-cv3 = V3 c1 (c1*c1) (c1*3)
-cv2 = V2 (C 1 2) (C 3 (-1))
-cv22 = V2 (C (-2) 1) (C 4 0)
+data Vector x = S x | V3 x x x | Bad deriving (Show, Eq)
+
+eval :: Num x => Vector x -> x
+eval (V3 a b c) = a + b + c
+eval (S x) = x
 
 instance Functor Vector where
   fmap f (V3 x y z) = V3 (f x) (f y) (f z)
-  fmap f (V2 x y) = V2 (f x) (f y)
   fmap f (S x) = S (f x)
 
 instance Applicative Vector where
   (V3 f g h) <*> (V3 x y z) = V3 (f x) (g y) (h z)
-  (V2 f g) <*> (V2 x y) = V2 (f x) (g y)
   pure t = S t
 
-instance Num v => Num (Vector v)  where
-  fromInteger x = fmap fromInteger $ V3 x x x
-  (+) v w = (+) <$> v <*> w
-  (*) v w = (*) <$> v <*> w-- this ought to be innerproduct. conj ect . . ., but we don't know it's complex!
-  negate v = (-1) * v
-  abs v = v * v --verify
-  signum = id -- arbitrary
+instance (Floating c, Num c, Comp c) => Comp (Vector c) where
+  conj vect = fmap conj vect
+  (<|>) a b = a * b
+
+instance (Floating v, Comp v, Num v) => Num (Vector v)  where
+  (+) (S a) (S b) = S $ a + b
+  (+) (V3 _ _ _) (S _) = Bad
+  (+) (S _) (V3 _ _ _) = Bad
+  (+) (V3 a b c) (V3 d e f) = V3 (a+d) (b+e) (c+f)
+
+  (*) (S a) (S b) = S $ a * b
+  (*) (S a) (V3 d e f) = V3 (a*d) (a*e) (a*f)
+  (*) (V3 d e f) (S a) = V3 (a*d) (a*e) (a*f)
+  (*) v w = (*) <$> v <*> (fmap conj w)
+
+  fromInteger x = S $ fromInteger x
+  abs v = S $ sqrt.eval $ v <|> v
+  signum = id -- not sure what this means.
+  negate v = fmap (* (-1)) v
