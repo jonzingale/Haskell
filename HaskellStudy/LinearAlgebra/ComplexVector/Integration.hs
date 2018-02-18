273136b103fff68c49782211239a9830601f212d
Jon Zingale <jonzingale@gmail.com>
2018-02-18 13:21:41 -0700

    toward integration methods, euler and improved euler

--- HaskellStudy/LinearAlgebra/ComplexVector/Integration.hs
+++ HaskellStudy/LinearAlgebra/ComplexVector/Integration.hs
@@ -1,8 +1,19 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FunctionalDependencies #-}
+
 module Integration where
 import ComplexVector
 import Complex
 
 type LinearT v = Vector v -> Vector v
+
+instance (Fractional v, Floating v, Comp v, Num v) => Integrable (LinearT v) where
+  euler f v = v + f v * delT 
+
+class Integrable v where
+  euler :: LinearT v -> Vector v -> Vector v
+
+
 
 delT :: (Fractional v, Floating v, Num v) => Vector v
 delT = V3 0.01 0.01 0.01
@@ -12,10 +23,10 @@ pt = V3 (C 1 0) (C 0 0) (C 0 0)
 pendulum :: Num v => Vector v -> Vector v
 pendulum (V3 a b c) = V3 (-b) a 0
 
-euler :: (Fractional v, Floating v, Comp v, Num v) => LinearT v -> Vector v -> Vector v
-euler f v = v + f v * delT 
+-- euler :: (Fractional v, Floating v, Comp v, Num v) => LinearT v -> Vector v -> Vector v
+-- euler f v = v + f v * delT 
 
-improvedEuler :: (Fractional v, Floating v, Comp v, Num v) => LinearT v -> Vector v -> Vector v
-improvedEuler f v = let ds = euler f v in
-                    let dds = euler f ds in
-                    fmap (* 0.5) (ds + dds)
+-- improvedEuler :: (Fractional v, Floating v, Comp v, Num v) => LinearT v -> Vector v -> Vector v
+-- improvedEuler f v = let ds = euler f v in
+--                     let dds = euler f ds in
+--                     fmap (* 0.5) (ds + dds)
