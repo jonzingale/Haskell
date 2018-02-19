e1aaecc6bd6c9f21fa16e26ab4651d8a2cd07620
Jon Zingale <jonzingale@gmail.com>
2018-02-19 00:54:28 -0700

    toward runge-kutta

--- HaskellStudy/LinearAlgebra/ComplexVector/Integration.hs
+++ HaskellStudy/LinearAlgebra/ComplexVector/Integration.hs
@@ -3,22 +3,26 @@ import ComplexVector
 import Complex
 
 type LinearT v = Vector v -> Vector v
-
+type Integration v = LinearT v -> Vector v -> Vector v
 
 delT :: (Fractional v, Floating v, Num v) => Vector v
-delT = V3 0.01 0.01 0.01
+delT = S 0.01 -- hmm
 
-pt = V3 (C 1 0) (C 0 0) (C 0 0)
+pt = V3 (C 1 0) (C 1 0) (C 0 0)
 
-pendulum :: Num v => Vector v -> Vector v
+pendulum :: Num v => LinearT v
 pendulum (V3 a b c) = V3 (-b) a 0
 
-euler :: (Fractional v, Floating v, Comp v, Num v) => LinearT v -> Vector v -> Vector v
+euler :: (Fractional v, Floating v, Comp v, Num v) => Integration v
 euler f v = v + f v * delT 
 
-improvedEuler :: (Fractional v, Floating v, Comp v, Num v) => LinearT v -> Vector v -> Vector v
+improvedEuler :: (Fractional v, Floating v, Comp v, Num v) => Integration v
 improvedEuler f v = let ds = euler f v in
                     let dds = euler f ds in
                     fmap (* 0.5) (ds + dds)
 
--- rungeKutta :: 
+-- This needs to be completed.
+rungeKutta :: (Fractional v, Floating v, Comp v, Num v) => Integration v
+rungeKutta f v = let k1 = delT * f v in
+                 let k2 = delT * f (v + k1 * delT) in
+                 k1 + k2
