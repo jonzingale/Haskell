e41f2c206bfceb7a47808841089640a0d58ec17a
Jon Zingale <jonzingale@gmail.com>
2018-02-22 09:49:47 -0700

    added Bit type for Vector

---
+++ HaskellStudy/LinearAlgebra/ComplexVector/Bit.hs
@@ -0,0 +1,47 @@
+
+{-# OPTIONS_GHC -Wno-missing-methods #-} -- Floating and Fractional
+
+module Bit where
+import Complex
+import Vector
+
+data Bit = One | Zero | Bad deriving (Show, Eq)
+
+class ZTwo m where
+  flip :: m -> m
+  val :: m -> Integer
+
+instance ZTwo Bit where
+  flip Zero = One
+  flip One = Zero
+  val Zero = 0
+  val One = 1
+
+instance Num Bit where
+  (+) a Zero = a
+  (+) Zero a = a
+  (+) One One = Zero
+  (*) a Zero = Zero
+  (*) Zero a = Zero
+  (*) One One = One
+  negate Zero = One
+  negate One = Zero
+  signum Zero = 0
+  signum One = 1
+  fromInteger 0 = Zero
+  fromInteger 1 = One
+  abs = id
+
+instance Floating Bit where
+  sqrt a = a
+
+instance Fractional Bit where
+  (/) _ Zero = Bad
+  (/) _ One = One
+
+instance Comp Bit where
+  conj = id
+  incl x | x == 0 = Zero
+         | otherwise = One
+
+bitVector = V3 One Zero One
