1e4a2a11e720c738c3f3869e49aa1fdbeb90667b
Jon Zingale <jonzingale@gmail.com>
2018-02-25 15:07:16 -0700

    renamed ComplexMatrix to Matrix, defined transpose and incl

--- HaskellStudy/LinearAlgebra/ComplexVector/Matrix.hs
+++ HaskellStudy/LinearAlgebra/ComplexVector/Matrix.hs
@@ -1,9 +1,9 @@
+
 {-# LANGUAGE MultiParamTypeClasses #-} -- for matrix dependent on vector
 {-# LANGUAGE FunctionalDependencies #-} -- for matrix dependent on vector
-
 {-# OPTIONS_GHC -Wno-missing-methods #-} -- for Num and Vector ThreeMatrix
 
-module ComplexMatrix where
+module Matrix where
 import Complex
 import Vector
 import Bit
@@ -20,25 +20,25 @@ instance Show a => Show (ThreeMatrix a) where
   show (M3 a b c) = (unlines.map show) [a, b, c]
 
 -- Maybe extend ThreeMatrix to Vector
--- eval, norm, <|>, projections.
+-- eval, norm, <|>, prs.
 instance Vector ThreeMatrix where
-  projections (M3 a b c) = [a, b, c]
+  prs (M3 a b c) = [a, b, c]
+  -- (<|>) (M3 a b c) (V3 x y z) = fmap (<|> (incl (V3 x y z))) (M3 a b c) -- bad typing
 
 instance Comp a => Comp (ThreeMatrix a) where
   conj = fmap conj
 
 class Matrix m v | v -> m where
-  transpose :: Comp v => m v -> m v
+  tr :: Comp v => m v -> m v -- Hermitian transpose
+  incl :: v -> m v
 
-instance Matrix ThreeMatrix (ThreeVector a) where
-  transpose (M3 x y z) = 
-    let [a, b, c] = projections x in
-    let [d, e, f] = projections y in
-    let [g, h, i] = projections z in
-    M3 (V3 a d g) (V3 b e h) (V3 c f i)
+instance (Num a) => Matrix ThreeMatrix (ThreeVector a) where
+  tr m = let [a, b, c, d, e, f, g, h, i] = foldr (++) [] $ prs.fmap prs $ m in
+    conj $ M3 (V3 a d g) (V3 b e h) (V3 c f i)
 
+  incl (V3 a b c) = M3 (V3 a 0 0) (V3 0 b 0) (V3 0 c 0)
 
--- instance (Floating a, Num a, Comp a) => Num (Mtrx a) where
+-- instance (Floating a, Num a, Comp a) => Num (ThreeMatrix a) where
   -- (+) (M a b c) (M x y z) = M (a+x) (b+y) (c+z)
   -- (-) (M a b c) (M x y z) = M (a-x) (b-y) (c-z)
   -- (*) (CM a b c) (CM x y z) = -- (AB)* = (A*)(B*)
@@ -53,6 +53,10 @@ bb = M3 (V3 Zero One Zero)
         (V3 One Zero One)
         (V3 One One Zero)
 
+rr :: ThreeMatrix (ThreeVector Double)
+rr = M3 (V3 1 2 3)
+        (V3 4 5 6)
+        (V3 7 8 8)
 {--
 DEVELOPED FROM PYTHON EXAMPLE
 import numpy as np
