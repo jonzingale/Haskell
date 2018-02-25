2ca629fd7129de833f2aa98ed1eee0edaf711a5d
Jon Zingale <jonzingale@gmail.com>
2018-02-25 16:27:00 -0700

    stubbed out for future code

---
+++ HaskellStudy/LinearAlgebra/ComplexVector/EigenVector.hs
@@ -0,0 +1,32 @@
+module EigenVector where
+import Matrix
+import Vector
+import Complex
+
+powerIteration :: ThreeMatrix a -> ThreeMatrix a
+powerIteration matrx = matrx
+
+{--
+DEVELOPED FROM PYTHON EXAMPLE
+import numpy as np
+
+def power_iteration(A, num_simulations):
+    # Ideally choose a random vector
+    # To decrease the chance that our vector
+    # Is orthogonal to the eigenvector
+    b_k = np.random.rand(A.shape[0])
+
+    for _ in range(num_simulations):
+        # calculate the matrix-by-vector product Ab
+        b_k1 = np.dot(A, b_k)
+
+        # calculate the norm
+        b_k1_norm = np.linalg.norm(b_k1)
+
+        # re normalize the vector
+        b_k = b_k1 / b_k1_norm
+
+    return b_k
+
+power_iteration(np.array([[0.5, 0.5], [0.2, 0.8]]), 10)
+--}
