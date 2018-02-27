cdb688a5b801dcbea62bfb50a3c7b4b46c3c1718
Jon Zingale <jonzingale@gmail.com>
2018-02-27 11:23:16 -0700

    fleshing out Num class for Vector

--- HaskellStudy/LinearAlgebra/DataVector.hs
+++ HaskellStudy/LinearAlgebra/DataVector.hs
@@ -1,12 +1,19 @@
+{-# OPTIONS_GHC -Wno-missing-methods #-} -- because of signum, fromInteger, *
+
 module DataVector where
 import qualified Data.VectorSpace as VS
 import Data.Vector
+import Data.Complex
 
 ten, sen :: Vector Integer
 ten = generate 10 (fromIntegral.(+1))
 sen = fromList [1..10]
 
+binOp :: (a -> a -> a) -> Vector a -> Vector a -> Vector a
+binOp bin a b = fromList $ Prelude.zipWith bin (toList a) (toList b)
+
 instance Num a => Num (Vector a) where
-  (+) v w = fromList $ Prelude.zipWith (+) (toList v) (toList w)
-  (*) v w = fromList $ Prelude.zipWith (*) (toList v) (toList w)
-  (-) v w = fromList $ Prelude.zipWith (-) (toList v) (toList w)
+  (+) v w = binOp (+) v w
+  -- (*) v w = binOp (*) v w
+  (-) v w = binOp (-) v w
+  abs v = v
