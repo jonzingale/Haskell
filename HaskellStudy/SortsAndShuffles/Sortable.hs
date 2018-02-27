b26ac32faffa52e8bddbc4b07d796ab96d27ed56
Jon Zingale <jonzingale@gmail.com>
2018-02-27 10:44:16 -0700

    SortsAndShuffles for HaskellStudy group

---
+++ HaskellStudy/SortsAndShuffles/Sortable.hs
@@ -0,0 +1,45 @@
+-- {-# OPTIONS_GHC -fno-warn-tabs #-}
+module Sortable where
+import Prelude hiding (head, (++), tail, length, filter)
+
+ex_1 :: Integer
+ex_1 = qsort 2937452435798
+
+ex_2 :: [Integer]
+ex_2 = qsort [2,9,3,7,4,5,2,4,3,5,7,9,8]
+
+instance Sortable Integer where
+  (++) n m = m + n * 10^(length m)
+  head n = mod n 10
+  tail n = div n 10
+  unit = 0
+
+instance (Ord a, Show a) => Sortable [a] where
+  (++) [] b = b
+  (++) (a:as) bs = a : (as ++ bs)
+  head (n:ns) = [n]
+  tail (n:ns) = ns
+  unit = []
+
+class (Ord s, Show s) => Sortable s where
+  filter :: (s -> Bool) -> s -> s
+  head, tail, qsort :: s -> s
+  (++) :: s -> s -> s
+  length :: s -> Int
+  unit :: s
+
+  length ns | ns == unit = 0
+            | otherwise = 1 + (length.tail) ns
+
+  filter b ns = f b ns unit
+    where
+      f b js accum | js == unit  = accum
+                   | (b.head) js = f b (tail js) $ head js ++ accum
+                   | otherwise   = f b (tail js) accum
+
+  qsort ns | ns == unit = unit
+           | otherwise = branch smaller ns ++ head ns ++ branch larger ns
+    where
+      branch f xs = qsort.f (head xs) $ tail xs
+      smaller n ns = filter (<= n) ns
+      larger n ns  = filter (>  n) ns
---
+++ HaskellStudy/SortsAndShuffles/SortableTree.hs
@@ -0,0 +1,57 @@
+-- {-# OPTIONS_GHC -fno-warn-tabs #-}
+module SortableTrees where
+import Prelude hiding (head, (++), tail, length, filter)
+
+t1 = Fork (Leaf 4) (Leaf 1)
+t2 = Fork (Leaf 3) t1
+t3 = t1 ++ t2
+t4 = t2 ++ t1
+
+data Btree a = Leaf a | Fork (Btree a) (Btree a) | Empty
+  deriving (Show , Eq)
+
+instance Functor Btree where
+  fmap f (Leaf n) = Leaf (f n)
+  fmap f (Fork l r) = Fork (fmap f l) (fmap f r)
+
+instance (Ord a, Show a) => Ord (Btree a) where
+  (<=) Empty t = True
+  (<=) t Empty = t == Empty
+  (<=) (Leaf a) (Leaf b) = a <= b
+  (<=) (Leaf a) (Fork l r) = (Leaf a) <= l
+  (<=) (Fork l r) (Fork s t) = l <= s
+
+instance (Ord a, Show a) => Sortable (Btree a) where
+  (++) Empty tree = tree
+  (++) tree Empty = tree
+  (++) left right = Fork left right
+  tail Empty = Empty
+  tail (Leaf a) = Empty
+  tail (Fork l r) = r
+  head Empty = Empty
+  head (Leaf a) = Leaf a
+  head (Fork l r) = l
+  unit = Empty
+
+class (Ord s, Show s) => Sortable s where
+  filter :: (s -> Bool) -> s -> s
+  head, tail, qsort :: s -> s
+  (++) :: s -> s -> s
+  length :: s -> Int
+  unit :: s
+
+  length ns | ns == unit = 0
+            | otherwise = 1 + (length.tail) ns
+
+  filter bool ns = f bool ns unit
+    where
+      f bool js accum | js == unit = accum
+                      | (bool.head) js = f bool (tail js) $  head js ++ accum
+                      | otherwise = f bool (tail js) accum
+
+  qsort ns | ns == unit = unit
+           | otherwise = branch smaller ns ++ head ns ++ branch larger ns
+    where
+      branch f xs = qsort.f (head xs) $ tail xs
+      smaller n ns = filter (<= n) ns
+      larger n ns  = filter (>  n) ns
