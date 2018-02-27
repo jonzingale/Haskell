4718fd611896b3b4652bdf9021ae28644b36f74b
Jon Zingale <jonzingale@gmail.com>
2018-02-27 11:16:23 -0700

    using the VectorSpace and Vector native libraries

--- HaskellStudy/LinearAlgebra/DataVector.hs
+++ HaskellStudy/LinearAlgebra/DataVector.hs
@@ -1,4 +1,12 @@
 module DataVector where
-import qualified Data.Vector as V
+import qualified Data.VectorSpace as VS
+import Data.Vector
 
-ten = V.generate 10 (+1)
+ten, sen :: Vector Integer
+ten = generate 10 (fromIntegral.(+1))
+sen = fromList [1..10]
+
+instance Num a => Num (Vector a) where
+  (+) v w = fromList $ Prelude.zipWith (+) (toList v) (toList w)
+  (*) v w = fromList $ Prelude.zipWith (*) (toList v) (toList w)
+  (-) v w = fromList $ Prelude.zipWith (-) (toList v) (toList w)
