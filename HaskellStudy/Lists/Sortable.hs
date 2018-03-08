c69f60afe07890c5855297cc2034f712655a89d3
Jon Zingale <jonzingale@gmail.com>
2018-03-07 17:42:37 -0700

    added two variations of Fisher-Yates shuffle, with swap and without (Durstenfeld)

--- HaskellStudy/Lists/Sortable.hs
+++ HaskellStudy/Lists/Sortable.hs
@@ -1,4 +1,4 @@
-module Sortable (sort, shuffle) where
+module Sortable (qsort, keyShuffle, fyShuffle, knuffle) where
 import Control.Monad.Zip
 import System.Random
 import Listable
@@ -12,16 +12,32 @@ randos :: [Integer]
 randos = randomRs (0, 10^6) $ mkStdGen 32
 
 class (Ord s, Listable s) => Sortable s where
-  sort, shuffle :: s -> s
+  qsort, keyShuffle, fyShuffle, knuffle :: s -> s
 
-  sort ns | ns == unit = unit
-          | otherwise = branch smaller ns +++ headL ns +++ branch larger ns
+  qsort ns | ns == unit = unit
+           | otherwise = branch smaller ns +++ headL ns +++ branch larger ns
     where
-      branch f xs = sort.f (headL xs) . tailL $ xs
+      branch f xs = qsort.f (headL xs) . tailL $ xs
       smaller n = filterL (<= n)
       larger  n = filterL (>  n)
 
-  shuffle = eval . map snd . sort . zipS randos
+  fyShuffle xs = f xs randos (lengthL xs) -- OG Fisher-Yates no swap.
+    where
+      f x _ 0 = unit
+      f xs (r:rs) l = let i = mod r l in
+        xs!!!i `cons` f (g xs i) rs (l-1)
+      g xs i = dropL (i+1) xs +++ takeL i xs
+
+  knuffle xs = f xs randos (lengthL xs) -- Fisher-Yates with swap, Durstenfeld
+    where
+      f xs _ 0 = xs
+      f xs (r:rs) l = f (swap l (mod r l) xs) rs (l-1)
+      replace t j xs = dropL (j+1) xs +++ t +++ takeL j xs
+      swap i j xs = replace (xs!!!j) i $ replace (xs!!!i) j xs
+      -- replace and swap behave differently for lists than for Integer.
+      -- the source the problem is that Listable Integers 'work' backwards.
+
+  keyShuffle = eval . map snd . qsort . zipS randos
     where
       eval = foldr cons unit
       zipS [] s = []
@@ -30,4 +46,3 @@ class (Ord s, Listable s) => Sortable s where
 
 instance Sortable Integer where
 instance Ord a => Sortable [a] where
-
