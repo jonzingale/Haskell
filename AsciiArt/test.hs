03a4aef669ee40bbe3d78129216ae838f3642c56
Jon Zingale <jonzingale@gmail.com>
2018-03-20 11:26:20 -0600

    towards using the ascii library

---
+++ AsciiArt/test.hs
@@ -0,0 +1,19 @@
+module Test where
+import Graphics.Ascii.Haha.Plot
+import Graphics.Ascii.Haha.Geometry
+
+{--
+data Pixel Source
+
+Constructors : Pixel Char String
+
+Instances:
+Eq Pixel   
+Show Pixel
+
+orderPoint :: Ord t => Point t -> Point t -> Ordering
+list :: (Integral i, RealFrac u) => u -> Rect u -> Bitmap u p -> [(Point i, p)]
+string :: (Integral i, Show i) => Bool -> Rect i -> Point i -> String -> String -> [(Point i, Pixel)] -> String
+--}
+
+pxl = Pixel '2' "this"
