e07261ccb09ed851733352bba6677e912494922f
Jon Zingale <jonzingale@gmail.com>
2018-03-24 17:21:57 -0600

    restored file

--- Farey.hs
+++ Farey.hs
@@ -11,28 +11,3 @@ boost (n,((a,b):(c,d):terms)) | b + d == n =
                                 (a,b) : (a+c,b+d) : boost (n, (c,d):terms )
                               | otherwise =
                                 (a,b) : boost (n, (c,d):terms )
-
-euler71 = g 11 (5,12)  (10^6)
-  where
-    g k (a,b) n | k <= n = g (k+7) (a+3, b+7) n
-                | otherwise = a-3
-
-{--
-Finding from Ivan Niven's 'Diophantine Approximations' the theorem of Hurwitz, 
-it is clear that any n/d to the left of 3/7 must satisfy 3d−7n=1. 
-Looking at the term before 3/7 in each Farey sequence, I noticed that 
-starting from the 11th Farey sequence each numerator increases by 3 and each 
-denominator by 7, every 7 sequences.  More generally, any Farey number n/d to 
-the left of a/b can be produced inductively by considering b sequences at (x,y) 
-and then incrementing (x+a, y+b).
-
-Of course looking at it now, It would be much simpler for any (n,d)
-to let j=10^6/d and consider j ∗ n − 1.
-
-euler(n,d)=n*div(10^6,d)−1
---}
-
-leftFarey nth (n,d) = 
-  let num = n * div nth d - 1 in
-  let denom = div (1 + num * d) n in
-  (num, denom)
