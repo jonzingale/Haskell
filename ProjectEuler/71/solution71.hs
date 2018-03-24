65e0998ed664eaf2b6f1280b9c5c4756e669342e
Jon Zingale <jonzingale@gmail.com>
2018-03-24 15:47:24 -0600

    Farey sequences and euler71

--- ProjectEuler/71/solution71.hs
+++ ProjectEuler/71/solution71.hs
@@ -1,16 +1,23 @@
--- The nth farey sequence is a list of all fractions
--- between zero and with numerator at most n.
--- fractions magically appear in order and reduced. 
+module Solution where
+import Math.NumberTheory.Primes.Factorisation -- factorise
+import Math.NumberTheory.Primes -- primes
 
-farey(0) = []
-farey(1) = [(0,1),(1,1)]
-farey(n) = boost (n,farey(n-1))
+{--
+Consider the fraction, n/d, where n and d are positive integers.
+If n<d and HCF(n,d)=1, it is called a reduced proper fraction.
 
-boost (n,[(a,b)]) = [(a,b)]
-boost (n,((a,b):(c,d):terms)) | b + d == n = 
-                                (a,b) : (a+c,b+d) : boost (n, (c,d):terms )
-                              | otherwise =
-                                (a,b) : boost (n, (c,d):terms )
+If we list the set of reduced proper fractions for d ≤ 8
+ in ascending order of size, we get:
+
+1/8, 1/7, 1/6, 1/5, 1/4, 2/7, 1/3, 3/8, 2/5, 3/7, 
+1/2, 4/7, 3/5, 5/8, 2/3, 5/7, 3/4, 4/5, 5/6, 6/7, 7/8
+
+It can be seen that 2/5 is the fraction immediately to the left of 3/7.
+
+By listing the set of reduced proper fractions for d ≤ 1,000,000
+in ascending order of size, find the numerator of the fraction
+immediately to the left of 3/7.
+--}
 
 euler71 = g 11 (5,12)  (10^6)
   where
