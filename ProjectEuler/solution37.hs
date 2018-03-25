2a61d1ad2f924f9108e253dc6564ed3ca3e219ce
Jon Zingale <jonzingale@gmail.com>
2018-03-25 12:13:25 -0600

    added comments

--- ProjectEuler/solution37.hs
+++ ProjectEuler/solution37.hs
@@ -2,6 +2,18 @@ module Solution where
 import Data.Numbers.Primes -- isPrime, primes
 import Prelude hiding (head, tail, length, last)
 
+{--
+The number 3797 has an interesting property. Being prime itself,
+it is possible to continuously remove digits from left to right,
+and remain prime at each stage: 3797, 797, 97, and 7.
+Similarly we can work from right to left: 3797, 379, 37, and 3.
+
+Find the sum of the only eleven primes that are both truncatable
+from left to right and right to left.
+
+NOTE: 2, 3, 5, and 7 are not considered to be truncatable primes.
+--}
+
 class (Ord s, Show s) => Sortable s where
   head, tail :: s -> s
   length :: s -> Int
