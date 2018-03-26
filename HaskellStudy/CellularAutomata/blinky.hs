f1c21bd0b19908965cf47fab9d6f2ba265056a14
Jon Zingale <jonzingale@gmail.com>
2018-03-26 16:03:50 -0600

    cellular automata

---
+++ HaskellStudy/CellularAutomata/blinky.hs
@@ -0,0 +1,38 @@
+module Blinky where
+import System.Random
+
+type Seed = Int
+data Board = B [Bool]
+
+instance Show Board where
+  show (B []) = ""
+  show (B (b:bs)) | b = "*" ++ show (B bs)
+                  | otherwise = " " ++ show (B bs)
+
+randomState :: Int -> Seed -> [Bool]
+randomState len = (take len).randomRs(False,True).mkStdGen
+
+board n = B $ randomState 80 n
+
+rule n ls | n == 0 = True
+          | n == length ls - 1 = True
+          | otherwise = blink n ls
+  where
+    blink n ls = case neighborhood n ls of
+      [False,False,False] -> False
+      [False,True,False] -> False
+      [False,True,True] -> True
+      [True,True,True] -> False
+      [True,False,True] -> False
+
+      [True,False,False] -> True
+      [True,True,False] -> True
+      [False,False,True] -> True
+
+neighborhood n ls = ls!!(n-1) : ls!!n : ls!!(n+1) : []
+
+blinkOnce (B bs) = B [rule i bs | i<-[0..length bs - 1]]
+
+main = do
+  let boards = iterate blinkOnce $ board 22
+  (putStr.unlines.map show) $ boards
