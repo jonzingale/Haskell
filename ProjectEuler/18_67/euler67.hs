4183fdaf8cfc341f9c0378cd89c21612be73189d
Jon Zingale <jonzingale@gmail.com>
2018-03-27 11:31:34 -0600

    needs better optimizations for euler67

--- ProjectEuler/18_67/euler67.hs
+++ ProjectEuler/18_67/euler67.hs
@@ -16,26 +16,29 @@ top to bottom is 23.
 8 5 9 3
 
 That is, 3 + 7 + 4 + 9 = 23.
---}
 
-csvParser file = 
-  let options = defaultDecodeOptions { decDelimiter = 32 } in
-  let parsedCsv = decodeWith options NoHeader file :: Either String (Vector [Int]) in
-  toList.fromRight empty $ parsedCsv
+k(15) => 0.04 secs
+k(20) => 0.95 secs
+k(25) => 32.13 secs
+--}
+type EitherInt = Either String (Vector [Int])
+data Tree = Empty | L Int | N Int Tree Tree
+
+instance (Num a, Num b) => Num (a,b) where
+  (+) (a,b) (c,d) = (a+c, b+d)
 
 main = do
   csv <- BL.readFile "triangle.csv"
   let maxedTree = buildAndMaxTree.csvParser $ csv
   print maxedTree
 
--- Still eats too much memory
-data Tree = Empty | L Int | N Int Tree Tree deriving (Show)
+csvParser file = -- <--- regulated for further testing.
+  let options = defaultDecodeOptions { decDelimiter = 32 } in
+  let parsedCsv = decodeWith options NoHeader file :: EitherInt in
+  take 20 $ toList.fromRight empty $ parsedCsv
 
 buildAndMaxTree tower = let ([aj]:paired) = map (zip [0..]) tower in
   f aj paired
   where
     f (j,a) [] = [a]
     f (j,a) (b:bs) = max (f ((0,a) + b!!j) bs) (f ((0,a) + b!!(j+1)) bs)
-
-instance (Num a, Num b) => Num (a,b) where
-  (+) (a,b) (c,d) = (a+c, b+d)
