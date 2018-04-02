7fee3112bdf38189ca88923eed1ed09ae9032d43
Jon Zingale <jonzingale@gmail.com>
2018-04-02 13:58:56 -0600

    towards raytracing

--- Attenuations/RayTracer.hs
+++ Attenuations/RayTracer.hs
@@ -13,8 +13,6 @@ Actually, order by length is the way to go.
 * order by lengths
 --}
 
--- (n, d) = (4.0, 3.0)
-
 xs (n,d) = zip [ norm.xcept n k $ d | k <- [1..40]] $ repeat "x"
   where
     fi = fromInteger
@@ -30,8 +28,21 @@ themGuys (n,d) = foldr (++) "" $ map snd $ sort (xs (n,d) ++ys (n,d))
 norm :: (Double, Double) -> Double
 norm (x,y) = sqrt (x^2 + y^2)
 
-pMap f pairs = [(p, f p) | p<-pairs]
-
 main = do
+  let pMap f pairs = [(p, f p) | p<-pairs]
   let them = pMap themGuys randPairs
   (putStr.unlines.map show) them 
+
+-----------
+
+ex1419 = "xyxyxxyxyxyxxyxyxyxxyxyxyxxy"
+
+
+maxMin (n,d) =
+  let xs = [mod (14*k) 19 | k<-[1..10]] in
+  let ys = [mod (19*k) 14 | k<-[1..10]] in
+  let zs = zip xs ys in
+  foldr (++) "" $ map f zs
+  where
+    f (x,y) | x >= y = "x"
+            | otherwise = "Y" 
