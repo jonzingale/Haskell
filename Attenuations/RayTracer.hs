06815d7ecd34f19cd366380529f0e8ce1823eabd
Jon Zingale <jonzingale@gmail.com>
2018-04-03 11:52:24 -0600

    fastest rabbit for now

--- Attenuations/RayTracer.hs
+++ Attenuations/RayTracer.hs
@@ -12,18 +12,18 @@ Actually, order by length is the way to go.
 * calcuate lengths
 * order by lengths
 --}
--- rewrite this for Integers, ACCURACY!
+-- rewrite as Integers, ACCURACY!
 xs (n,d) = zip [ norm.xcept n k $ d | k <- [1..40]] $ repeat "x"
   where
     fi = fromInteger
     xcept n k d = (fi k, (n*(fi k))/ d)
 
-ys (n,d) = zip [ norm.ycept n k $ d| k <- [1..40]] $ repeat "y"
+ys (n,d) = zip [ norm.ycept n k $ d | k <- [1..40]] $ repeat "y"
   where
     fi = fromInteger
     ycept n k d = ((d*(fi k))/ n, fi k)
 
-themGuys (n,d) = foldr (++) "" $ map snd $ sort (xs (n,d) ++ys (n,d))
+themGuys (n,d) = foldr (++) "" $ map snd $ sort (xs (n,d) ++ ys (n,d))
 
 norm :: (Double, Double) -> Double
 norm (x,y) = sqrt (x^2 + y^2)
@@ -33,22 +33,19 @@ main = do
   let them = pMap themGuys randPairs
   (putStr.unlines.map show) them 
 
------------
 {--
 Rythmic technique.
 Sarah points out that one only needs compute to
 the half-way point and then mirror.
 --}
 
-rabbits (n,d) = '.' : f n d 1 1
+-- length $ take (10^6) $ rabbits (123,101)
+-- (1.14 secs, 724,168,432 bytes)
+rabbits (n,d) = f n d 0 0
   where
-    f n d i j | n*i < d*j = 'x' : f n d (i+1) j
-              | n*i > d*j = 'Y' : f n d i (j+1)
-              | otherwise = '.' : f n d (i+1) (j+1)
-
-
-
-
+    f n d i j | n*i < d*j = 'L' : f n d (i+1) j
+              | n*i > d*j = 'r' : f n d i (j+1)
+              | otherwise = '.' : f n d 1 1
 
 
 
