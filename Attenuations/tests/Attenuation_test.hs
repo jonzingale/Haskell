0797cc52f8eddde32fbf8e2993140fd2bff015b4
Jon Zingale <jonzingale@gmail.com>
2018-04-04 23:39:36 -0600

    renamings

--- Attenuations/tests/Attenuation_test.hs
+++ Attenuations/tests/Attenuation_test.hs
@@ -7,15 +7,14 @@ import Attenuation
 
 
 -- rayLength
-test_rayLengths_equality :: IO ()
-test_rayLengths_equality = do 
+test_rayLength_equality = do 
   let (point, slope) = ((5/14, 0), (14,19))
   let testRL  = rayLength  point (toAngleRad slope)
   let testRL' = rayLength' point slope
   assertEqual testRL testRL'
 
 -- toAngleDeg
-prop_radiansToDeg :: Slope -> Bool
-prop_radiansToDeg (n,d) = toAngleDeg (n,d) == toAngleRad (n,d) * 180 / pi
+prop_toAngleDeg :: Slope -> Bool
+prop_toAngleDeg (n,d) = toAngleDeg (n,d) == toAngleRad (n,d) * 180 / pi
 
 main = htfMain htf_thisModulesTests
