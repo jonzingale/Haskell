c6f1c046fec98571a8814050e02f2294d23bf290
Jon Zingale <jonzingale@gmail.com>
2018-04-08 19:47:12 -0600

    fixup

--- Attenuations/Tests/SymmetryTests.hs
+++ Attenuations/Tests/SymmetryTests.hs
@@ -20,26 +20,20 @@ prop_ComposeAssoc f g h x =
 tol :: Double -> Integer
 tol d = round $ d * 10^12
 
--- TODO: figure out replay args.
--- replayArg = "Just (TFGenR 15067B55359906C0776B9C0A73ACEE7D9C124B4AE3DAC3AFCB451E04B1EF7BD1 0 31 5 0,28)"
--- pt = ((0.0, 0.0), -63.8735667731305)
--- prop_rot4IdReplay =
---   withQCArgs (\a -> a { replay = read replayArg })
---   prop_rot4Id
-
 -- Rotation Tests
 prop_rot90 = do
-  x <- choose (0, pi)
-  return $ (tol.cos) (pi/2 - x) == (tol.sin) x
+  θ <- choose (0, pi)
+  return $ (tol.cos) (pi/2 - θ) == (tol.sin) θ
 
-prop_rot4Id x y t =
-  let coords = ((x, y),t) in
-  let rotFour = foldr (.) id [rot90 | x<-[1..4]] in
-  (mtol.divPi.rotFour) coords == mtol coords
+prop_rot4Id x y = do
+  t <- choose (0, 2*pi)
+  let coords = ((x, y),t)
+  let rotFour = foldr (.) id [rot90 | x<-[1..4]]
+  return $ (mtol.divPi.rotFour) coords == mtol coords
   where
     divPi ((x, y),theta) = ((x, y), theta - (2*pi))
     mtol ((x,y), t) = ((ttol x, ttol y), ttol t)
-    ttol d = round $ d * 10^9 -- not very good accuracy.
+    ttol d = round $ d * 10^12 -- not very good accuracy.
 
 prop_rotInv' x y t = let coords = ((x, y),t) in
   (mtol.rot90.rot270) coords == mtol coords
