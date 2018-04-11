3fd1e0f32c104e9afd36a1d4040953bf7959a4ee
Jon Zingale <jonzingale@gmail.com>
2018-04-10 18:25:42 -0600

    εδ labelling

--- Attenuations/Tests/ExplicitGenerators.hs
+++ Attenuations/Tests/ExplicitGenerators.hs
@@ -9,8 +9,11 @@ interval = choose (0, 1::Double)
 zeroToPi = choose (0, pi::Double)
 zeroToTau =choose (0, tau::Double)
 
+{--
+  δ
+ε_\|
+--}
 epsilonRegion x = choose (3*pi/4 + x*pi/4, pi) -- super-εδ-Condition
-
 deltaRegion x = choose (pi/2, pi/2 + x*pi/4) -- sub-εδ-Condition
 
 
