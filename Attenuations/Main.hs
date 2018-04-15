007ed03a757fe18b44380b8779adcf5a1aa43420
Jon Zingale <jonzingale@gmail.com>
2018-04-15 11:58:22 -0600

    notes for future progress

--- Attenuations/Main.hs
+++ Attenuations/Main.hs
@@ -6,11 +6,19 @@ import RayTracer.Rhythm
 arraySize = 5
 array = [[1..5], [6..11], [15..20], [21..26], [31..36]]
 
+
+{--
+rayLength needs to change. How can I be sure that I am
+passing legitimate values (x,0) or (y,0). Further,
+calculating tan θ once is all that is necessary and
+would greatly simplify the code.
+--}
+
 transport cs θ = f cs 0 0 θ 0
   where
     retCond x y j k t = x + j*tan t >= arraySize || y + k*tan t >= arraySize
 
-    g (a,b) (r,s) th = lookUp array (r,s) * rayLength (a,b) th
+    g (a,b) (r,s) th = lookUp array (r,s) * rayLength (a,b) th -- see note above
 
     f (x, y) j k θ accum | retCond x y j k θ = accum
                          | x + j*tan θ <= y + k*tan θ =
