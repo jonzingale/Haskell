44d41367effa4e5d106496d1d4229f99d3814e38
Jon Zingale <jonzingale@gmail.com>
2018-04-15 11:39:55 -0600

    towards main for raytracing

--- Attenuations/Main.hs
+++ Attenuations/Main.hs
@@ -3,9 +3,27 @@ import RayTracer.RayLength
 import RayTracer.Lattice
 import RayTracer.Rhythm
 
--- At some point create individual Test directory
--- and run tests through here somehow.
+arraySize = 5
+array = [[1..5], [6..11], [15..20], [21..26], [31..36]]
+
+transport cs θ = f cs 0 0 θ 0
+  where
+    retCond x y j k t = x + j*tan t >= arraySize || y + k*tan t >= arraySize
+
+    g (a,b) (r,s) th = lookUp array (r,s) * rayLength (a,b) th
+
+    f (x, y) j k θ accum | retCond x y j k θ = accum
+                         | x + j*tan θ <= y + k*tan θ =
+                            f (x,y) (j+1) k θ (accum + g (x,y) (j,k) θ)
+                         | otherwise =
+                            f (x,y) j (k+1) θ (accum + g (x,y) (j,k) θ)
+
+-- Eventually this will be either Array
+-- or Accelerate and likely not [[a]] but [a].
+lookUp :: [[Double]] -> (Double, Double) -> Double
+lookUp as (ii, jj) = let (i, j) = (floor ii, floor jj) in (as!!i)!!j
+
+-- testCase : transport (12.3, 0) (pi/4)
 
 main = do
-  let len = rayLength (2/5,0) (pi/4)
-  print len
+  print "awesomeness"
