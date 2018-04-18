aba001c3b9fec6618ffb8a3cab772f35a71da997
Jon Zingale <jonzingale@gmail.com>
2018-04-17 23:21:18 -0600

    a much simplified 622

--- ProjectEuler/622/622Small.hs
+++ ProjectEuler/622/622Small.hs
@@ -5,25 +5,18 @@ import Data.Bits
 
 import Data.List
 
--- main = do print var622
--- 19084065 lcm for factors of 2^60-1 less than 60
+main = do print var622
 
--- var622 :: Integer -- 32767 == 2^15-1
--- var622 = sum.remSort $ map ((+ 1).eval) goodBits
--- var622 = remSort $ map ((+ 1).eval) goodBits
+var622 :: Integer
+var622 = sum.goodBits $ 60
 
--- sum [152,218,1058,4682] -- the 15 case
--- 3010983666182123972
-
--- mapping = [3,3,5,5,7,11,13,31,41,61,151,331,1321] -- duplicate factors
--- mapping = [7,31,151] -- 2^15-1
--- mapping = [3,5,17] -- 2^8-1
-
-goodBits :: [Integer]
-goodBits = [ n + 1 | n <- listDivisors (2^8-1), bitCond n]--  && n /= llcm]
+goodBits :: Integer -> [Integer]
+goodBits n = [ k + 1 | k <- listDivisors (2^n-1), divCond n k]
   where
-    llcm = 15 --19084065
-    bitCond k = mod(div k llcm) llcm /= 0 && k > 8
+    divCond n k = allTrue [ mod (2^t-1) k /= 0 | t <- midFacts n]
+    midFacts = tail.reverse.listDivisors
+    allTrue = foldr (&&) True
+
 
 -- goodBits :: [Integer]
 -- goodBits = [n | n<-[1..2^3-1], bitCond n] -- 3 because 3 unique factors
