df9b32a04387579d8dc596cf47c32bb68ebb61e2
Jon Zingale <jonzingale@gmail.com>
2018-04-18 10:15:05 -0600

    midDivisors

--- ProjectEuler/622/FactorisationHelpers.hs
+++ ProjectEuler/622/FactorisationHelpers.hs
@@ -2,9 +2,17 @@ module FactorisationHelpers where
 import Math.NumberTheory.Primes.Factorisation -- divisors
 import Data.Set (singleton, Set, mapMonotonic, unions, toList)
 
+-- just give proper divisors
+midDivisors :: Integer -> [Integer]
+midDivisors n = f.tail.toList.divisors $ n
+  where
+    f [x] = []
+    f (x:xs) = x:f xs
+
 listDivisors :: Integer -> [Integer]
 listDivisors = tail.toList.divisors
 
+-- modified to just give proper divisors
 divisors :: Integer -> Set Integer
 divisors n
     | n < 0     = divisors (-n)
@@ -15,4 +23,5 @@ divisors n
 divisorsFromCanonical :: [(Integer,Int)] -> Set Integer
 divisorsFromCanonical = foldl step (singleton 1)
   where
-    step st (p,k) = unions (st:[mapMonotonic (*pp) st | pp <- take k (iterate (*p) p) ])
+    step st (p,k) = unions (st:[mapMonotonic (*pp) st | 
+                            pp <- take k (iterate (*p) p) ])
