171979c61ce94e0dd2e95e96ce9b81ea9e76bd26
Jon Zingale <jonzingale@gmail.com>
2018-04-23 22:32:29 -0600

    readDouble :: Bytestring -> Double

---
+++ Attenuations/RayTracer/ReadDouble.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE ForeignFunctionInterface #-}
+
+module RayTracer.ReadDouble where
+import qualified Data.ByteString.Lazy.Char8 as LB
+import qualified Data.ByteString as SB
+import Data.ByteString.Internal (inlinePerformIO)
+import Foreign.C.String (CString)
+import Foreign.C (CDouble)
+import Data.Maybe (fromJust)
+
+foreign import ccall unsafe "stdlib.h atof" c_atof :: CString -> IO Double
+unsafeReadDouble = inlinePerformIO . flip SB.useAsCString c_atof
+
+readDouble = unsafeReadDouble . SB.concat . LB.toChunks
+readInt = fst . fromJust . LB.readInt
