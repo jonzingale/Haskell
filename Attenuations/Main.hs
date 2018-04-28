b8a2e6d688c2475c2754fc0136880a8ea07bb86a
Jon Zingale <jonzingale@gmail.com>
2018-04-27 19:35:24 -0600

    toward a main for raytracing

--- Attenuations/Main.hs
+++ Attenuations/Main.hs
@@ -1,8 +1,16 @@
 module Main where
-import RayTracer.RayLength
-import RayTracer.Lattice
-import RayTracer.Rhythm
+import RayTracer.Transport
+import RayTracer.FileToVector
 
+{--
+Todo:
+* Simulate 1M rays from gaussian point source
+* Parallelize code
+* Monadic Run?
+* Tests: Verify / Validate
+* Is Strictness on FileToVector valuable?
+--}
 
 main = do
-  print "awesomeness"
+  myArray <- anArray
+  return $ totalAttenuation (pi/4) myArray -- error on pi/2
