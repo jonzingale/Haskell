9471290a5ce5d18021f1c9110392e4e0d07e8807
Jon Zingale <jonzingale@gmail.com>
2018-04-29 22:12:50 -0600

    needs rethinking, see TransportSketch

--- Attenuations/RayTracer/Transport.hs
+++ Attenuations/RayTracer/Transport.hs
@@ -15,29 +15,48 @@ NOTES:
  * tan only really works here because I am considering θ <= pi/2. NEEDS GENERALIZED.
  * There WILL be double counting if the points are on the diagonal.
 --}
+arraySize = 4
+testRayLength = totalRayLength (8/9) (pi/3)
 
-(x,y,theta) = (8/9, 0, pi/3)
-testRayLength = totalRayLength (8/9) (pi/3) -- 4.2222222222222205
+(x, theta, phi) = (8/9, pi/3, pi/2)
+yval = (1-fractional x) * tan theta
+ycrossings = takeWhile ((< arraySize).norm) [ (k, yval + k * tan theta) | k <- [0..]]
+xcrossings = takeWhile ((< arraySize).norm) [ (x + k / tan theta, k) | k <- [0..]]
+
+norm :: (Double, Double) -> Double
+norm (x,y) = sqrt $ x*x + y*y
+
+walkD _ [] _ = []
+walkD [] _ _ = []
+walkD (x:xs) (y:ys) theta
+  | theta == pi/2 || theta == 0 = [] -- to cover asymptotics
+  | x > arraySize || y > arraySize = []
+  | x < y = x : walkD xs (y:ys) theta
+  | otherwise = y : walkD (x:xs) ys theta
+  -- | x < y = rayLength (fractional x, 0) theta : walkD xs (y:ys) theta
+  -- | otherwise = rayLength (0, fractional y) theta : walkD (x:xs) ys theta
+
+-- (x,y,theta) = (8/9, 0, pi/3) -- 4.2222222222222205
+-- (x,y,theta) = (0, 0, 1*pi/8) -- 6.308644059797899
+-- (x,y,theta) = (0, 0, 3*pi/8) -- 6.308644059797899
+
 
 fractional :: Double -> Double
 fractional = snd.properFraction
 
--- 𝜆> [rayLength (fractional x,0) (pi/3)|x<-xs]
--- [0.22222222222222227,1.0675216838429702,1.1547005383792517,0.7581206070844659]
--- 𝜆> [rayLength (0,fractional y) (pi/3)|y<-ys]
--- [0.9324783161570293,8.71788545362814e-2,0.39657993129478536,0.7059810080532891]
-
-totalRayLength x theta =  
-  let yval = (1-fractional x) * tan theta in -- calculate 1 time.
+totalRayLength x theta =
+  let yval = (1 - fractional x) * tan theta in -- calculate 1 time.
   let ycrossings = [ yval + k * tan theta | k <- [0..]] in -- y @ xn
   let xcrossings = [ x + k / tan theta | k <- [0..]] in -- x @ yn
   walk xcrossings ycrossings theta
 
   where
+    -- normWhile = takeWhile ((< arraySize).norm)
     walk _ [] _ = 0
     walk [] _ _ = 0
     walk (x:xs) (y:ys) theta
-      | x > 3 || y > 4 = 0
+      | theta == pi/2 || theta == 0 = arraySize -- to cover asymptotics
+      | x > arraySize-1 || y > arraySize = 0
       | x < y = rayLength (fractional x, 0) theta + walk xs (y:ys) theta
       | otherwise = rayLength (0, fractional y) theta + walk (x:xs) ys theta
 
