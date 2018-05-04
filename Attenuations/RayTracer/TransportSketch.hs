0b330017827787931085195bad7ff54470434f0d
Jon Zingale <jonzingale@gmail.com>
2018-05-04 12:18:27 -0600

    toward preprocessing limits

--- Attenuations/RayTracer/TransportSketch.hs
+++ Attenuations/RayTracer/TransportSketch.hs
@@ -1,35 +1,48 @@
-module RayTracer.TransportSketch  where
+module RayTracer.TransportSketch where
+import RayTracer.RegionDetection
 import RayTracer.FileToVector
 import RayTracer.RayLength
+
+-- data Region = LeftSide | Top | RightSide | Bad deriving (Eq, Show)
 
 fractional :: Double -> Double
 fractional = snd.properFraction
 
-arraySize = 3.1 -- <--- another error prone spot.
--- (x, theta, phi) = (8/9, pi/3, pi/2)
--- testRayLength = totalRayLength (8/9) (pi/3)
+arraySize = 3 -- <--- another error prone spot.
 
 -- length diffs
-tourists = take 2 $ zip (xcrossings 0 (pi/4)) (ycrossings  0 (pi/4))
+tourists x th = take 4 $ zip (xcrossings x th) (ycrossings  x th)
 
+-- Exit point is arraySize dependent.
+preprocessLims x theta size =
+  let exitReg = exitRegion x theta size in
+  vals exitReg
+  where
+    vals LeftSide  = 0 -- x < 0
+    vals Top       = arraySize -- y > arraySize
+    vals RightSide = arraySize -- x > arraySize
+
+
+-- NEITHER CROSSINGS CAN HANDLE θ > pi/2 -- likely the conditionals
+-- use takeWhile or a preprocessed Limit instead
 -- ys vaues at integer x.
 xcrossings x theta =
   let ypt k = x + k / tan theta in
   let rLen k = sqrt $ k**2 + (ypt k - x)**2 in
-  [ (ypt k, rLen k) | k <- [0..], ypt k >= 0]
+  [ (ypt k, rLen k) | k <- [0..] ] --, ypt k >= 0]
 
 -- xs vaues at integer y.
-ycrossings x theta = 
+ycrossings x theta =
   let xpt k = (k-x) * tan theta in
   let rLen k = sqrt $ (k-x)**2 + (xpt k)**2 in
-  [ (xpt k, rLen k) | k <- [1..], xpt k >= 0]
+  [ (xpt k, rLen k) | k <- [1..]] --, xpt k >= 0]
 
 --- close to being pretty good.
 totalRayLength x theta =
   f (xcrossings x theta) (ycrossings x theta) arraySize 0
   where
     f ((y,r1):xcs) ((x,r2):ycs) s accum
-      | y > s || x > s = accum
+      | y > s || x > s || y < 0 || x < 0 = accum
       | r1 < r2 = f xcs ((x,r2):ycs) s r1
       | otherwise = f ((y,r1):xcs) ycs s r2
 
