146700c7adb138c193724425b5340c183ed288af
Jon Zingale <jonzingale@gmail.com>
2018-05-14 09:29:57 -0600

    simplified methods

--- Attenuations/RayTracer/FileToVector.hs
+++ Attenuations/RayTracer/FileToVector.hs
@@ -1,8 +1,6 @@
 
 {-# LANGUAGE BangPatterns #-}
-module RayTracer.FileToVector (qArray, qAry49, fileToAry, anArray,
-                               fortyNineDoubles, vLength, vSum
-                               ) where
+module RayTracer.FileToVector (qArray, fileToAry, vLength, vSum) where
 import qualified Data.ByteString.Lex.Fractional as L
 import qualified Data.ByteString.Char8 as L
 import qualified Data.Vector.Unboxed as U
@@ -19,8 +17,8 @@ $ time ./FileToVector
 
 -- The basic idea:
 main = do
-    !s <- L.readFile "./Tests/data1M" -- 1_000_000
-    print . U.sum . parse $ s
+  ary <- fileToAry "./Tests/data1M"
+  return $ qArray 1000 (20, 300) ary
 
 parse :: L.ByteString -> U.Vector Double
 parse = U.unfoldr step
@@ -29,34 +27,20 @@ parse = U.unfoldr step
         Nothing       -> Nothing
         Just (!k, !t) -> Just (k, L.tail t)
 
--- A run at a useful Parser
+type Dimension = Int -- a single dimension of lattice
+type Coords = (Int, Int)
+
+-- The following methods perhaps part of type class?
+fileToAry :: FilePath -> IO (U.Vector Double)
 fileToAry file = do
   !s <- L.readFile file
   return.parse $ s
 
-anArray = do
-  !s <- L.readFile "./Tests/data1M"
-  return.parse $ s
-
-fortyNineDoubles = do
-  !s <- L.readFile "./Tests/data49Doubles"
-  return.parse $ s
-
-type Coords = (Int, Int)
-type Dimension = Int -- a single dimension of lattice
+qArray :: U.Unbox a => Dimension -> Coords -> U.Vector a -> a
+qArray size (x, y) a = (U.!) a (x + y * size)
 
 vLength :: U.Unbox a => U.Vector a -> Int
 vLength = U.length
 
 vSum :: (Num b, U.Unbox b) => U.Vector b -> b
-vSum = U.foldr (+) 0
-
-qArray :: U.Unbox a => Dimension -> Coords -> U.Vector a -> a
-qArray size (x, y) a = (U.!) a (x + y * size)
-
-qAry49 :: U.Unbox a => Coords -> U.Vector a -> a
-qAry49 (x, y) a = (U.!) a (x + y * 7)
-
-testIndex = do
-  ary <- anArray
-  return $ qArray 1000 (20, 300) ary
+vSum = U.sum
