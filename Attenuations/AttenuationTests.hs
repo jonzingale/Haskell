13c07150a1d053ee5cee8ba832aff8ff9897e4a8
Jon Zingale <jonzingale@gmail.com>
2018-05-13 18:24:30 -0600

    restructuring tests for new methods

--- Attenuations/AttenuationTests.hs
+++ Attenuations/AttenuationTests.hs
@@ -5,18 +5,20 @@
 
 module AttenuationTests where
 import Tests.ExplicitGenerators
-import RayTracer.RayLength
+-- import RayTracer.RayLength
 import RayTracer.Transport
-import RayTracer.Lattice
-import RayTracer.Rhythm
+-- import RayTracer.Lattice
+-- import RayTracer.Rhythm
 import Test.Framework
 
-import {-@ HTF_TESTS @-} Tests.IndexerTests
-import {-@ HTF_TESTS @-} Tests.SymmetryTests
-import {-@ HTF_TESTS @-} Tests.XRegionTests
+import {-@ HTF_TESTS @-} Tests.TransportTests
+-- import {-@ HTF_TESTS @-} Tests.IndexerTests
+-- import {-@ HTF_TESTS @-} Tests.SymmetryTests
+-- import {-@ HTF_TESTS @-} Tests.XRegionTests
 
 main = htfMain htf_importedTests
 
-symmetryTests = htfMain htf_Tests_SymmetryTests_thisModulesTests
-indexerTests = htfMain htf_Tests_IndexerTests_thisModulesTests
-xRegionTests = htfMain htf_Tests_XRegionTests_thisModulesTests
+transportTests = htfMain htf_Tests_TransportTests_thisModulesTests
+-- symmetryTests = htfMain htf_Tests_SymmetryTests_thisModulesTests
+-- indexerTests = htfMain htf_Tests_IndexerTests_thisModulesTests
+-- xRegionTests = htfMain htf_Tests_XRegionTests_thisModulesTests
