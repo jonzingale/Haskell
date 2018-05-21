db3c2ea14be7e0218a9c74857d6cfd6c79e2c7ad
Jon Zingale <jonzingale@gmail.com>
2018-05-20 18:03:21 -0600

    added appropriate allOnes monadicIO property test

--- Attenuations/Tests/TransportTests.hs
+++ Attenuations/Tests/TransportTests.hs
@@ -6,7 +6,20 @@ module Tests.TransportTests where
 import Tests.ExplicitGenerators
 import RayTracer.FileToVector -- fileToAry, qArray, vLength, vSum
 import RayTracer.Transport
+
+import Test.QuickCheck.Monadic
 import Test.Framework
+
+prop_allOnesScalesArray :: TestCoords -> Property
+prop_allOnesScalesArray (C x θ) = monadicIO $ do
+  ary <- run allOnes
+  let (_:ijSeg) = transport x θ -- because the head is not necessary.
+  let cellEval = (* 7).snd.(!! 1) $ transport (x/7) θ -- (*7).(rl).(/7) == rl
+  let latticeEval = sum [ seg * (qArray 7 ij ary) | (ij, seg) <- takeWhile stopCond ijSeg]
+  assert $ (eBall 13) cellEval latticeEval
+  where
+    stopCond ((x,y), s) = x<7 && y<7
+
 
 allOnes = fileToAry "./Tests/dataTestAllOnes" -- 7x7
 fortyNineDoubles = fileToAry "./Tests/data49Doubles" -- 7x7
@@ -27,39 +40,4 @@ test_allOnesDiagonal = do
   assertEqual eval (7 * sqrt 2)
   where
     stopCond ((x,y), s) = x<7 && y<7
-
-prop_allOnesScalesArray = do
-  let ary = take 49 $ repeat 1 -- to avoid mixing monads, IO and Gen
-  θ <- zeroToHalfPi -- extend this how?
-  x <- interval
-  let (_:ijSeg) = transport x θ -- because the head is not necessary.
-  let cellEval = (* 7).snd.(!! 1) $ transport (x/7) θ -- (*7).(rl).(/7) == rl
-  let latticeEval = sum [ seg * (query ij ary) | (ij, seg) <- takeWhile stopCond ijSeg]
-  return $ (eBall 13) cellEval latticeEval
-  where
-    stopCond ((x,y), s) = x<7 && y<7
-    query (i,j) ary = (!!) ary (i + j * 7)
-
--- prop_allOnesScales :: Gen Bool
-test_allOnesScales = do
-  -- θ <- zeroToPi
-  -- x <- interval
-  let (x, θ) = (0, pi/4)
-  ary <- allOnes
-  let (_:ijSeg) = transport x θ -- because the head is not necessary.
-  let cellEval = (* 7).snd.(!! 1) $ transport (x/7) θ -- (*7).(rl).(/7) == rl
-  let latticeEval = sum [ seg * (qArray 7 ij ary) | (ij, seg) <- takeWhile stopCond ijSeg]
-  return $ (eBall 13) cellEval latticeEval
-  where
-    stopCond ((x,y), s) = x<7 && y<7
-
--- prop_reflectInv :: (Point, Angle) -> Bool
--- prop_reflectInv cs = (mtol.reflectY.reflectY) cs == mtol cs
-
--- prop_RhoIsReflectedRho' :: Gen Bool
--- prop_RhoIsReflectedRho' = do
---   y <- interval
---   t <- rhoRegion y
---   let (csR, tR) = reflectY ((0,y), t)
---   return $ (eBall 13) (rho (0,y) t) (rho' csR tR)
 
