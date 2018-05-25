096081f3cea4ea1e3b39cfbe574ca3f0493d67bf
Jon Zingale <jonzingale@gmail.com>
2018-05-25 13:44:41 -0600

    generalized saveArray, gradArray

--- Attenuations/RayTracer/DataWriter.hs
+++ Attenuations/RayTracer/DataWriter.hs
@@ -1,8 +1,23 @@
 import Data.Array.Unboxed -- strict fast Arrays
-import System.Random
+import System.Random -- randomRs
+import Data.List -- sort
 
-saveArr = writeFile "./Tests/data" $ unlines.map show $ elems bigArray
+randos = randomRs (0, 1::Double).mkStdGen $ 42
+
+-- saveArr "gradArray" gradArray
+saveArr :: String -> UArray Int Double -> IO()
+saveArr file ary = writeFile ("./Tests/" ++ file) $
+          unlines.map show $ elems ary
 
 bigArray :: UArray Int Double
-bigArray = listArray bounds $ randomRs (0, 1::Double).mkStdGen $ 42
+bigArray = listArray bounds randos
   where bounds = (0::Int, 10^6-1)
+
+gradArray :: UArray Int Double
+gradArray =
+  let grades = sort.take 7 $ randos in
+  let bounds = (1::Int, 49) in
+  let ary = foldr (++) [] $ map sevenOfEm grades in
+  listArray bounds ary
+  where
+    sevenOfEm = (take 7).repeat
