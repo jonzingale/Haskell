31fc0f311c98cde6c7977a106c742e5c365b5540
Jon Zingale <jonzingale@gmail.com>
2018-06-06 11:24:41 -0600

    qArray for 3D lattices

--- Attenuations/RayTracer/FileToVector.hs
+++ Attenuations/RayTracer/FileToVector.hs
@@ -18,7 +18,7 @@ $ time ./FileToVector
 -- The basic idea:
 main = do
   ary <- fileToAry "./Tests/data1M"
-  return $ qArray 1000 (20, 300) ary
+  return $ qArray 1000 (20, 300, 0) ary
 
 parse :: L.ByteString -> U.Vector Double
 parse = U.unfoldr step
@@ -28,7 +28,8 @@ parse = U.unfoldr step
         Just (!k, !t) -> Just (k, L.tail t)
 
 type Dimension = Int -- a single dimension of lattice
-type Coords = (Int, Int)
+type Coords = (Int, Int, Int)
+type Coords2D = (Int, Int)
 
 -- The following methods perhaps part of type class?
 fileToAry :: FilePath -> IO (U.Vector Double)
@@ -36,8 +37,12 @@ fileToAry file = do
   !s <- L.readFile file
   return.parse $ s
 
+qArray2D :: U.Unbox a => Dimension -> Coords2D -> U.Vector a -> a
+qArray2D size (x, y) a = (U.!) a (x + y * size)
+
+-- How should this work?
 qArray :: U.Unbox a => Dimension -> Coords -> U.Vector a -> a
-qArray size (x, y) a = (U.!) a (x + y * size)
+qArray size (x, y, z) a = (U.!) a (x + y * size)
 
 vLength :: U.Unbox a => U.Vector a -> Int
 vLength = U.length
