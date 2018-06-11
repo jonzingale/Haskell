bee414831381ff8177fda0f271540988ba100558
Jon Zingale <jonzingale@gmail.com>
2018-06-10 18:37:11 -0600

    repositioned for current path

--- Attenuations/AttenuationTests.hs
+++ Attenuations/AttenuationTests.hs
@@ -5,22 +5,12 @@
 
 module AttenuationTests where
 import Tests.ExplicitGenerators
--- import RayTracer.RayLength
+import RayTracer.HelpersTransport3D
 import RayTracer.Transport3D
--- import RayTracer.Lattice
--- import RayTracer.Rhythm
 import Test.Framework
 
 import {-@ HTF_TESTS @-} Tests.TransportTests3D
--- import {-@ HTF_TESTS @-} Tests.TransportTests
--- import {-@ HTF_TESTS @-} Tests.IndexerTests
--- import {-@ HTF_TESTS @-} Tests.SymmetryTests
--- import {-@ HTF_TESTS @-} Tests.XRegionTests
 
 main = htfMain htf_importedTests
 
 transportTests = htfMain htf_Tests_TransportTests3D_thisModulesTests
--- transportTests = htfMain htf_Tests_TransportTests_thisModulesTests
--- symmetryTests = htfMain htf_Tests_SymmetryTests_thisModulesTests
--- indexerTests = htfMain htf_Tests_IndexerTests_thisModulesTests
--- xRegionTests = htfMain htf_Tests_XRegionTests_thisModulesTests
