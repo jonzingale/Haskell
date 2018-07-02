bacba5b24ceb2d231bb24cab5a38a9c99d94da10
Jon Zingale <jonzingale@gmail.com>
2018-07-02 10:44:27 -0600

    transport fixup

--- Attenuations/RayTracer/Transport.hs
+++ Attenuations/RayTracer/Transport.hs
@@ -9,45 +9,22 @@ segment :: Coords -> Coords -> SegmentLength
 segment (x1, y1, z1) (x2, y2, z2) =
   sqrt $ (x2-x1)**2 + (y2-y1)**2 + (z2-z1)**2
 
-{--
-This is going to need very very much work.
-θ, φ cases individually.
---}
-
 transport:: EntryCoords-> EntryAngles -> [(IntCoords, SegmentLength)]
-transport (x, z) (θ, φ)
-  | θ > pi/2 && φ > pi/2 =
-                 f (xcs (x, z) (θ, φ))
-                 (ycs (x, z) (θ, φ))
-                 (zcs (x, z) (θ, φ))
-                 (x, 0, z) -- pt
-                 (floor x, 0, floor z) -- i j k, z offset by φ?
-                 (negate 1, negate 1) -- sig
-  | θ > pi/2 && φ <= pi/2 =
-                 f (xcs (x, z) (θ, φ))
-                 (ycs (x, z) (θ, φ))
-                 (zcs (x, z) (θ, φ))
-                 (x, 0, z) -- pt
-                 (floor x, 0, floor z) -- i j k, z offset by φ?
-                 (negate 1, 1) -- sig
-  | θ <= pi/2 && φ <= pi/2 =
-                 f (xcs (x, z) (θ, φ))
-                 (ycs (x, z) (θ, φ))
-                 (zcs (x, z) (θ, φ))
-                 (x, 0, z)
-                 (floor x, 0, floor z)
-                 (1, 1)
-  | θ <= pi/2 && φ > pi/2 =
-                 f (xcs (x, z) (θ, φ))
-                 (ycs (x, z) (θ, φ))
-                 (zcs (x, z) (θ, φ))
-                 (x, 0, z) -- pt
-                 (floor x, 0, floor z)
-                 (1, negate 1)
+transport (x, z) (θ, φ) =
+    f (xcs (x, z) (θ, φ))
+    (ycs (x, z) (θ, φ))
+    (zcs (x, z) (θ, φ))
+    (x, 0, z) -- pt
+    (floor x, 0, floor z) -- i j k, z offset by φ?
+    (orientation θ φ) -- sig
   where
     (xcs, ycs, zcs) = (xcrossings, ycrossings, zcrossings)
 
-    -- order of the following methods matters.
+    orientation θ φ | θ > pi/2 && φ > pi/2 = (negate 1, negate 1)
+                    | θ > pi/2 && φ <= pi/2 = (negate 1, 1)
+                    | θ <= pi/2 && φ > pi/2 = (1, negate 1)
+                    | θ <= pi/2 && φ <= pi/2 = (1, 1)
+
     -- xcs ycs zcs (p,q,r) (i,j,k) sig
     f ((xh,yh,zh): xcs) ((xv,yv,zv): ycs) ((xd,yd,zd): zcs) pt (i, j, k) (sθ, sφ)
       | yh == minimum [yh, yv, yd] = -- x case
