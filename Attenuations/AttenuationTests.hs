4bd6c2a064775a59f1e0aa3a8e4d7146e75b0a49
Jon Zingale <jonzingale@gmail.com>
2018-07-04 16:22:23 -0600

    towards full lattice tests

--- Attenuations/AttenuationTests.hs
+++ Attenuations/AttenuationTests.hs
@@ -12,6 +12,7 @@ import Test.Framework
 
 import {-@ HTF_TESTS @-} Tests.PureComponentsTests
 import {-@ HTF_TESTS @-} Tests.SpecificRayTests
+import {-@ HTF_TESTS @-} Tests.FullLatticeTests
 import {-@ HTF_TESTS @-} Tests.LatticeTests
 import {-@ HTF_TESTS @-} Tests.SymmetryTests
 import {-@ HTF_TESTS @-} Tests.DiagonalTests
@@ -23,3 +24,5 @@ latticeTests = htfMain htf_Tests_LatticeTests_thisModulesTests
 symmetryTests = htfMain htf_Tests_SymmetryTests_thisModulesTests
 diagonalTests = htfMain htf_Tests_DiagonalTests_thisModulesTests
 specificRayTests = htfMain htf_Tests_SpecificRayTests_thisModulesTests
+
+fullLatticeTests = htfMain htf_Tests_FullLatticeTests_thisModulesTests
