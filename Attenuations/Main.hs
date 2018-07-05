8f1542d16750b6d1bd4ba8546b3e03b16b74ece1
Jon Zingale <jonzingale@gmail.com>
2018-07-04 18:33:57 -0600

    towards speed tests

--- Attenuations/Main.hs
+++ Attenuations/Main.hs
@@ -1,6 +1,8 @@
 module Main where
-import RayTracer.Transport
 import RayTracer.FileToVector
+import RayTracer.Transport
+import RayTracer.Crossings
+import System.Random
 
 {--
 Todo:
@@ -11,6 +13,24 @@ Todo:
 * Is Strictness on FileToVector valuable?
 --}
 
+allOnes = fileToAry "./Tests/data1M"
+
+totalAttenuation (x, z) (θ, φ) ary =
+  let ijkSeg = transport (x, z) (θ, φ) in
+  sum [ seg * qArray 100 ijk ary | (ijk, seg) <- takeWhile stopCond ijkSeg]
+  where
+    stopCond ((x,y,z), s) =
+      x<100 && y<100 && z<100 &&
+      x>=0 && y>=0 && z>=0
+
+rCoords =
+  let [a, b, c, d] = take 4 $ randomRs (3, 100) $ mkStdGen 10 in
+  let xs = randomRs (0, 100::Double) $ mkStdGen a in
+  let zs = randomRs (0, 100::Double) $ mkStdGen b in
+  let θs = randomRs (0, pi::Double)  $ mkStdGen c in
+  let φs = randomRs (0, pi::Double)  $ mkStdGen d in
+  zip (zip xs zs) (zip θs φs)
+
 main = do
-  myArray <- anArray
-  return $ totalAttenuation (pi/4) myArray -- error on pi/2
+  ary <- allOnes
+  return $ sum [totalAttenuation cs as ary | (cs, as) <- take (10^6) rCoords ]
