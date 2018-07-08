fd434ee6f93974163c7e20a67277368df01af800
Jon Zingale <jonzingale@gmail.com>
2018-07-07 23:51:30 -0600

    note

--- Attenuations/Main.hs
+++ Attenuations/Main.hs
@@ -6,6 +6,7 @@ Todo:
 * Simulate 1M rays from gaussian point source
 * pass (filename, seed) to compiled tracer
 * thread and return fileType.
+* self-contained bash script.
 
 To Compile and Run:
 ghc -O2 --make Main.hs -threaded -rtsopts
