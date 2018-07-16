4e84384d2f32be39b387a54bcd3653c2840750c4
Jon Zingale <jonzingale@gmail.com>
2018-07-16 15:28:30 -0600

    back to stable

--- Attenuations/RayTracer/ParallelTracer.hs
+++ Attenuations/RayTracer/ParallelTracer.hs
@@ -12,7 +12,7 @@ Eight Threaded compiled: 1M rays, 100^3 ~ 9 secs
 
 attenuation ary ((x, z), (θ, φ)) =
   let ijkSeg = takeWhile stopCond $ transport (x, z) (θ, φ) in
-  sum [ seg * qArray 100 ijk ary | ((i,j,k), seg) <- ijkSeg]
+  sum [ seg * qArray 100 ijk ary | (ijk, seg) <- ijkSeg]
   where
     stopCond ((x,y,z), s) =
       x<100 && y<100 && z<100 &&
