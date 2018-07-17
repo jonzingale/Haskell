12e3a69f411d0eb4f8681afebb5280e05dd48a9d
Jon Zingale <jonzingale@gmail.com>
2018-07-16 22:55:48 -0600

    these are giant steps

--- Attenuations/RayTracer/GaussianBeam.hs
+++ Attenuations/RayTracer/GaussianBeam.hs
@@ -41,14 +41,16 @@ cheapAngles = do
   cheapφs
 
 cheapθs = do
-  let ins = [(0,1,1),(0,1,0),(0,-1,1),(0,-1,0),(1,0,1),(1,0,0),(-1,0,1),(-1,0,0)]
+  let ins = [(0,1,1),(0,1,0),(0,-1,1),(0,-1,0),(1,0,1),
+             (1,0,0),(-1,0,1),(-1,0,0)]
   let vals = [(x, z, d, theta x z d) | (x,z,d)<-ins]
   putStr "X Z D θ\n"
   putStr.unlines.(map show) $ vals
   putStr "\n"
 
 cheapφs = do
-  let ins = [(0,1,1),(0,1,0),(0,-1,1),(0,-1,0),(1,0,1),(1,0,0),(-1,0,1),(-1,0,0)]
+  let ins = [(0,1,1),(0,1,0),(0,-1,1),(0,-1,0),(1,0,1),
+             (1,0,0),(-1,0,1),(-1,0,0)]
   let vals = [(x, z, d, phi x z d) | (x,z,d)<-ins]
   putStr "X Z D φ\n"
   putStr.unlines.(map show) $ vals
@@ -63,20 +65,31 @@ rDisc = [(r*cos θ, r*sin θ) | (r, θ) <- zip rs θs]
     rs = mkNormals' (50::Double, 0.1) 32
     θs = randomRs (0, 2*pi::Double) $ mkStdGen 32
 
-norm x z = sqrt $ x^2 + z^2
-
+-- Sarah's linearly independent theory
 theta x z d =
-  let x' = if x == 0 then 10**(-13) else x in
-  pi/2 - atan (norm x z / d) * cos (atan (z/x'))
+  let d' = if d == 0 then 10**(-13) else d in
+  atan (x/d')
 
 phi x z d =
-  let x' = if x == 0 then 10**(-10) else x in
-  atan (norm x z / d) * sin (atan (z/x'))
+  let d' = if d == 0 then 10**(-13) else d in
+  pi/2 - atan (z/d')
+
+{-- some kind of dependent theory
+  norm x z = sqrt $ x^2 + z^2
+
+  theta x z d =
+    let x' = if x == 0 then 10**(-13) else x in
+    pi/2 - atan (norm x z / d) * cos (atan (z/x'))
+
+  phi x z d =
+    let x' = if x == 0 then 10**(-10) else x in
+    atan (norm x z / d) * sin (atan (z/x'))
+--}
 
 -- test theta theta'
 test f g = and [cond f g x z d| (x,z,d) <- take 100 randos3]
   where
-    cond f g x z d = abs (f x z d - g x z d) < 10**(-14)
+    cond f g x z d = abs (f x z d - g x z d) < 10**(-10)
 
 randos3 = [ (x, z, d) | (d, (x, z)) <- zip randos10 $ zip randosI randosJ]
   where
