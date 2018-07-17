f72d82b97bc9c94e218a2a10c019329611771d1b
Jon Zingale <jonzingale@gmail.com>
2018-07-17 14:35:00 -0600

    centering for beam

--- Attenuations/RayTracer/GaussianBeam.hs
+++ Attenuations/RayTracer/GaussianBeam.hs
@@ -5,22 +5,13 @@ import System.Random
 
 type Ray = (EntryCoords, EntryAngles)
 type EntryCoords = (XCoord, ZCoord)
+type Center = (XCoord, ZCoord)
 type EntryAngles = (Angle, Angle)
 type Distance = Double
 type XCoord = Double
 type YCoord = Double
 type ZCoord = Double
 type Angle  = Double
-
-{--
-coords: (x, z, θ, φ)
-distance: d
-density: ρ
-radius: r
-
-boundary angle = ∂θ
-density and angle: (ρ, d) -> [(x, z, θ, φ)]
---}
 
 {--
 normally distributed values about (μ, σ).
@@ -33,28 +24,22 @@ mkNormals' (2::Double, 0.001) 32
 
 maximum $ take 10000 $ mkNormals' (50::Double, 2) 32
 --}
-beam :: Distance -> [Ray]
-beam d = map (ray d) rDisc
+
+beam :: Distance -> Center -> [Ray]
+beam d cs = map (ray d) $ rDisc cs
 
 ray :: Distance -> EntryCoords -> Ray
 ray d (x, z) = ((x, z), (aTan (x) d, aTan (z) d))
   where
     aTan t d =
       case d of
-        0 -> atan (t/10**(-13))
-        _ -> atan (t/d)
+        0 -> atan $ t / 10**(-13)
+        _ -> atan $ t / d
 
 -- normally distributed disc: take 10 rdiscCarte
-rDisc = [(r*cos θ + 50, r*sin θ + 50) | (r, θ) <- zip rs θs]
+rDisc (x, z) = [(r*cos θ + x, r*sin θ + z) | (r, θ) <- zip rs θs]
   where
     rs = mkNormals' (50::Double, 2) 32 -- loose beam
     -- rs = mkNormals' (50::Double, 0.1) 32 -- tight beam
     θs = randomRs (0, 2*pi::Double) $ mkStdGen 32
 
-theta x z d =
-  let d' = if d == 0 then 10**(-13) else d in
-  atan (x/d')
-
-phi x z d =
-  let d' = if d == 0 then 10**(-13) else d in
-  pi/2 - atan (z/d')
