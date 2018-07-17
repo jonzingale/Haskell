ba7803a47f80d37fb66327fec91ea1043120e10d
Jon Zingale <jonzingale@gmail.com>
2018-07-16 23:04:02 -0600

    towards a beam

--- Attenuations/RayTracer/GaussianBeam.hs
+++ Attenuations/RayTracer/GaussianBeam.hs
@@ -35,27 +35,6 @@ mkNormals' (2::Double, 0.001) 32
 maximum $ take 10000 $ mkNormals' (50::Double, 12) 32
 99.44566204060355
 --}
-
-cheapAngles = do
-  cheapθs
-  cheapφs
-
-cheapθs = do
-  let ins = [(0,1,1),(0,1,0),(0,-1,1),(0,-1,0),(1,0,1),
-             (1,0,0),(-1,0,1),(-1,0,0)]
-  let vals = [(x, z, d, theta x z d) | (x,z,d)<-ins]
-  putStr "X Z D θ\n"
-  putStr.unlines.(map show) $ vals
-  putStr "\n"
-
-cheapφs = do
-  let ins = [(0,1,1),(0,1,0),(0,-1,1),(0,-1,0),(1,0,1),
-             (1,0,0),(-1,0,1),(-1,0,0)]
-  let vals = [(x, z, d, phi x z d) | (x,z,d)<-ins]
-  putStr "X Z D φ\n"
-  putStr.unlines.(map show) $ vals
-  putStr "\n"
-
 beam :: Perspective -> Distance -> [Perspective]
 beam ((x, z), (θ, φ)) d = [((x, z), (θ, φ))]
 
@@ -65,7 +44,6 @@ rDisc = [(r*cos θ, r*sin θ) | (r, θ) <- zip rs θs]
     rs = mkNormals' (50::Double, 0.1) 32
     θs = randomRs (0, 2*pi::Double) $ mkStdGen 32
 
--- Sarah's linearly independent theory
 theta x z d =
   let d' = if d == 0 then 10**(-13) else d in
   atan (x/d')
@@ -73,26 +51,3 @@ theta x z d =
 phi x z d =
   let d' = if d == 0 then 10**(-13) else d in
   pi/2 - atan (z/d')
-
-{-- some kind of dependent theory
-  norm x z = sqrt $ x^2 + z^2
-
-  theta x z d =
-    let x' = if x == 0 then 10**(-13) else x in
-    pi/2 - atan (norm x z / d) * cos (atan (z/x'))
-
-  phi x z d =
-    let x' = if x == 0 then 10**(-10) else x in
-    atan (norm x z / d) * sin (atan (z/x'))
---}
-
--- test theta theta'
-test f g = and [cond f g x z d| (x,z,d) <- take 100 randos3]
-  where
-    cond f g x z d = abs (f x z d - g x z d) < 10**(-10)
-
-randos3 = [ (x, z, d) | (d, (x, z)) <- zip randos10 $ zip randosI randosJ]
-  where
-    randosJ  = randomRs (0, 1::Double) $ mkStdGen 11
-    randosI  = randomRs (0, 1::Double) $ mkStdGen 16
-    randos10 = randomRs (0,10::Double) $ mkStdGen 23
