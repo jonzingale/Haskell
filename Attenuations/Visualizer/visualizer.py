132cb69d1a5ddc63fbbeeae5212fd9cf6164b799
Jon Zingale <jonzingale@gmail.com>
2018-07-26 11:47:15 -0600

    reads data file and produces image

--- Attenuations/Visualizer/visualizer.py
+++ Attenuations/Visualizer/visualizer.py
@@ -1,22 +1,19 @@
 import numpy as np
-import matplotlib
-import matplotlib.pyplot as plt
-
-from pdb import set_trace as st
 from PIL import Image
 
+from pdb import set_trace as st
+
+# HSV: (360, 255, 255)
+
 # convert file to ary
-ary = np.loadtxt('dataTenThousandOnes', dtype='float')
+ary = np.loadtxt('datastratifiedArray10K', dtype='float')
 
 # load tmp file as HSV
-img = Image.open("tmp.jpeg")
-hsv_img = img.convert("HSV")
-px = hsv_img.load()
+img = Image.open("tmp.jpeg").convert("HSV")
+px = img.load()
 
-for t in range(0,10**4):
-  # value to pixel
-  px[t % 100, t // 100] = (int(ary[t]*140), 1000, 100)
+for t in range(0,10**4): # value to pixel
+  px[t % 100, t // 100] = (int(ary[t]*200), 255, 255) # actual file
+  # px[t % 100, t // 100] = (int(ary[t]*170), 255, 255) # nice colors
 
-hsv_img.show()
-
-# st()
+img.show()
