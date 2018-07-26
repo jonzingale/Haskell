056452d246a13d7f3d0118f40f8049cebeb2d0f9
Jon Zingale <jonzingale@gmail.com>
2018-07-26 13:30:54 -0600

    renderImage method

--- Attenuations/Visualizer/visualizer.py
+++ Attenuations/Visualizer/visualizer.py
@@ -3,18 +3,22 @@ from PIL import Image
 import numpy as np
 
 # HSV: (360, 255, 255)
+Stratified = './../Tests/dataStratified1M'
+randos1M = './../Tests/data1M'
 
-# convert file to ary
-ary = np.loadtxt('./../Tests/stratified1M', dtype='float')
-size = int(np.sqrt(ary.size))
 
-# generate HSV image of corresponding size
-img = Image.new('HSV', (size, size), 0)
-px = img.load()
+def renderImage(filename):
+  ary = np.loadtxt(filename, dtype='float')
+  size = int(np.sqrt(ary.size))
 
-for t in range(0,size**2): # value to pixel
-  px[t % size, t // size] = (170, 255, int(ary[t]*255)) # actual file LIGHT
-  # px[t % 100, t // 100] = (int(ary[t]*200), 255, 255) # actual file HUE
-  # px[t % 100, t // 100] = (int(ary[t]*170), 255, 255) # nice colors
+  # generate HSV image of corresponding size
+  img = Image.new('HSV', (size, size), 0)
+  px = img.load()
 
-img.show()
+  for t in range(0,size**2): # value to pixel
+    # px[t % size, t // size] = (170, 255, int(ary[t]*255)) # actual file LIGHT
+    px[t % size, t // size] = (int(ary[t]*200), 255, 255) # actual file HUE
+
+  img.show()
+
+renderImage(randos1M)
