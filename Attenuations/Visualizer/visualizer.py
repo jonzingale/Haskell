f30a5f928807c5bd37a23cc37fc40c82c435b730
Jon Zingale <jonzingale@gmail.com>
2018-07-26 13:25:57 -0600

    1 million values into image

--- Attenuations/Visualizer/visualizer.py
+++ Attenuations/Visualizer/visualizer.py
@@ -1,19 +1,20 @@
-import numpy as np
-from PIL import Image
-
 from pdb import set_trace as st
+from PIL import Image
+import numpy as np
 
 # HSV: (360, 255, 255)
 
 # convert file to ary
-ary = np.loadtxt('datastratifiedArray10K', dtype='float')
+ary = np.loadtxt('./../Tests/stratified1M', dtype='float')
+size = int(np.sqrt(ary.size))
 
-# load tmp file as HSV
-img = Image.open("tmp.jpeg").convert("HSV")
+# generate HSV image of corresponding size
+img = Image.new('HSV', (size, size), 0)
 px = img.load()
 
-for t in range(0,10**4): # value to pixel
-  px[t % 100, t // 100] = (int(ary[t]*200), 255, 255) # actual file
+for t in range(0,size**2): # value to pixel
+  px[t % size, t // size] = (170, 255, int(ary[t]*255)) # actual file LIGHT
+  # px[t % 100, t // 100] = (int(ary[t]*200), 255, 255) # actual file HUE
   # px[t % 100, t // 100] = (int(ary[t]*170), 255, 255) # nice colors
 
 img.show()
