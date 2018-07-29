4c4976ccda59e21db6bfadc6b53eefe5b122f9df
Jon Zingale <jonzingale@gmail.com>
2018-07-29 11:53:00 -0600

    better stopping cond

--- Attenuations/RayTracer/ParallelTracer.hs
+++ Attenuations/RayTracer/ParallelTracer.hs
@@ -6,7 +6,9 @@ import RayTracer.DataWriter (savePlate)
 import RayTracer.Transport (transport)
 import RayTracer.GaussianBeam (beam)
 
-{--
+-- import qualified Data.Vector.Unboxed as U
+
+{-- Trace times:
 Single Threaded interpreted: 1M rays, 100^3 ~ 15 minutes
 Single Threaded compiled: 1M rays, 100^3 ~ 42 secs
 Eight Threaded compiled: 1M rays, 100^3 ~ 9 secs
@@ -19,11 +21,10 @@ a source 1mm distance to the front face is 4 units from the exit.
 mmToUnits :: Double -> Double
 mmToUnits d  = 2 * d
 
-size = 100
+size = 100 -- lattice side length
 
 attenuation ary ((x, z), (θ, φ)) = -- this could be written better
-  let path = ((0,0,0), 0) : (takeWhile stopCond $ transport (x, z) (θ, φ)) in
-  -- let path = takeWhile stopCond $ transport (x, z) (θ, φ) in
+  let path = takeWhile stopCond $ transport (x, z) (θ, φ) in
   let s = sum [ seg * qArray size ijk ary | (ijk, seg) <- path] in
   let (i,j,k) = fst.last $ path in
   (i, k, s)
@@ -32,18 +33,18 @@ attenuation ary ((x, z), (θ, φ)) = -- this could be written better
       x<size && y<size && z<size &&
       x>=0  && y>=0  && z>=0
 
--- TODO: better filter non-lattice values
 parallelTrace ary = do
-  let gBeams = (beam.mmToUnits) 100 1 -- Distance Deviation
+  let gBeams = (beam.mmToUnits) 100 2 -- Distance Deviation
   let rays = map (attenuation ary) gBeams
   let results = rays `using` parListChunk 64 rdeepseq
   return results -- [(x, z, SegmentLength)]
 
--- import qualified Data.Vector.Unboxed as U
 testTrace = do
   emptyAry <- fileToAry "./Tests/dataEmptyAry"
   ary <- fileToAry "./Tests/dataStratifiedArray3D"
+  -- trace lattice
   plateAry <- parallelTrace ary
+  -- process image file
   let processedPlate = processPlate plateAry emptyAry
   savePlate "tmp" processedPlate
   -- displays dynamic range for new file
