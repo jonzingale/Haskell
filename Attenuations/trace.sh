0f83a1b2c5043fc39d50e9b4b4ea39206525ba8a
Jon Zingale <jonzingale@gmail.com>
2018-07-31 14:46:08 -0600

    better progress messages

--- Attenuations/trace.sh
+++ Attenuations/trace.sh
@@ -8,5 +8,7 @@
 
 ghc -O2 --make Main.hs -threaded -rtsopts
 time ./Main +RTS -N8
+echo 'finished trace'
 rm Main.o Main.hi Main RayTracer/*.o RayTracer/*.hi
+echo 'removed files, visualizing data'
 python ./Visualizer/visualizer.py
