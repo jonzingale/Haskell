5f43a282551efc5b37de409db4d50084c9f8d356
Jon Zingale <jonzingale@gmail.com>
2018-07-30 22:35:17 -0600

    more reliable parsing for Lazy Bytestrings

--- Attenuations/RayTracer/FileToVector.hs
+++ Attenuations/RayTracer/FileToVector.hs
@@ -4,33 +4,37 @@
 
 module RayTracer.FileToVector (qArray, fileToAry, vLength, vSum,
                                uArray2D, qArray2D, displayRange) where
-
-import qualified Data.ByteString.Lex.Fractional as L
-import qualified Data.ByteString.Char8 as L
+import qualified Data.ByteString.Lazy.Char8 as L
 import qualified Data.Vector.Unboxed as U
+-- import qualified Data.ByteString.Lex.Fractional as L
+-- import qualified Data.ByteString.Char8 as L
+-- import qualified Data.Vector.Unboxed as U
+-- import Data.ByteString.Lazy.Char8 hiding (minimum, maximum, putStr)
 import System.Environment
 
 type Dimension = Int -- a single dimension of lattice
 type Coords = (Int, Int, Int)
 type Coords2D = (Int, Int)
 
-{--
-https://mail.haskell.org/pipermail/haskell-cafe/2009-August/064900.html
-Suggested Fix? Data.ByteString.Lazy.Char8
-
---}
-
 fileToAry :: FilePath -> IO (U.Vector Double)
 fileToAry file = do
-  !s <- L.readFile file
-  return.parse $ s
+  s <- L.readFile file
+  return $ U.fromList $ readDouble s
 
-parse :: L.ByteString -> U.Vector Double
-parse = U.unfoldr step
-  where
-     step !s = case L.readExponential s of
-        Nothing       -> Nothing
-        Just (!k, !t) -> Just (k, L.tail t)
+readDouble :: L.ByteString -> [Double]
+readDouble bs = map (read.(L.unpack)) $ L.lines $ bs
+
+-- fileToAry :: FilePath -> IO (U.Vector Double)
+-- fileToAry file = do
+--   !s <- L.readFile file
+--   return.parse $ s
+
+-- parse :: L.ByteString -> U.Vector Double
+-- parse = U.unfoldr step
+--   where
+--      step !s = case L.readExponential s of
+--         Nothing       -> Nothing
+--         Just (!k, !t) -> Just (k, L.tail t)
 
 uArray2D :: U.Unbox a => Dimension -> Coords2D -> a -> U.Vector a -> U.Vector a
 uArray2D size (x, y) v a = (U.//) a [(x + y * size, v)]
@@ -39,8 +43,8 @@ qArray2D :: U.Unbox a => Dimension -> Coords2D -> U.Vector a -> a
 qArray2D size (x, y) a = (U.!) a (x + y * size)
 
 qArray :: U.Unbox a => Dimension -> Coords -> U.Vector a -> a
-qArray size (x, y, z) a = (U.!) a (x + y * size * size + z * size)
--- qArray size (x, y, z) a = (U.!) a (x + z * size * size + y * size)
+-- qArray size (x, y, z) a = (U.!) a (x + y * size * size + z * size)
+qArray size (x, y, z) a = (U.!) a (x + z * size * size + y * size)
 
 vLength :: U.Unbox a => U.Vector a -> Int
 vLength = U.length
