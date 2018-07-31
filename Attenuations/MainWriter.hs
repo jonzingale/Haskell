478fd912899dbc91dad75bd3719340c512d2d72f
Jon Zingale <jonzingale@gmail.com>
2018-07-31 12:44:00 -0600

    more flexible dataWriter

--- Attenuations/MainWriter.hs
+++ Attenuations/MainWriter.hs
@@ -8,6 +8,37 @@ import System.Random
 
 type ULattice = UArray Int Double
 
+{--
+4GB 1/1000 density 10^9 Doubles: real 22m35.839s
+
+Benchmarks:
+size  ary_size   time
+19 MB 100^3      user: 3mins
+302MB 250^3      user: 8mins
+1.5GB 500^3      user: _mins # expect 2GB
+4  GB 1000^3     user: _mins
+
+Exp regression:
+a = 10293934.8
+b = 1.010689625
+y = a*b^500
+--}
+main = do
+    args <- getArgs
+    case args of
+      [n] -> do
+        dataGeneration (read n)
+      [] -> do
+        dataGeneration 100
+      _ -> putStrLn "Wrong number of arguments"
+
+
+-- Produces an empty plate and a stratified data file of given size
+dataGeneration :: Int -> IO()
+dataGeneration n = do
+  saveZeros n
+  saveArr ("StratifiedArray3D_" ++ show n) $ stratifiedArray3D n
+
 -- ghc -O2 MainWriter.hs
 -- time ./MainWriter bigSparsey
 
@@ -16,24 +47,23 @@ type ULattice = UArray Int Double
 randos :: [Double]
 randos = randomRs (0, 1).mkStdGen $ 32
 
-saveArr :: String -> ULattice -> IO ()
+-- saveArr "GradArray" gradArray => "./Tests/dataGradArray"
+saveArr :: String -> ULattice -> IO()
 saveArr file ary =
   writeFile ("./Tests/data" ++ file) $ aryToStr ary
   where aryToStr = unlines.(map show).elems
 
--- 4GB 1/1000 density 10^9 Doubles: real 22m35.839s
-bigSparceArray :: ULattice
-bigSparceArray =
-  let sparse = randomRs (1::Int, 1000) $ mkStdGen 32 in
-  let spRandos = [ if r == 1 then t else 0.0 | (t, r) <- zip randos sparse] in
-  let bounds = (0::Int, 10^9-1) in
-  listArray bounds spRandos
+saveZeros :: Int -> IO()
+saveZeros n =
+  let zeros = take (n^2) $ repeat (0.0::Double) in
+  writeFile ("./Tests/dataEmptyAry_" ++ (show n)) $ aryToStr zeros
+  where aryToStr = unlines.(map show)
 
-main = do
-    args <- getArgs
-    case args of
-      [string] -> do
-        saveArr string bigSparceArray
-      [] -> do
-        saveArr "tmp" bigSparceArray
-      _ -> putStrLn "Wrong number of arguments"
+-- takes a size and returns a cube.
+stratifiedArray3D :: Int -> ULattice
+stratifiedArray3D size =
+  let grades = take size randos in
+  let ary = foldr (++) [] $ map crossSection grades in
+  listArray (1::Int, size^3) ary
+  where
+    crossSection = take (size^2) . repeat
