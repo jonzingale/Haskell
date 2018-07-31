9cd3242fff72440bae8142c8fe99dbdca2b5737c
Jon Zingale <jonzingale@gmail.com>
2018-07-30 22:33:13 -0600

    a place to experiment with better File Writing

--- Attenuations/RayTracer/exp_FileToVector.hs
+++ Attenuations/RayTracer/exp_FileToVector.hs
@@ -1,59 +1,18 @@
-{-# LANGUAGE FlexibleContexts #-}
-import           Control.Monad.Primitive     (PrimMonad, PrimState)
-import qualified Data.ByteString.Lazy        as L
-import qualified Data.Vector.Generic.Mutable as M
-import qualified Data.Vector.Unboxed         as U
-import           Data.Word                   (Word8)
 
-main :: IO ()
-main = do
-    -- Get all of the contents from stdin
-    -- lbs <- L.getContents
-    let file = "./Tests/dataBigSparsey"
-    lbs <- L.readFile file
+-- {-# LANGUAGE FlexibleContexts #-}
+-- {-# LANGUAGE OverloadedStrings #-}
+-- {-# LANGUAGE ScopedTypeVariables #-}
 
-    -- Create a new 256-size mutable vector
-    -- Fill the vector with zeros
-    mutable <- M.replicate 256 0
+import qualified Data.ByteString.Lazy.Char8 as L
+import qualified Data.Vector.Unboxed as U
 
-    -- Add all of the bytes from stdin
-    -- addBytes mutable lbs
+test = "./Tests/dataAllOnes100"
 
-    -- Freeze to get an immutable version
-    vector <- U.unsafeFreeze mutable
+fileToAry :: FilePath -> IO (U.Vector Double)
+fileToAry file = do
+  s <- L.readFile file
+  return $ U.fromList $ readDouble s
 
-    -- Print the frequency of each byte
-    -- In newer vectors: we can use imapM_
-    -- U.zipWithM_ printFreq (U.enumFromTo 0 255) vector
-
-    -- just return the vector
-    print vector
-
-addBytes :: (PrimMonad m, M.MVector v Int)
-         => v (PrimState m) Int
-         -> L.ByteString
-         -> m ()
-addBytes v lbs = mapM_ (addByte v) (L.unpack lbs)
-
-addByte :: (PrimMonad m, M.MVector v Int)
-        => v (PrimState m) Int
-        -> Word8
-        -> m ()
-addByte v w = do
-    -- Read out the old count value
-    oldCount <- M.read v index
-    -- Write back the updated count value
-    M.write v index (oldCount + 1)
-  where
-    -- Indices in vectors are always Ints. Our bytes come in as Word8, so we
-    -- need to convert them.
-    index :: Int
-    index = fromIntegral w
-
-printFreq :: Int -> Int -> IO ()
-printFreq index count = putStrLn $ concat
-    [ "Frequency of byte "
-    , show index
-    , ": "
-    , show count
-    ]
+readDouble :: L.ByteString -> [Double]
+readDouble bs = map (read.(L.unpack)) $ L.lines $ bs
+  
