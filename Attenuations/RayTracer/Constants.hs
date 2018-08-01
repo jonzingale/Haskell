79f60acc8e46db1ffc4de5187646f4fb56eab512
Jon Zingale <jonzingale@gmail.com>
2018-08-01 01:12:08 -0600

    Constants for RayTracing

---
+++ Attenuations/RayTracer/Constants.hs
@@ -0,0 +1,12 @@
+module RayTracer.Constants where
+{--
+center @ GaussianBeam
+size   @ ParallelTracer
+size   @ PhotographicPlate
+range(0, size**3) @ visualizer.py
+--}
+
+size = 250::Int
+
+center :: Double
+center = (fromIntegral size) /  2
