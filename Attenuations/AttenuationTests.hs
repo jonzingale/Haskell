c1e3dd213719037d7d9f16b71f24648687cde3c8
Jon Zingale <jonzingale@gmail.com>
2018-08-02 17:31:54 -0600

    fixiup

--- Attenuations/AttenuationTests.hs
+++ Attenuations/AttenuationTests.hs
@@ -1,6 +1,5 @@
-
 -- http://hackage.haskell.org/package/HTF-0.13.2.4/docs/Test-Framework-Tutorial.html
--- http://www.pstcc.edu/departments/natural_behavioral_sciences/Web%20Physics/TRIGG/Chapter(4).htm
+
 {-# OPTIONS_GHC -F -pgmF htfpp #-}
 
 module AttenuationTests where
@@ -25,6 +24,5 @@ pureComponentsTests = htfMain htf_Tests_PureComponentsTests_thisModulesTests
 specificRayTests = htfMain htf_Tests_SpecificRayTests_thisModulesTests
 symmetryTests = htfMain htf_Tests_SymmetryTests_thisModulesTests
 diagonalTests = htfMain htf_Tests_DiagonalTests_thisModulesTests
-latticeTests = htfMain htf_Tests_LatticeTests_thisModulesTests
-
 gaussianTests = htfMain htf_Tests_GaussianTests_thisModulesTests
+latticeTests = htfMain htf_Tests_LatticeTests_thisModulesTests
