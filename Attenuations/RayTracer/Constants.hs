25d772bf8b21f6099f9eb43cdfcec2af245e69ab
Jon Zingale <jonzingale@gmail.com>
2018-08-02 17:32:15 -0600

    approximated coupons

--- Attenuations/RayTracer/Constants.hs
+++ Attenuations/RayTracer/Constants.hs
@@ -11,7 +11,7 @@ size = 100::Int
 center :: Double
 center = (fromIntegral size) /  2
 
--- Number of rays desired by coupon collection.
+-- Number of rays desired by coupon collection. 14Million @ 1000
 raySize = 1*10**6 -- default
 -- raySize | size < 500 = 1*10**6
 --         | otherwise =
