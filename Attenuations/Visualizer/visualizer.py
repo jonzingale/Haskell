456898a0a72b855f7d8cd65f4e526b0e5fbb8721
Jon Zingale <jonzingale@gmail.com>
2018-08-02 18:00:43 -0600

    fixup

--- Attenuations/Visualizer/visualizer.py
+++ Attenuations/Visualizer/visualizer.py
@@ -3,18 +3,12 @@ from PIL import Image
 import numpy as np
 import datetime
 
-# size = 500
-# size = 700 # file size ~ 123965k
-# size = 500 # file size ~ 4411k
-size = 100 # 1154993
+size = 500
 
 testTrace = './Data/dataSavedPlate'
 window = (750, 750)
 
 mm = size * np.sqrt(3)
-
-def time():
-  return(datetime.datetime.now().strftime('%s'))
 
 def renderPixel(t, ary): # RGB
   val = ary[t]
@@ -25,16 +19,15 @@ def renderPixel(t, ary): # RGB
 
 def renderImage(filename):
   ary = np.loadtxt(filename, dtype='d')
-
-  # generate image of corresponding size
   img = Image.new('RGB',(size, size), 0)
   px = img.load()
 
   for t in range(0, size**2): # value to pixel
     px[t % size, t // size] = renderPixel(t, ary)
 
+  time = datetime.datetime.now().strftime('%s')
   resized = img.resize(window)
-  resized.save('./Visualizer/Images/image_'+time()+'.png')
+  resized.save('./Visualizer/Images/image_' + time + '.png')
   resized.show()
 
 renderImage(testTrace)
