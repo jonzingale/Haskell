02d72dd0437f46dba65aaa4a63bb99deac7f021d
Jon Zingale <jonzingale@gmail.com>
2018-08-05 11:21:41 -0600

    tighter type constraint

--- Attenuations/RayTracer/FileToVector.hs
+++ Attenuations/RayTracer/FileToVector.hs
@@ -4,11 +4,12 @@ module RayTracer.FileToVector (qArray, fileToAry, uArray2D, qArray2D) where
 import qualified Data.ByteString.Lazy.Char8 as L
 import qualified Data.Vector.Unboxed as U
 
+type Lattice = U.Vector Double
 type Dimension = Int -- single side of lattice
 type Coords = (Int, Int, Int)
 type Coords2D = (Int, Int)
 
-fileToAry :: FilePath -> IO (U.Vector Double)
+fileToAry :: FilePath -> IO Lattice
 fileToAry file = do
   s <- L.readFile file
   return $ U.fromList $ readDouble s
@@ -16,11 +17,11 @@ fileToAry file = do
 readDouble :: L.ByteString -> [Double]
 readDouble bs = map (read.(L.unpack)) $ L.lines $ bs
 
-uArray2D :: U.Unbox a => Dimension -> Coords2D -> a -> U.Vector a -> U.Vector a
+uArray2D :: Dimension -> Coords2D -> Double -> Lattice -> Lattice
 uArray2D size (x, z) v a = (U.//) a [(x + z * size, v)]
 
-qArray2D :: U.Unbox a => Dimension -> Coords2D -> U.Vector a -> a
+qArray2D :: Dimension -> Coords2D -> Lattice -> Double
 qArray2D size (x, y) a = (U.!) a (x + y * size)
 
-qArray :: U.Unbox a => Dimension -> Coords -> U.Vector a -> a
+qArray :: Dimension -> Coords -> Lattice -> Double
 qArray size (x, y, z) a = (U.!) a (x + y * size + z * size * size)
