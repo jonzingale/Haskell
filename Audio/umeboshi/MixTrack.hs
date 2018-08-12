a47a76f79efbdfea6e561393aabdcb91445f066a
Jon Zingale <jonzingale@gmail.com>
2018-08-12 13:41:09 -0600

    removed wav

--- Audio/umeboshi/MixTrack.hs
+++ Audio/umeboshi/MixTrack.hs
@@ -33,8 +33,7 @@ buildTrack :: BPM -> Measure -> [Int32] -> VectSamples
 buildTrack bpm (M (Time n m) mstr) samples =
   let empty = mkEmptyZeroVector bpm (M (Time n m) mstr) in
   let subDiv = div (U.length empty) (length mstr) in
-  (U.//) empty (f mstr 0 subDiv samples)
-  -- U.accum (+) empty (f mstr 0 subDiv samples)
+  U.accum (+) empty (f mstr 0 subDiv samples)
   where
     f [] _ _ _ = []
     f (x:xs) i subDiv ss
@@ -51,31 +50,25 @@ testBuild = do
   w4 <- opHiHat
   w5 <- handClap
   let [drum1, drum2, drum3, drum4, drum5] = map unpack [w1, w2, w3, w4, w5]
-  let track1 = buildTrack 120 (M (Time 5 4) ".xx") drum1
-  let track2 = buildTrack 120 (M (Time 5 4) ".xxxx") drum2
-  let track3 = buildTrack 120 (M (Time 5 4) ".") drum3
-  let track4 = buildTrack 120 (M (Time 5 4) "x") drum4
-  let track5 = buildTrack 120 (M (Time 5 4) ".x") drum5
+  let track1 = buildTrack 130 (M (Time 5 4) ".xx") drum1
+  let track2 = buildTrack 130 (M (Time 5 4) "xxxxx") drum2
+  let track3 = buildTrack 130 (M (Time 5 4) ".") drum3
+  let track4 = buildTrack 130 (M (Time 5 4) "x") drum4
+  let track5 = buildTrack 130 (M (Time 5 4) ".x") drum5
   let drums1 = foldr (U.zipWith (+)) track1 [track2, track3, track4, track5]
 
-  let track1 = buildTrack 120 (M (Time 7 4) ".xx") drum1
-  let track2 = buildTrack 120 (M (Time 7 4) ".xxxxxx.xxxxxx") drum2
-  let track3 = buildTrack 120 (M (Time 7 4) ".x.x") drum3
-  let track4 = buildTrack 120 (M (Time 7 4) "x.") drum4
-  let track5 = buildTrack 120 (M (Time 7 4) ".x") drum5
+  let track1 = buildTrack 130 (M (Time 7 4) ".xx") drum1
+  let track2 = buildTrack 130 (M (Time 7 4) "xxxxxxx") drum2
+  let track3 = buildTrack 130 (M (Time 7 4) ".x.x") drum3
+  let track4 = buildTrack 130 (M (Time 7 4) "x.") drum4
+  let track5 = buildTrack 130 (M (Time 7 4) ".x") drum5
   let drums2 = foldr (U.zipWith (+)) track1 [track2, track3, track4, track5]
 
-  let track1 = buildTrack 120 (M (Time 3 4) ".") drum1
-  let track2 = buildTrack 120 (M (Time 3 4) ".x.") drum2
-  let track3 = buildTrack 120 (M (Time 3 4) "x.x") drum3
-  let track4 = buildTrack 120 (M (Time 3 4) "x.") drum4
-  let track5 = buildTrack 120 (M (Time 3 4) ".x") drum5
+  let track1 = buildTrack 130 (M (Time 3 4) ".") drum1
+  let track2 = buildTrack 130 (M (Time 3 4) ".x.") drum2
+  let track3 = buildTrack 130 (M (Time 3 4) "x.x") drum3
+  let track4 = buildTrack 130 (M (Time 3 4) "x.") drum4
+  let track5 = buildTrack 130 (M (Time 3 4) ".x") drum5
   let drums3 = foldr (U.zipWith (+)) track1 [track2, track3, track4, track5]
-  -- let track = foldr (U.zipWith normSum) track1 [track2, track3, track4, track5]
   makeWavFile $ pack $ U.toList $ U.concat [drums1,drums2,drums1,drums3]
 
-normSum :: Int32 -> Int32 -> Int32
-normSum s t =
-  let s' = fromIntegral s / (2**31-1) in
-  let t' = fromIntegral t / (2**31-1) in
-  ceiling((s' + t') * (2**31-1))
--- Audio/umeboshi/Sequencer.hs
+++ Audio/umeboshi/Sequencer.hs
@@ -1,53 +1,71 @@
 module Sequencer where
+import qualified Data.Vector.Unboxed as U
 import Data.Int (Int32)
-import Data.WAVE
 import Samples
 import Wave
 
-{--
-Parser?
-"x..x..x.."
-
-"x........"
-"...x....."
-"......x.."
-then sums
---}
-
 type BPM = Float
-type SamplesPerBeat = Float
 data Signature = Time Int Int -- example: 3 4
 data Measure = M Signature String
-data Track = Trk Sample [Measure]
+type VectSamples = U.Vector Int32
 
 instance Show Measure where
   show (M (Time b q) str) = show b ++ "/" ++ show q ++ ": " ++ str
 
-toMeasure :: Signature -> String -> Measure
-toMeasure sig str = M sig str
+{--
+Todo:
+cymbals hang over vectorized measure (length sample > length subDiv)
+dithering in mkEmptyZeroVector
+display score
+--}
 
-exM :: Measure
-exM = toMeasure (Time 3 4) "x.x.xx."
-
-exMix = trackToWave 12 (Trk snare [M (Time 3 4) "x.."])
-
-trackToWave :: BPM -> Track -> IO()
-trackToWave bpm (Trk wavIO ms) = do
-  wav <- wavIO
-  let mono = unpack wav
-  let emptyTrack = foldr (++) [] $ map (mkEmptyMeasure bpm) ms -- expensive
-  let them = mixA mono emptyTrack
-  makeWavFile $ pack them
-
--- Helpers
-sampsPerBeat :: BPM -> SamplesPerBeat
-sampsPerBeat bpm = 60 * 44100 / bpm
-
-mkEmptyMeasure :: BPM -> Measure -> [Int32]
-mkEmptyMeasure bpm (M (Time b _) _) =
+-- mkEmptyMeasure 120 exM
+mkEmptyZeroVector :: BPM -> Measure -> VectSamples
+mkEmptyZeroVector bpm (M (Time b _) _) =
   let beats = (fromIntegral b)::Float in
-  let sampPerMeasure = bpm * beats * 735 in
-  take (floor sampPerMeasure) $ repeat (0::Int32)
+  let samplesPerMeasure = beats * 60 * 44100 / bpm in
+  U.replicate (ceiling samplesPerMeasure) (0::Int32) -- dither this?
 
-mixA :: [Int32] -> [Int32] -> [Int32]
-mixA as bs = zipWith (+) as bs -- mix mono mono
+buildTrack :: BPM -> Measure -> [Int32] -> VectSamples
+buildTrack bpm (M (Time n m) mstr) samples =
+  let empty = mkEmptyZeroVector bpm (M (Time n m) mstr) in
+  let subDiv = div (U.length empty) (length mstr) in
+  U.accum (+) empty (f mstr 0 subDiv samples)
+  where
+    f [] _ _ _ = []
+    f (x:xs) i subDiv ss
+      | x == 'x' = getPairs ss i subDiv ++ f xs (i+1) subDiv ss
+      | otherwise = f xs (i+1) subDiv ss
+
+    getPairs ss i subDiv = 
+      [(j + i*subDiv, val) | (j, val) <- zip [0..] ss]
+
+testBuild = do
+  w1 <- hiTom
+  w2 <- maracas
+  w3 <- rimshot
+  w4 <- opHiHat
+  w5 <- handClap
+  let [drum1, drum2, drum3, drum4, drum5] = map unpack [w1, w2, w3, w4, w5]
+  let track1 = buildTrack 130 (M (Time 5 4) ".xx") drum1
+  let track2 = buildTrack 130 (M (Time 5 4) "xxxxx") drum2
+  let track3 = buildTrack 130 (M (Time 5 4) ".") drum3
+  let track4 = buildTrack 130 (M (Time 5 4) "x") drum4
+  let track5 = buildTrack 130 (M (Time 5 4) ".x") drum5
+  let drums1 = foldr (U.zipWith (+)) track1 [track2, track3, track4, track5]
+
+  let track1 = buildTrack 130 (M (Time 7 4) ".xx") drum1
+  let track2 = buildTrack 130 (M (Time 7 4) "xxxxxxx") drum2
+  let track3 = buildTrack 130 (M (Time 7 4) ".x.x") drum3
+  let track4 = buildTrack 130 (M (Time 7 4) "x.") drum4
+  let track5 = buildTrack 130 (M (Time 7 4) ".x") drum5
+  let drums2 = foldr (U.zipWith (+)) track1 [track2, track3, track4, track5]
+
+  let track1 = buildTrack 130 (M (Time 3 4) ".") drum1
+  let track2 = buildTrack 130 (M (Time 3 4) ".x.") drum2
+  let track3 = buildTrack 130 (M (Time 3 4) "x.x") drum3
+  let track4 = buildTrack 130 (M (Time 3 4) "x.") drum4
+  let track5 = buildTrack 130 (M (Time 3 4) ".x") drum5
+  let drums3 = foldr (U.zipWith (+)) track1 [track2, track3, track4, track5]
+  makeWavFile $ pack $ U.toList $ U.concat [drums1,drums2,drums1,drums3]
+
