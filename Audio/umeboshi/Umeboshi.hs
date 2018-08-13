cf7db8c09c12e75ce2cd7171c52cf0553b34a41b
Jon Zingale <jonzingale@gmail.com>
2018-08-12 20:10:18 -0600

    fixup

--- Audio/umeboshi/Umeboshi.hs
+++ Audio/umeboshi/Umeboshi.hs
@@ -14,15 +14,18 @@ testBuild = do
    hiTom, kick, kick2, maracas, opHiHat, rimshot, snare, tom] <- roland808
 
   let m1 = [(".xx", hiTom),("xxxxx", maracas),(".", rimshot),
-            ("x", opHiHat),(".x", handClap)]
+            ("x", opHiHat),(".x", snare)]
   let m2 = [(".xx", hiTom),("xxxxxxx", maracas),("...x", snare),
             (".x..", rimshot),("x.", opHiHat),(".x", handClap)]
   let m3 = [(".", hiTom),(".x.", maracas),("x.x", rimshot),
             ("x.", opHiHat),(".x", handClap)]
 
-  let track1 = buildTrack 120 (Time 5 4) m1                     
-  let track2 = buildTrack 120 (Time 7 4) m2
-  let track3 = buildTrack 120 (Time 3 4) m3
+  let track1 = buildTrack 122 (Time 5 4) m1
+  let track2 = buildTrack 122 (Time 7 4) m2
+  let track3 = buildTrack 122 (Time 3 4) m3
 
-  let rhythm = [track2,track2,track1,track3,track1,track3,track3,track3,track3]
+  let rhythm = [track2,track2,track2,
+                track1,track3,track1,track3,
+                track3,track3,track3]
+
   makeWavFile $ pack $ U.toList $ U.concat $ rhythm
