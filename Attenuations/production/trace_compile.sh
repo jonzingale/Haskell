19b93413b9976d38e1435c65745ddabfece7d642
Jon Zingale <jonzingale@gmail.com>
2018-08-17 17:17:33 -0600

    just compiles

--- Attenuations/production/trace_compile.sh
+++ Attenuations/production/trace_compile.sh
@@ -3,20 +3,19 @@
 # . trace.sh <FILENAME> -x <Distance mm> -d <STANDARD DEVIATION> -s <SEED>
 # . trace.sh ./Data/sparseArray3D -x 1000 -d 2 -s 23
 
-file="$1 $2 $3 $4 $5 $6 $7"
+# file="$1 $2 $3 $4 $5 $6 $7"
 
 ghc -O2 MainWriter.hs
-time ./MainWriter
+# time ./MainWriter
 cp MainWriter ./compiled/DataWriter
 rm MainWriter.o MainWriter.hi MainWriter
-echo 'finished writing necessary data'
+# echo 'finished writing necessary data'
 
 echo 'starting tracer compilation'
 ghc -O2 --make Main.hs -threaded -rtsopts -fforce-recomp # -prof -fprof-auto -fprof-cafs
-
-echo "starting trace: $(date)"
+# echo "starting trace: $(date)"
 # time ./Main $file +RTS -N8 # -sstderr -p # 8 virtual cores
-cp Main ./compiled/
+# cp Main ./compiled/
 rm Main.o Main.hi Main RayTracer/*.o RayTracer/*.hi
 
 # echo 'visualizing data'
