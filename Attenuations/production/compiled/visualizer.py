5c81059e3489caba8304371df228db2654557f5b
Jon Zingale <jonzingale@gmail.com>
2018-08-18 00:37:46 -0600

    better luminosity normalization

--- Attenuations/production/compiled/visualizer.py
+++ Attenuations/production/compiled/visualizer.py
@@ -8,7 +8,6 @@ size = 1000
 window = (750, 750)
 testTrace = './Data/savedPlate'
 checkSumMsg = "Test Trace Fails CheckSum: %.9f instead of %.9f"
-mm = size # * np.sqrt(3) # adjust for photo luminosity.
 checkSumVal = 86781328.601920098
 
 def checkSum(ary):
@@ -16,7 +15,7 @@ def checkSum(ary):
   if (len(sys.argv) == 1) and (sumAry != checkSumVal):
     print(checkSumMsg % (sumAry, checkSumVal))
 
-def renderPixel(t, ary): # RGB
+def renderPixel(t, ary, mm): # RGB
   val = ary[t]
   if val <= 0: return((0,0,0))
   else:
@@ -29,12 +28,12 @@ def renderImage(filename):
   px = img.load()
   checkSum(ary)
 
+  mm = np.amax(ary) # normalize photo luminosity.
   for t in range(0, size**2): # value to pixel
-    px[t % size, t // size] = renderPixel(t, ary)
+    px[t % size, t // size] = renderPixel(t, ary, mm)
 
-  time = datetime.datetime.now().strftime('%s')
   resized = img.resize(window)
-
+  time = datetime.datetime.now().strftime('%s')
   resized.save('./Images/image_' + time + '.png')
   resized.show()
 
