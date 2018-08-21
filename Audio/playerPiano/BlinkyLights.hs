06c757438ff862d4a625ca777a8c2acbf83fa6f9
Jon Zingale <jonzingale@gmail.com>
2018-08-20 21:59:41 -0600

    big steps towards

--- Audio/playerPiano/BlinkyLights.hs
+++ Audio/playerPiano/BlinkyLights.hs
@@ -1,22 +1,35 @@
 module BlinkyLights where
 import qualified Data.Vector.Unboxed as U
+import System.Random
 
 type Board = U.Vector Double
 type Index = Int
 
-tenK = 10^2
+tenK = 1000
 halfZeros = take tenK $ repeat 0
 initState = U.fromList $ [1::Double] ++ halfZeros
 
-rule neigh = case neigh of
-  [0,0,0] -> 0
-  [0,0,1] -> 1
-  [0,1,0] -> 0
-  [0,1,1] -> 1
-  [1,0,0] -> 1
-  [1,0,1] -> 1
-  [1,1,0] -> 1
-  [1,1,1] -> 0
+randos :: Board
+randos = U.fromList $ map fromIntegral $ take tenK $
+            randomRs(0,1::Int) $ mkStdGen 23
+
+toBin :: Int -> [Double]
+toBin n = take 8 $ (f n) ++ repeat 0.0
+  where
+    f 0 = []
+    f n = (fromIntegral.mod n) 2 : (f.div n) 2
+
+rule neigh =
+  let [a,b,c,d,e,f,g,h] = toBin 90 in
+  case neigh of
+    [0,0,0] -> a
+    [0,0,1] -> b
+    [0,1,0] -> c
+    [0,1,1] -> d
+    [1,0,0] -> e
+    [1,0,1] -> f
+    [1,1,0] -> g
+    [1,1,1] -> h
 
 neighbors :: Board -> Index -> [Double]
 neighbors b i = map ((U.!) b)[mod (i-1) tenK, i, mod (i+1) tenK]
--- Audio/playerPiano/PlayerPiano.hs
+++ Audio/playerPiano/PlayerPiano.hs
@@ -9,9 +9,13 @@ freqPerSample freq = freq * 2 * pi / 44100
 
 ff :: Board -> Double -> Double
 ff board t =
-  let harms = U.map (\x -> sin t / x) $ U.fromList [1..100] in
+  let harms = U.map (\x -> sin (t * x)) $ U.fromList [1..10] in
   let tots = U.zipWith (*) board harms in
   U.sum tots / U.sum board
+
+gg :: Board -> Double -> Double
+gg board t =
+  (/ 5) $ sin (t/4) + sin (t/2) + sin (t) + sin (t*2) + sin (t/3)
 
 s1 :: Frequency -> DurationSecs -> Volume -> Board -> VectSamples
 s1 freq len volume bb =
@@ -21,12 +25,6 @@ s1 freq len volume bb =
   U.fromList.duration.setVol $ sine
 
 main =
-  let bs = take 20 $ iterate update initState in -- too slow
-  let them = map (s1 600 0.2 (maxBound `div` 2)) bs in
-
+  let bs = take 30 $ iterate update initState in -- too slow
+  let them = map (s1 50 1 (maxBound `div` 2)) bs in
   makeWavFile $ U.concat them
-
--- mix :: VectSamples -> VectSamples -> VectSamples
--- mix a b = U.zipWith (+) (half a) (half b)
---   where half = U.map $ flip div 2
-
