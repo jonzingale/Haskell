c249ffa75020d384efc342ac3cc0d7f99ea207e0
Jon Zingale <jonzingale@gmail.com>
2018-08-22 10:02:45 -0600

    mobius function doubles and half

--- Audio/playerPiano/Mobius.hs
+++ Audio/playerPiano/Mobius.hs
@@ -19,15 +19,18 @@ divisorsFromCanonical = foldl step (singleton 1)
   where
     step st (p,k) = unions (st:[mapMonotonic (*pp) st | pp <- take k (iterate (*p) p) ])
 
-mobius :: Integer -> Integer
+-- modified to be 2 and 1/2 
+-- rather than 1 and -1.
+mobius :: Integer -> Double
 mobius n = f.factorise' $ n
   where
     f ds | ds == [(1,1)] = 1
          | any (> 1) $ map snd ds = 0
-         | even.length $ ds = 1
-         | otherwise = -1
+         | even.length $ ds = 2
+         | otherwise = 1/2
 
 mobiusV :: [[Double]]
-mobiusV = [map (fromIntegral.mobius) $ listDivisors k | k<-[1..]]
+mobiusV = [map mobius $ listDivisors k | k<-[1..]]
 
-paddedMobius = map ((U.fromList).(take 24).(flip (++) $ repeat 0)) mobiusV
+paddedMobius = map f mobiusV
+    where f = (U.fromList).(take 12).(flip (++) $ repeat 0)
