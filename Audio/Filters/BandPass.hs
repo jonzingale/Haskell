2c5a5f6bc32532a793f734fc260ce6083c73d015
Jon Zingale <jonzingale@gmail.com>
2018-08-25 11:44:39 -0600

    working bandPass filter

--- Audio/Filters/BandPass.hs
+++ Audio/Filters/BandPass.hs
@@ -1,12 +1,17 @@
 module Filters.BandPass where
 import qualified Data.Vector.Unboxed as U
 import Data.Int (Int32)
+import System.Random
 import Data.WAVE
 import Wave
 
 {--
 http://www.analog.com/media/en/technical-documentation/dsp-book/dsp_book_Ch16.pdf
 --}
+
+randos :: VectSamples
+randos = (U.fromList).(take 22050) $ rs
+  where rs = randomRs (minBound, maxBound::Int32) $ mkStdGen 23
 
 type SamplesR = U.Vector Double
 type VectSamples = U.Vector Int32
@@ -37,14 +42,21 @@ mixBands ss tt = U.zipWith (+) ss tt -- may need normalized
 -- needs q as well as freq
 bandPass :: CutOffFreq -> VectSamples -> VectSamples -- q about 352.8
 bandPass freq samples = -- Convolve the input signal & filter kernel
+  let (low, hi) = (freq - 100, freq + 100) in
   let xx = (U.map fromIntegral samples)::SamplesR in
   let padx = (U.++) (U.replicate mm (0::Double)) xx in
-  let lp = U.generate (U.length xx) (f padx (hh 500)) in
-  let hp = spectralInv $ U.generate (U.length xx) (f padx (hh 700)) in
+  let lp = U.generate (U.length xx) (f padx (hh low)) in
+  let hp = spectralInv $ U.generate (U.length xx) (f padx (hh hi)) in
   U.map floor $ U.drop mm $ spectralInv.mixBands lp $ hp
   where
     f x h j = sum [(U.!) x (j+mm-i) * (U.!) h i | i<-[0..mm]]
 
+test =
+  let qs = [200, 400, 600, 1000, 2000, 5000, 10000] in
+  let them = [bandPass] <*> qs <*> [randos] in
+  makeWavFile $ U.concat them
+
 testBandPass = do
   w <- getWAVEFile "blow.wav"
-  makeWavFile $ bandPass 32 $ unpack w
+  let them = [bandPass] <*> [200, 400, 600, 1000] <*> [unpack w]
+  makeWavFile $ U.concat them
