fa7c6deaf2b02aaedd47a6be35f1fa966457e1ea
Jon Zingale <jonzingale@gmail.com>
2018-08-27 11:30:43 -0600

    radical cleanpu

--- Audio/Filters/FFTFilters.hs
+++ Audio/Filters/FFTFilters.hs
@@ -5,10 +5,16 @@ import Numeric.FFT (fft, ifft)
 import Data.Int (Int32)
 import Data.Complex
 
+type FilterKernel = U.Vector (Complex Double)
 type VectSamples = U.Vector Int32
-type SamplesC = U.Vector (Complex Double)
 type CutOffFreq = Complex Double
 type Q = Complex Double
+
+{--
+FFT Transform both inport x[n] and impulse response h[n].
+Multiply the two transforms. Invert FFT to derive the
+convolved inputs. Note that Length of lists must be 2^a.
+--}
 
 mm = 2^18::Complex Double -- power of 2
 
@@ -17,41 +23,34 @@ hamming j m  = 0.54 - 0.46*cos(2*pi*j/m)
 sinc f j m | j == m/2 = 2*pi*f/44100
            | otherwise = sin(2*pi*f/44100 * (j-m/2)) / (j-m/2)
 
-{--
-FFT Transform both inport x[n] and impulse response h[n].
-Multiply the two transforms. Invert FFT to derive the
-convolved inputs. Note that Length of lists must be 2^a.
---}
-
 fftHighPass :: CutOffFreq -> VectSamples -> VectSamples
-fftHighPass fc ss =
-  let xx = (U.map fromIntegral ss)::SamplesC
-      h = fft $ U.toList $ specInv (hh fc)
-      x = fft $ U.toList xx
-      cc = ifft $ zipWith (*) h x
-  in U.map floor $ U.fromList $ map realPart cc
+fftHighPass fc ss = convolve ss (specInv.hh $ fc)
 
--- not quite right. spectral inversions on H[n]
 fftBandPass :: Q -> CutOffFreq -> VectSamples -> VectSamples
-fftBandPass q freq ss =
-  let (low, hi) = (freq - q/2, freq + q/2)
-      lp = fftLowPass low ss
-      hp = fftHighPass hi ss
-      bandBlock = U.zipWith (+) lp hp
-  in specInv bandBlock
+fftBandPass q freq vs =
+  let 
+    lp = hh $ freq - q/2
+    hp = specInv.hh $ freq + q/2
+    bp = specInv $ U.zipWith (+) lp hp
+  in convolve vs bp
 
 fftLowPass :: CutOffFreq -> VectSamples -> VectSamples
-fftLowPass fc ss =
-  let xx = (U.map fromIntegral ss)::SamplesC
-      h = fft $ U.toList (hh fc)
-      x = fft $ U.toList xx
-      cc = ifft $ zipWith (*) h x
-  in U.map floor $ U.fromList $ map realPart cc
+fftLowPass fc ss = convolve ss (hh fc)
 
-specInv :: (U.Unbox b, Num b) => U.Vector b -> U.Vector b
-specInv ss = U.map negate ss
+convolve :: VectSamples -> FilterKernel -> VectSamples
+convolve xs hs =
+  let xx = (U.map fromIntegral xs)::FilterKernel
+      [x, h] = map (fft.(U.toList)) [xx, hs] 
+      c = ifft $ zipWith (*) x h
+  in U.fromList $ map (floor.realPart) c
 
-hh :: CutOffFreq -> SamplesC -- kernel
+specInv :: FilterKernel -> FilterKernel
+specInv h =
+  let m = div (floor.realPart $ mm) 2
+      ih = U.map negate h
+  in (U.//) ih [(m, (U.!) ih m + 1)]
+
+hh :: CutOffFreq -> FilterKernel
 hh fc = normalize $ U.generate (floor.realPart $ mm) (g.fromIntegral)
   where
     normalize h = U.map (/ (U.sum h)) h
