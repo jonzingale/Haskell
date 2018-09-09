a798f7bc9af58eb00b978a5e4a51fad6d88f9ca8
Jon Zingale <jonzingale@gmail.com>
2018-09-09 15:04:46 -0600

    towards discreteLogistic synth

---
+++ Audio/dlogisticADSR/DiscreteLogistic.hs
@@ -0,0 +1,37 @@
+module DiscreteLogistic where
+import qualified Data.Vector.Unboxed as U
+import Data.Int (Int32)
+import System.Random
+import Data.WAVE
+import Wave
+
+type Trajectory = [Population]
+type Population = Double -- 0 < x < 1
+type Rate = Double -- 0 < r < 4
+
+{--
+x(n+1) = r * xn * (1-xn)
+--}
+
+main r =
+  let xs = take 2000 $ map (* 5000) $ runLogistic r in -- scale populations
+  let samples = map (s1 0.2 (maxBound `div` 2)) xs in
+  makeWavFile $ U.concat samples
+
+runLogistic :: Rate -> Trajectory
+runLogistic r = iterate (logistic r) (1 / exp 1)
+
+logistic :: Rate -> Population -> Population
+logistic r xn = r * xn * (1-xn)
+
+freqPerSample :: Double -> Double
+freqPerSample freq = freq * 2 * pi / 44100
+
+s1 :: DurationSecs -> Volume -> Frequency -> VectSamples
+s1 len volume freq =
+  let setVol = U.map (round . (* fromIntegral volume)) in
+  let sine = map (\x -> sin x) [0.0, freqPerSample freq..] in
+  let duration = take.round $ len * 44100 in
+  setVol $ U.fromList $ duration sine
+
+dispLogistic r = putStr.unlines.map show $ runLogistic r
