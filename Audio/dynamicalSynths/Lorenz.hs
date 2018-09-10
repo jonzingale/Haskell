b7dc6e990b5fdc1b0c1bc842aefe31e578053a60
Jon Zingale <jonzingale@gmail.com>
2018-09-09 19:42:16 -0600

    stereo

--- Audio/dynamicalSynths/Lorenz.hs
+++ Audio/dynamicalSynths/Lorenz.hs
@@ -7,21 +7,23 @@ import Wave
 type ODE = Coords -> Coords
 type Coords = (Double, Double, Double)
 type Trajectory = [Coords]
+type Duration = Int
 
 {--
 This synthesizer is designed to represent
 Lorenz equation time series as a tone.
 --}
 
-eBall = 0.002 -- both accuracy and center frequency
+eBall = 0.003 -- both accuracy and center frequency
 maxVal = (2^31-1)/3::Double -- averages 3 signals
 
-main = makeWavFile xyzWav
+main = makeStereoWavFile left right
   where
-    xyzWav = U.fromList $ map (round.tot) $ runLorenz (1,1,1)
-    tot (x,y,z) = x * maxVal * 0.04 +
-                  y * maxVal * 0.03 +
-                  z * maxVal * 0.02
+    cs = (1,1,1)
+    left = U.fromList $ map (round.lf) $ runLorenz 120 cs
+    right = U.fromList $ map (round.rf) $ runLorenz 120 cs
+    lf (x,y,z) = x * maxVal * 0.04 + 0.4 * y * maxVal * 0.03
+    rf (x,y,z) = z * maxVal * 0.02 + 0.4 * y * maxVal * 0.03
 
 euler :: ODE -> Coords -> Coords
 euler f (x,y,z) =
@@ -35,8 +37,8 @@ euler f (x,y,z) =
         ddz = dz + r' * eBall
     in ((dx + ddx) /2.0, (dy + ddy) /2.0,  (dz + ddz) /2.0)
 
-runLorenz :: Coords -> Trajectory
-runLorenz cs = take (120*44100) $ -- take 120 gives 2 mins
+runLorenz :: Duration -> Coords -> Trajectory
+runLorenz sec cs = take (sec*44100) $
   iterate (euler lorenz) cs
 
 lorenz :: Coords -> Coords
