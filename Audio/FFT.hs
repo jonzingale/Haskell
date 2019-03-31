2788804a10cb0d886e491a50813beb0f17b073f9
Jon Zingale <jonzingale@gmail.com>
2019-03-31 11:52:58 -0600

    towards using inverse FFT to build blinky lights on energy domain

---
+++ Audio/FFT.hs
@@ -0,0 +1,21 @@
+module FFT where
+import Math.FFT
+
+{--
+dft :: (FFTWReal r, Ix i, Shapable i) =>
+  CArray i (Complex r) -> CArray i (Complex r)
+
+idft :: (FFTWReal r, Ix i, Shapable i) =>
+  CArray i (Complex r) -> CArray i (Complex r)
+--}
+
+{--
+ dct1, dct1N, dct2, dct2N, dct3, dct3N, dct4, dct4N,
+ destroyInput, dft, dftCR, dftCRG, dftCRGU, dftCRN,
+ dftCRO, dftCROG, dftCROGU, dftCRON, dftG, dftGU, dftHR,
+ dftHRN, dftN, dftRC, dftRCG, dftRCN, dftRH, dftRHN, dftRRG,
+ dftRRN, dht, dhtN, dst1, dst1N, dst2, dst2N, dst3,
+ dst3N, dst4, dst4N, estimate, exhaustive, exportWisdomString,
+ idft, idftN, importWisdomString, importWisdomSystem, measure,
+ patient, preserveInput
+--}
