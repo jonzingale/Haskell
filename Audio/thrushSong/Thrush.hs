20321d0d6d601f5a3a0f42805e75bf7c7cc69771
Jon Zingale <jonzingale@gmail.com>
2019-06-11 10:02:10 -0600

    fixup

--- Audio/thrushSong/Thrush.hs
+++ Audio/thrushSong/Thrush.hs
@@ -14,11 +14,21 @@ thrush = "HermitThrush.wav"
 
 viewThrush = viewSamples thrush
 
+maxVal = (2^31-1)/3::Double -- averages 3 signals
+
 -- Mixes three trajectories into a stereo file.
 main = makeStereoWavFile left right
   where
-    cs = (1,1,1)
-    left = U.fromList $ map (round.lf) $ runLorenz 120 cs
+    cs = (1,1,1) -- arbitrary initial coordinates.
+    left  = U.fromList $ map (round.lf) $ runLorenz 120 cs
     right = U.fromList $ map (round.rf) $ runLorenz 120 cs
     lf (x,y,z) = x * maxVal * 0.04 + 0.4 * y * maxVal * 0.03
     rf (x,y,z) = z * maxVal * 0.02 + 0.4 * y * maxVal * 0.03
+
+-- reconstruction X,Y pairs
+seriesPair :: [(Double, Double)]
+seriesPair =
+  let delay = take 100 $ repeat 0 in
+  let xs = map x_val $ runLorenz 1 (1,1,1) in
+  zip xs (delay ++ xs)
+
