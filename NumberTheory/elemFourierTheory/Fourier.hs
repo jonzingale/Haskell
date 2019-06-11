608b30e3e7622cccc259bb8010c677f007f8a445
Jon Zingale <jonzingale@gmail.com>
2019-06-10 23:56:19 -0600

    Cyclic classes

--- NumberTheory/elemFourierTheory/Fourier.hs
+++ NumberTheory/elemFourierTheory/Fourier.hs
@@ -1,10 +1,19 @@
-module Thrush where
+{-# OPTIONS_GHC -Wno-missing-methods #-}
+module Fourier where
 import qualified Data.Vector.Unboxed as U
-import Data.Int (Int32)
-import Data.WAVE
-import Wave
+import Data.Complex
 
-type ODE = Coords -> Coords
-type Coords = (Double, Double, Double)
-type Trajectory = [Coords]
-type Duration = Int
+
+num :: Complex Float
+num = 2 :+ 3
+
+sumNum = num + num
+congru = Z 2 3
+
+data Cyclic x = Z x x deriving (Eq) -- k + mZ
+
+instance (Show x, Integral x) => Show (Cyclic x) where
+  show  (Z k m) = show (k `mod` m) ++ " + " ++ show m ++ "Z" 
+
+instance (Num x, Integral x) => Num (Cyclic x) where
+  (+) (Z k m) (Z l n) = Z (mod (k+l) m) m
