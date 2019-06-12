4046512e7a429dfbf9ff481b4fcc808f32554a2b
Jon Zingale <jonzingale@gmail.com>
2019-06-12 09:37:41 -0600

    towards L2 functions

--- NumberTheory/elemFourierTheory/Fourier.hs
+++ NumberTheory/elemFourierTheory/Fourier.hs
@@ -13,6 +13,8 @@ congru = Z 2 3
 
 data Cyclic x = Z x x deriving (Eq) -- k + mZ
 
+-- todo: write Eq instance
+
 instance (Show x, Integral x) => Show (Cyclic x) where
   show  (Z k m) = show (k `mod` m) ++ " + " ++ show m ++ "Z" 
 
@@ -22,4 +24,24 @@ instance (Num x, Integral x) => Num (Cyclic x) where
 
 {--
 The homomorphism taking cyclic groups to roots of unity.
+It may be good to verify that this is in fact a homomorphism.
+chi(0) = 1, chi(x)*chi(y) = chi(x+y)
 --}
+
+chi :: Integral x => Cyclic x -> Complex Float
+chi (Z x m) =
+  let ratio = fromIntegral x / fromIntegral m in
+  exp $ 2 * pi * ratio * (0 :+ (-1))
+
+{--
+Towards an L2(G) class, the n-dimensional
+vector space of complex valued functions on G
+
+Note: This class seems possible to write exactly
+because G dual is finite.
+--}
+
+del :: Integral x => Cyclic x -> Cyclic x -> Complex Float
+del a x | a == x = 1
+        | otherwise = 0
+
