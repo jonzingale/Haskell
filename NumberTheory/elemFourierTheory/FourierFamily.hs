f3440ed50be04e9fa601761bf491cec30ba7719f
Jon Zingale <jonzingale@gmail.com>
2019-06-15 10:37:04 -0600

    fixups

--- NumberTheory/elemFourierTheory/FourierFamily.hs
+++ NumberTheory/elemFourierTheory/FourierFamily.hs
@@ -17,22 +17,50 @@ sumNum = num + num
 congru :: Cyclic Int
 congru = Z 2 3
 
-data Cyclic x = Z x x deriving (Eq) -- k + mZ
+-- Cyclic
+-- Should Cyclic be a class? ord, data, etc ...
+data Cyclic x = Z x x deriving Eq -- k + mZ
 
 instance (Show x, Integral x) => Show (Cyclic x) where
   show  (Z k m) = show (k `mod` m) ++ " + " ++ show m ++ "Z" 
 
 instance (Num x, Integral x) => Num (Cyclic x) where
   (+) (Z k m) (Z l n) = Z (mod (k+l) m) m
+-----
+
+-- Cyclic 2
+class (Num a) => Abelian a where
+  char :: a -> Complex Double
+  sum :: (a -> Complex Double) -> (a -> Complex Double) -> Complex Double
+  -- char 
+-----
 
 
-class (Show cyclic, Show complex) => Character cyclic complex where
-  eval :: cyclic -> complex
+-- Characters
+class (Num abGroup) => Character abGroup where
+  eval :: abGroup -> Complex Double
 
-instance (Integral x, Show x, RealFloat t, Show t) =>
-  Character (Cyclic x) (Complex t) where
-  eval (Z x n) = 
+instance Integral x => Character (Cyclic x) where
+  eval (Z x n) =
     let ratio = fromIntegral x / fromIntegral n in
     exp $ 2 * pi * ratio * (0 :+ (-1))
+-----
 
-test = eval congru::(Complex Float)
+-- instance Num (Character) where
+  -- (+) x1 x2 = eval x1 * eval x2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
