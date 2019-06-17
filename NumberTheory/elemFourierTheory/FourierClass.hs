0ca08346014184810c612c7df2564ce20c15fff2
Jon Zingale <jonzingale@gmail.com>
2019-06-17 08:39:24 -0600

    fixup

--- NumberTheory/elemFourierTheory/FourierClass.hs
+++ NumberTheory/elemFourierTheory/FourierClass.hs
@@ -6,17 +6,13 @@ import Data.Complex
 
 type C = Complex Float
 num :: Complex Float
-num = 2 :+ 3
+num = 2 :+ 11
 
 sumNum = num + num
-elemG = Zn 2 3
-chi0 = Chi (\a -> eval a)
-chi1 = Chi (\a -> eval $ a + (gen a) )
-
-(<|>) :: Cyclic -> Cyclic -> C
-(<|>) (Chi f) (Zn k m) = f (Zn k m)
-test_inner1 = chi0 <|> elemG
-test_inner2 = chi1 <|> (idG elemG)
+em1 = Zn 2 11
+chi0 = Chi (\a -> eval $ a + Zn 0 (ord a))
+chi1 = Chi (\a -> eval $ a + gen a )
+chi2 = Chi (\a -> eval $ a + gen a + gen a)
 
 data Cyclic = Zn Int Int | Chi ( Cyclic -> C ) -- k + mZ
 
@@ -31,6 +27,8 @@ instance Num Cyclic where
   negate (Chi f) = Chi $ f . negate
 
 class Num a => Abelian a where
+  (<||>) :: a -> a -> C
+  (<|>) :: a -> a -> a -> C
   chars :: a -> [a]
   char :: a -> a -> C
   dual :: a -> a
@@ -48,17 +46,29 @@ class Num a => Abelian a where
 
 instance Abelian Cyclic where
   chars j = map (Chi . (\x a -> eval(x + a))) $ elems j
+  (<||>) (Chi f) (Zn k m) = f (Zn k m) -- verify not conjugate
+  -- (<||>) (Chi f) (Chi h) | f == h = ord domain
+                         -- | otherwise = 0 -- intended othogonality
 
+  (<|>) (Chi f) (Chi h) g = sum [(conjugate.f) gi * h gi | gi <- elems g]
   eval (Zn x m) =
     let ratio = fromIntegral x / fromIntegral m in
-    exp $ 2 * pi * ratio * (0 :+ (-1))
+    exp $ 2 * pi * (0 :+ 1) * ratio
 
   gen (Zn a m) = Zn 1 m
   ord (Zn x m) = m
 
--- returns rootsUnity
-test_chars = zipWith (<|>) (chars elemG) (elems elemG)
 
+tt f h g = [(conjugate (f <||> gi)) * (h <||> gi) | gi <- elems g]
 
+-- test functions
+test_chars = zipWith (<||>) (chars em1) (elems em1)
+test_inner1 = chi0 <|> em1
+test_inner2 = chi1 <|> (idG em1)
+test_unity = foldr (+) ((!!0).chars $ em1)(chars em1) <|> (idG em1)
+test_sum_of_nontrivial_char = sum [ chi1 <||> g | g <- elems em1]
+test_conj_inv = [(conjugate $ x <||> em1) * (x <||> em1) | x <- chars em1]
+
+-- verify_orthogonality_of_chars = ( chi2 <|> chi1) (Zn 3 6)
 
 
