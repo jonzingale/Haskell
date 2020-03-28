bbacdbae3169582f55324e9950a5ff565cc4f024
Jon Zingale <jonzingale@gmail.com>
2020-03-27 19:59:54 -0600

    lifted inverse image to class

--- Logic/FunctorialLogic.hs
+++ Logic/FunctorialLogic.hs
@@ -1,29 +1,33 @@
 module FunctorialLogic where
 {--
-  really forAll and thereExists should be defined for parts of dom(f)
-  as they are rather trivial for points.
-  forAll :: (s -> b) -> [s] -> [b]
-  thereExists :: (s -> b) -> [s] -> [b]
+  Really forAll and exists should be defined for parts of dom(f)
+  ie. P(S) in P(X). Perhaps begin by specifying Poset with connectives
+  or at least a powerset method.
 --}
 
-class Logical s where
-  thereExists :: (s -> b) -> s -> b
-  forAll :: (s -> b) -> s -> [b]
-  thereExists f a = f a
-  forAll f a = undefined
+class Eq s => Logical s where
+  exists :: (s -> b) -> [s] -> [b]
+  exists f as = map f as
 
-  invImg :: Eq b => (s -> b) -> b -> [s]
+  forAll ::Eq b => (s -> b) -> [s] -> [b]
+  forAll f as = [ f x | x <- invImg f (map f as), elem x as ]
+
+  invImg :: Eq b => (s -> b) -> [b] -> [s]
+  invImg f bs = [ a | a <- domain,  b <- bs, f a == b ]
+
   domain :: [s]
 
 instance Logical Char where
-  invImg f b = [a | a <- ['a'..], f a == b]
   domain = ['a'..]
 
 instance Logical Int where
-  invImg f b = [a | a <- domain, f a == b]
   domain = [0..] ++ map negate [0..]
 
 intEx :: [Int]
-intEx = invImg (\ z -> z `mod` 10^5) 2
+intEx = invImg (\ z -> z `mod` 10^5) [2]
 
-charEx = invImg (\ c -> c:"hars") "ahars"
+invCharEx :: [Char]
+invCharEx = invImg (\ c -> c:"hars") ["ahars"]
+
+allCharEx = forAll (\ c -> "s") "abcs"
+existsCharEx = exists (\ c -> c:"hars") "abcs"
