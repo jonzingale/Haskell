1eb2485c24fb20d440870da12781ca1607696586
Jon Zingale <jonzingale@gmail.com>
2020-03-27 20:42:58 -0600

    closer to realistic logical Sets

--- Logic/PosetLogic.hs
+++ Logic/PosetLogic.hs
@@ -1,33 +1,30 @@
 module PosetLogic where
-import qualified Data.Set as DA
+import Prelude hiding (map, filter)
 import Data.Set
 
--- to unwrap powerset of things.
-unwrap x = Prelude.map toList $ toList x
+type PowerSet s = Set (Set s)
 
-class Eq s => Logical s where
-  exists :: Ord b => (s -> b) -> Set s -> Set b
-  exists f = DA.map f
+class (Eq s, Ord s) => Logical s where
+  domain :: PowerSet s
+
+  exists :: Ord b => (Set s -> Set b) -> PowerSet s -> PowerSet b
+  exists f = map f
 
   -- forAll ::Eq b => (s -> b) -> Set s -> Set b
   -- forAll f as = [ DA.map f x | x <- invImg f (DA.map f as), elem x as ]
 
-  invImg :: (Eq b, Ord b) => (s -> b) -> Set b -> Set s
-  invImg f setB = DA.filter (\a -> member (f a) setB) domain
+  invImg :: (Eq b, Ord b) => (Set s -> Set b) -> PowerSet b -> PowerSet s
+  invImg f setB = filter (\a -> member (f a) setB) $ domain
 
-  domain :: Set s
+  incl :: [s] -> PowerSet s
+  incl = powerSet.fromList
 
 instance Logical Char where
-  domain = fromList ['a'..]
+  domain = incl ['a'..]
 
-instance Logical Int where
-  domain = fromList $ [0..] ++ Prelude.map negate [0..]
-
-intEx :: Set Int -- times out!!!
-intEx = invImg (\ z -> z `mod` 10) $ singleton 2
-
-invCharEx :: Set Char
-invCharEx = invImg (\ c -> c:"hars") $ fromList ["a", "bc"]
+-- Examples
+invCharEx :: PowerSet Char -- times out hard
+invCharEx = invImg (\ c -> union c (fromList "hars")) $ incl "ab"
 
 -- allCharEx = forAll (\ c -> "s") "abcs"
 -- existsCharEx = exists (\ c -> c:"hars") "abcs"
