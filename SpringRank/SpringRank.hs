3904bf2c58ec34e3769d17261f144e0112b6bd3a
Jon Zingale <jonzingale@gmail.com>
2020-03-31 13:09:38 -0600

    working algorithm, now to detokenize

--- SpringRank/SpringRank.hs
+++ SpringRank/SpringRank.hs
@@ -1,30 +1,40 @@
 module SpringRank where
 
 import Numeric.LinearAlgebra.Sparse
-import Data.Sparse.SpMatrix (SpMatrix)
-import Data.Sparse.SpVector (SpVector, createv)
+import Data.Sparse.SpMatrix (SpMatrix, sparsifySM, nrows)
+import Data.Sparse.SpVector (SpVector, createv, sparsifySV, onesSV)
 import Data.Sparse.Common (diagonalSM)
 
 import Adjacency (mkAdjacency, nodes, tokenize)
 import CsvParser (kirchoff)
 import NumericalHelper
 
+-- ('1', 1.25)
+-- ('11', 1.0694444444444442)
+-- ('21', 1.0249999999999995)
+-- ('3', 0.62500000000000011)
+-- ('22', 0.22499999999999964)
+-- ('12', 0.18055555555555525)
+-- ('2', 0.0)
+
+shiftRank :: SpVector Double -> SpVector Double
+shiftRank v = let minV = constv (length v) (minimum v) in v - minV
+
 springrank = do
   k <- kirchoff
-  let adj = mkAdjacency k
-  let n = length.nodes.tokenize $ k
-  let one = createv.take n $ repeat 1
+  let adj = mkAdjacency k -- Matrix
+  let n = nrows adj -- Int
+  let one = onesSV n -- Vector
 
-  let k_in = adj #> one
-  let k_out = transpose adj #> one
+  let c = adj + transpose adj -- Matrix
+  let k_out = transpose adj #> one -- Vector
+  let k_in = adj #> one -- Vector
 
-  let c = adj + transpose adj
+  let d1 = diagonalSM $ k_in + k_out -- Matrix
+  let d2 = k_out - k_in -- Vector
 
-  let d1 = diagonalSM $ k_in + k_out
-  let d2 = k_out - k_in
+  let a = sparsifySM $ eye n + d1 - c -- Matrix
+  let b = sparsifySV $ one + d2 -- Vector
 
-  let b = one + d2
-  let aa = eye n + d1 - c
-
-  solved <- (aa <\> b)
-  print $ map snd $ toListSV solved
+  solution <- a <\> b -- Vector
+  print $ shiftRank solution
