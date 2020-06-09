5a26700c7946208fe08df0fb066d01d5b2e44c3a
Jon Zingale <jonzingale@gmail.com>
2020-06-09 12:02:58 -0600

    fixup

--- NumberTheory/Pollard.hs
+++ NumberTheory/Pollard.hs
@@ -2,11 +2,10 @@
 
 module Pollard where
 
-number = 300^300-1
-
-pollard n = f 2 2 1 n
+pollard n = f 2 5 n 1
   where
-    epGcd x y n = gcd n $ abs $ g x - (g.g) y
-    f x y 1 n = f (g x) (g.g $ y) (epGcd x y n) n
-    f x y d n = d
+    f x y n 1 = f (g x) (g y) n (gcd n $ x - g y)
+    f x y n d = d
     g t = t^2 + 1
+
+test = map pollard [1234, 300^300-1, 121]
