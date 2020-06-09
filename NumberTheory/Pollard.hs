78a4bbeac5d430ad59dfde59bd4a8f4cd5750de1
Jon Zingale <jonzingale@gmail.com>
2020-06-09 13:56:31 -0600

    one less argument

--- NumberTheory/Pollard.hs
+++ NumberTheory/Pollard.hs
@@ -2,11 +2,12 @@
 
 module Pollard where
 
-pollard n = f 2 5 n 1
+pollard n = f 1 (g 1) n
   where
-    f x y n 1 = f (g x) (gg y) n (gcd n $ x - y)
-    f x y n d = d
+    f x y n | gcd n (x - y) == 1 = f (g x) (gg y) n
+            | otherwise = gcd n (x - y)
     g t = t^2 + 1
     gg = g.g
 
-test = map pollard [1234, 300^300-1, 121]
+test1 = map pollard [1234, 300^300-1, 121]
+test2 = take 95 $ map pollard [2..]
