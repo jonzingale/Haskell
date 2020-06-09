a62e1fb887775d13a68d7b5f511dfa8aec0ec856
Jon Zingale <jonzingale@gmail.com>
2020-06-09 12:05:26 -0600

    clean u

--- NumberTheory/Pollard.hs
+++ NumberTheory/Pollard.hs
@@ -4,8 +4,9 @@ module Pollard where
 
 pollard n = f 2 5 n 1
   where
-    f x y n 1 = f (g x) (g y) n (gcd n $ x - g y)
+    f x y n 1 = f (g x) (gg y) n (gcd n $ x - y)
     f x y n d = d
     g t = t^2 + 1
+    gg = g.g
 
 test = map pollard [1234, 300^300-1, 121]
