470f86f7e5b6a0cb3b19d19422083b7d34e03c98
Jon Zingale <jonzingale@gmail.com>
2020-06-09 09:40:06 -0600

    sevens trick and problem 50

--- NumberTheory/SevensTrick.hs
+++ NumberTheory/SevensTrick.hs
@@ -3,6 +3,8 @@ module SevensTrick where
 number = 300^300-1
 
 sevensTrick :: Integer -> Bool
-sevensTrick n | n == 0 || n == 7 || n == -7 = True
-              | n > 7 = sevensTrick ((div n 10) - 2 * (mod n 10))
-              | otherwise = False
+sevensTrick n | n > 7 = sevensTrick $ (div n 10) - 2 * (mod n 10)
+              | otherwise = any (== n) [-7, 0, 7]
+
+-- https://twitter.com/UKMathsTrust/status/1269913635087736832
+problem50 = sevensTrick number
