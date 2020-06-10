159d90045ce77077ae3bea6af3bc9bf72d6765cf
Jon Zingale <jonzingale@gmail.com>
2020-06-10 09:20:51 -0600

    pollard in a unguarded form

--- NumberTheory/Pollard.hs
+++ NumberTheory/Pollard.hs
@@ -9,5 +9,12 @@ pollard n = f 1 (g 1) n
     g t = t^2 + 1
     gg = g.g
 
+pollard' n = f 1 (g 1) n 1
+  where
+    f x y n 1 = f (g x) (gg y) n $ gcd n (x - y)
+    f x y n d = d
+    g t = t^2 + 1
+    gg = g.g
+
 test1 = map pollard [1234, 300^300-1, 121]
 test2 = take 95 $ map pollard [2..]
