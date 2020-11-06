526b09a77090ef01581b9cd97068b806066c7e50
Jon Zingale <jonzingale@gmail.com>
2020-11-06 11:39:55 -0700

    deprecated

--- Heyting/heyting.hs
+++ Heyting/heyting.hs
@@ -1,6 +1,7 @@
 module Heyting where
 import Math.NumberTheory.Primes.Factorisation (factorise)
 import Data.Numbers.Primes (primeFactors)
+import qualified Data.Set as S
 
 {--
 Let a∗ = max { b ∈ L : b ∧ a = 0 } be the pseudocomplement of a.
@@ -13,8 +14,18 @@ the existence of an implication is not automatic.
 
 Hmm, how should I represent:
 1. bounded lattices?
-2. lack of arbitrary joins, wedge sums?
+2. lack of arbitrary joins, wedge sums? dla?
 --}
+
+data BoundedInt = B Int | Infinity deriving (Show, Eq)
+
+instance Ord BoundedInt where
+  (<=) (B a) (B b) = a <= b
+  (<=) (B a) Infinity = True
+  (<=) Infinity Infinity = True
+  (>=) = flip (<=)
+
+testImplies = himplies (B 3) (B 5)
 
 class Heyting h where
   hmeet, hjoin, himplies :: h -> h -> h
@@ -22,21 +33,51 @@ class Heyting h where
   top, bottom :: h
 
 -- arbitrary joins here.
-instance Heyting Int where
-  top = undefined
-  bottom = 1
-  hmeet a b = gcd a b
-  hjoin a b = lcm a b
-  hcomplement a = foldr hjoin bottom [ z | z <- [1..10], hmeet a z <= bottom]
+instance Heyting BoundedInt where
+  top = Infinity
+  bottom = B 1
+  hmeet (B a) (B b) = B $ gcd a b
+  hjoin (B a) (B b) = B $ lcm a b
+  hcomplement a =
+    foldr hjoin bottom [ B z | z <- [1..12], hmeet a (B z) <= bottom]
   -- hcomplement a = himplies a bottom -- first fix himplies
-  himplies a b = -- todo: think about the bounds here
-    foldr hjoin bottom [ z | z <- [1..b], hmeet z a <= b]
 
-testImplies = himplies 3 (5::Int)
+  -- todo: think about the better bounds here
+  himplies a bb =
+    let (B b) = bb in 
+    foldr hjoin bottom [ B z | z <- [1..12], hmeet (B z) a <= bb]
+
+data BoundedSet a = BS (S.Set a) | Top deriving (Show)
+
+instance Eq (BoundedSet a) where
+  (==) (BS a) (BS b) = (S.size a) == (S.size b)
+
+-- S.fromList :: Ord a => [a] -> S.Set a
+-- S.insert :: Ord a => a -> S.Set a -> S.Set a, elems
+instance Ord (BoundedSet a) where
+  (<=) (BS a) (BS b) = (S.size a) <= (S.size b)
+  (<=) (BS a) Top = True
+  (<=) Top Top = True
+  (>=) = flip (<=)
 
 {--
-todo:
-* monoidal Heyting with bottom as empty
-* generalize to any poset?
-* causal sets, boolean sub-algebra, ...
+I need a way to index subsets, (BS z) <- [bottom..top]. The deeper issue is
+likely an ontological one. I may need to have a psuedo-lattice in hand first,
+ie., know what the full union looks like and how to derive the subsets.
 --}
+
+instance Ord a => Heyting (BoundedSet a) where
+  top = Top
+  bottom = BS S.empty
+  hjoin (BS a) (BS b) = BS $ S.intersection a b
+  hmeet (BS a) (BS b) = BS $ S.union a b
+  himplies a b = undefined
+  hcomplement a = undefined
+
+{--
+Try with Object in hand? Do I do this monadically?
+for complement and implication, I will need to append some
+context and work from that.
+--}
+data Relations a = R [(a, a)] deriving (Show, Eq) -- a -> b
+
