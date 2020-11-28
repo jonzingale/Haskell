2c838684d0539b9542e4f41797b73cecfef4e049
Jon Zingale <jonzingale@gmail.com>
2020-11-28 14:08:54 -0700

    mostly sketched soundEvents from ChemEvents

--- Audio/codon/Event.hs
+++ Audio/codon/Event.hs
@@ -1,17 +1,8 @@
 module Event where
 import AminoAcidToPitch
 import AminoAcid
+import Peptide
 import Codon
-
-data ChemEvent = Event {
-  codon :: Codon,
-  bases :: String,
-  aminoAcid :: AminoAcid
-} deriving (Show, Eq)
-
-data Duration = Eighth | Quarter | Half | Whole
-data HarmonicDistribution = Harmonics [Double]
-data ThirdBase = Hmm
 
 class SoundEvent a where
   pitch :: a -> Pitch
@@ -29,3 +20,25 @@ instance SoundEvent ChemEvent where
       'c' -> Whole
   harmonics = undefined
   thirdBase = undefined
+
+--
+
+data ChemEvent = Event {
+  codon :: Codon,
+  bases :: String,
+  aminoAcid :: AminoAcid
+} deriving (Show, Eq)
+
+data Duration = Eighth | Quarter | Half | Whole
+data HarmonicDistribution = Harmonics [Double]
+data ThirdBase = Hmm
+
+peptideToEvents :: Peptide -> [ChemEvent]
+peptideToEvents ps = f.triples $ ps
+  where
+    f [] = []
+    f (s:ss) = Event (fst.toCodonAcid $ s) s (snd.toCodonAcid $ s) : f ss
+
+triples :: Peptide -> [String]
+triples (a:b:c:cs) = (a:b:c:[]) : triples cs
+triples [] = []
