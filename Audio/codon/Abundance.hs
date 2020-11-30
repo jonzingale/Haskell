9ca922c1fd0969a076e1bf9995dc40e52fb305a5
Jon Zingale <jonzingale@gmail.com>
2020-11-30 09:47:55 -0700

    corrected sortAcids, sorting peptides by length

--- Audio/codon/Abundance.hs
+++ Audio/codon/Abundance.hs
@@ -1,30 +1,38 @@
 module Abundance where
 import Event (peptideToEvents, aminoAcid)
-import Peptide (peptides)
-import Data.List (findIndex, sortBy)
+import Peptide (Peptide, peptides)
+import Data.List (sortBy, partition)
 import Data.Function (on)
 import AminoAcid
 
+{--
+Abundance is a collection of helper functions for calculating
+abundances for various data: peptide counts, peptide lengths ...
+--}
+
 -- sort acids by abundance: most to least
+sortAcids :: IO [AminoAcid]
 sortAcids = do
   ps <- peptides
   let acids = map aminoAcid $ concat $ map peptideToEvents ps
-  let counts = f acids zeros
-  let sorted = sortBy (flip compare `on` snd) $ zip aas counts
-  return $ fst.unzip $ sorted -- ordered acids but not counts
+  let sorted = sortBy (flip compare `on` snd) $ f acids
+  return $ fst.unzip $ sorted -- ordered acids sans counts
   where
-    f [] acc = acc
-    f (a:as) zs = f as (accumulate a zs)
+    f [] = []
+    f (a:as) =
+      let (s, r) = partition (== a) (a:as) in
+      (a, length s) : f r
 
-aas = [
-  Stop, Phenylalanine, Leucine, Isoleucine, Methionine, Valine, Serine,
-  Proline, Threonine, Alanine, Tyrosine, Histidine, Glutamine, Asparagine,
-  Lysine, Aspartic, Glutamic, Cysteine, Tryptophan, Arginine, Glycine]
+sortPeptides :: IO [([AminoAcid], Int)]
+sortPeptides = do
+    ps <- peptides
+    let pepAcids = map (map aminoAcid) $ map peptideToEvents ps
+    let psWithLens = [(pep, length pep) | pep <- pepAcids]
+    let sorted = sortBy (flip compare `on` snd) psWithLens
+    return $ sorted
 
-zeros = take 21 $ repeat 0
+-- [4406,2596,1274,420,276,223,122,122,62,25,21,19,17,14,12,11,10,4,2]
+peptideCounts = do
+  ps <- sortPeptides
+  return $ map snd ps
 
-accumulate :: AminoAcid -> [Int] -> [Int]
-accumulate aa acc =
-  let Just idx = findIndex (== aa) aas in
-  let sel = (take (idx-1) zeros) ++ [1] ++ zeros in
-  zipWith (+) sel acc
