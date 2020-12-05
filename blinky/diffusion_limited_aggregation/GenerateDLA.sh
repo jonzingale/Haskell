b09babd9153c0eea2e47926b690a9f1656382d3b
Jon Zingale <jonzingale@gmail.com>
2020-12-04 23:54:25 -0700

    parallelizing and benchmarking

--- blinky/diffusion_limited_aggregation/GenerateDLA.sh
+++ blinky/diffusion_limited_aggregation/GenerateDLA.sh
@@ -3,9 +3,18 @@
 # This script compiles, cleans up, and plays music generated from RNA
 # . GenerateDLA.sh
 
+# parallel
 echo 'compiling Main'
-ghc -O2 --make Main.hs
+ghc -O2 --make Main.hs -threaded -rtsopts -fforce-recomp
 echo 'removing files'
 rm *.o *.hi
 echo 'generating diffusion limited aggregate'
-Main
+time ./Main +RTS -N8
+
+# classic
+# echo 'compiling Main'
+# ghc -O2 --make Main.hs
+# echo 'removing files'
+# rm *.o *.hi
+# echo 'generating diffusion limited aggregate'
+# time ./Main
--- blinky/diffusion_limited_aggregation/Main.hs
+++ blinky/diffusion_limited_aggregation/Main.hs
@@ -1,10 +1,38 @@
 module Main where
+import DLAVector (bsize, blinkStates, board, bounds)
+-- import DLA (bsize, blinkStates, board, bounds)
+import Control.Parallel.Strategies (rdeepseq, parListChunk, using)
+-- import Data.Vector.Strategies (using, parVector)
+import qualified Data.Vector as U
+import qualified Codec.Picture.Types as M
+import Codec.Picture -- JuicyPixel
 import Control.Monad.State
+import Control.Monad.ST
 import System.Random
-import DLAVector
 
 -- . GenerateDLA.sh
 
-main = do -- 7000 steps, 4000 particles, 400x400 in 40 secs
-  let dla = runState (blinkStates 7000 board) (mkStdGen 12)
-  print dla
+-- For testing DLAVector
+-- main = do -- 7000 steps, 4000 particles, 400x400 in 40 secs
+--   let dla = runState (blinkStates 7000 board) (mkStdGen 12)
+--   print dla
+
+main :: IO ()
+main = do savePngImage "images/tmp.png" $ ImageRGB8 genImage
+
+genImage :: Image PixelRGB8
+genImage = runST $ do
+  let dla = runState (blinkStates 9000 board) (mkStdGen 42)
+  let points = U.toList.bounds.fst $ dla
+  let pPoints = points `using` (parListChunk 1024 rdeepseq)
+  mimg <- M.newMutableImage bsize bsize
+  dlaToImage pPoints mimg
+  where
+    dlaToImage [] mi = M.unsafeFreezeImage mi
+    dlaToImage ((x,y):ps) mi =
+      do writePixel mi x y (PixelRGB8 128 128 128)
+         dlaToImage ps mi
+
+-- TODO: Animated Gif
+-- writeGifAnimation :: FilePath -> GifDelay -> GifLooping -> [Image PixelRGB8] -> Either String (IO ())
+-- let gif = writeGifAnimation "tmp.gif" 10 LoopingForever [img, lmg]
