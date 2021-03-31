bcaefc3b2d1763f868a140cde9e6f3c6c21ae07e
Jon Zingale <jonzingale@gmail.com>
2021-03-31 15:04:13 -0600

    rewrite sized_tome, fixups

---
+++ compression/VectorizedBwt.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE BangPatterns #-}
+
+module VectorizedBwt where
+import qualified Data.Vector.Unboxed as V
+import Sort (shuffle)
+
+{--
+Note:
+- An attempt to unpack the details of an optimized Burrow-Wheeler transform.
+- So far no more time efficient than UnpackedBwt, but worse on space.
+
+Todo:
+- write sort with specifiable lexigraphical tolerance.
+- chunk initial tome
+--}
+
+-- Note: so far no more time efficient than UnpackedBwt, but worse on space
+test = burrows_wheeler tome
+
+tome :: String -- infinite
+tome = foldr (++) "" $ repeat "banana_bandana"
+
+-- efficiently generate tome of length 1024.
+sized_tome :: String -> V.Vector Char
+sized_tome text = V.cons '|' $ V.generate 1024 (\j -> text !! j)
+
+-- Note: chunked into blocks of size 1024
+burrows_wheeler :: String -> String
+burrows_wheeler str =
+  let st = sized_tome str in
+  sort.shuffle.(take (V.length st)).rotate $ st
+  where
+    rotate vect = vect : rotate (V.snoc (V.tail vect) (V.head vect))
+
+-- TODO: Write such that the output as indices rather than strings.
+sort :: [V.Vector Char] -> String
+sort [] = []
+sort [x] = [V.last x]
+sort (x:xs) = sort (less x xs) ++ [V.last x] ++ sort (more x xs)
+  where
+    less a bs = filter (<= a) bs
+    more a bs = filter (> a) bs
