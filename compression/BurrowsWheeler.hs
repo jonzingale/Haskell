b0e76214fa045dfc8b88d0219fe5f90378669e11
Jon Zingale <jonzingale@gmail.com>
2021-03-30 21:44:13 -0600

    fixups and comments

--- compression/BurrowsWheeler.hs
+++ compression/BurrowsWheeler.hs
@@ -1,34 +1,33 @@
 module BurrowsWheeler (bwt, inv_bwt, julia) where
-import Data.Char
-import Data.List 
+import Data.List (sort)
+import Data.Char (ord)
 
--- given a string without a |, return the burrows-wheeler transform
+-- given a string without a '|', return the burrows-wheeler transform
+-- NOTE: sort is true lexigraphical order, not just the first character!
 
 julia = "julia"
 banana = "^BANANA"
 
 --------------forward method below
-bwt xs = (last.trans.sort.(take (length xs+1)).rotate) ("|"++xs)
+bwt :: String -> String
+bwt xs = last.trans.sort.(take $ length xs+1).rotate $ "|" ++ xs
 
 rotate :: [a] ->[[a]]
-rotate (x:xs) = (x:xs) : rotate (xs++[x])
+rotate (x:xs) = (x:xs) : rotate (xs ++ [x])
 
 --------------inverse method below
-inv_bwt str = map chr $ inv_bwt_int $ map ord str
-
-inv_bwt_int :: [Int] -> [Int]
-inv_bwt_int [] = []
-inv_bwt_int zs = head [ b | b <-block zs [], (head.reverse) b == 124 ]
+inv_bwt :: String -> String
+inv_bwt [] = []
+inv_bwt zs = head [ b | b <- block zs [], (ord.head.reverse) b == 124 ]
 
 block :: Ord a => [a] -> [[a]] -> [[a]]
 block xs [] = block xs [xs]
 block xs xss
   | length xs == length xss = trans xss
-  | otherwise = block xs $ xs : ((trans.sort.trans) xss)
+  | otherwise = block xs $ xs : (trans.sort.trans $ xss)
 
 --- this exists in Data.List but was fun to write anyway
 trans :: [[a]] -> [[a]]
 trans [] = []
 trans ([]:xss) = trans xss -- the curious case?
 trans xs = (map head xs) : trans (map tail xs)
-
