59976c8d6fc9f08a0ea93cfbdd76f87eb95dcfbd
Jon Zingale <jonzingale@gmail.com>
2021-12-13 04:35:22 -0700

    better scaling

--- Audio/thrushSong/AudioTakens.hs
+++ Audio/thrushSong/AudioTakens.hs
@@ -17,35 +17,35 @@ This module constructs phase space manifolds from wave files.
 
 -- Constants & Types
 type File = String
-bsize = 5000 :: Int -- image size
-maxVal = (2^31-1) :: Int
-hsize = div bsize 2
+bsize = 3000 :: Int -- image size
+hsize = fromIntegral bsize / 2.1 :: Float
 delay = 120 -- heuristically found for Lorenz
 density = 20
 
-wav1 = "audio/peptideSymphony.wav" -- codon
+-- wav1 = "audio/peptideSymphony.wav" -- codon
 -- wav1 = "audio/umeboshi1.wav"
 -- wav1 = "audio/whistle.wav"
 -- wav1 = "audio/umeboshi_with_mobius.wav"
 -- wav1 = "audio/rebab.wav"
 -- wav1 = "audio/whale.wav"
 -- wav1 = "audio/HermitThrush.wav"
---
 
-main :: IO ()
-main = do savePngImage "images/tmp.png" $ ImageRGB8 genImage
+thrush = main "audio/HermitThrush.wav"
 
-genImage :: Image PixelRGB8
-genImage = runST $ do
+main :: File -> IO ()
+main file = do savePngImage "images/tmp.png" $ ImageRGB8 (genImage file)
+
+genImage :: File -> Image PixelRGB8
+genImage file = runST $ do
   mimg <- M.newMutableImage bsize bsize
-  let wav = unsafePerformIO.takensFromWave $ wav1
+  let wav = unsafePerformIO.takensFromWave $ file
   toImage wav mimg
 
   where
     toImage [] mi = M.unsafeFreezeImage mi
     toImage ((x,y):ps) mi =
-      do writePixel mi x y (PixelRGB8 128 200 150)
-      -- do writePixel mi x y (PixelRGB8 255 255 255)
+      do writePixel mi x y (PixelRGB8 128 200 150) -- trace green
+      -- do writePixel mi x y (PixelRGB8 255 255 255) -- white
          toImage ps mi
 
 takensFromWave :: File -> IO [(Int, Int)]
@@ -54,11 +54,13 @@ takensFromWave file = do
   let xs = preprocess wav
   let ys = drop delay xs
   let zs = drop delay ys
-
   let as = drop delay zs
   return $ drawLine density $ zip xs ys
-  where
-    preprocess wav = map (rescale.fromIntegral.(!!0)) $ waveSamples wav
-    rescale x = hsize + div x (div maxVal hsize)
-    xcoord (x,_,_) = x
 
+preprocess :: WAVE -> [Int]
+preprocess wav =
+  let samples = map head $ waveSamples wav in
+  let maxV = fromIntegral.maximum.map abs $ samples in -- inefficient
+  map ((rescale maxV).fromIntegral) samples
+  where
+    rescale mv x = floor $ fromIntegral x * hsize/mv + hsize
