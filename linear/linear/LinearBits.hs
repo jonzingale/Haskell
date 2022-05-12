385fe4c2374858b8659e4bd92eab65a33f4866a0
Jon Zingale <jonzingale@gmail.com>
2022-05-12 00:32:05 -0600

    cleanup

--- linear/linear/LinearBits.hs
+++ linear/linear/LinearBits.hs
@@ -7,32 +7,16 @@ import Data.Bits
 {--
 This library treats Bits as if they were Vectors and Matrices.
 solution5 solves the WoW lights problem for 5 lights.
-
-// inv(l4)*l54
-ls = 727834
-  [[1, 0, 1, 1, 0], // 22
-   [0, 0, 1, 1, 0], // 6
-   [1, 1, 0, 0, 0], // 24
-   [1, 1, 0, 1, 0]] // 26
-
-22 <|> 19::Int => 18
-[1, 0, 1, 1, 0] <|> [1, 0, 0, 1, 1] => [1, 0, 0, 1, 0]
-
-map (eval.((<|>) 19)) m => [0,1,1,0] => 6
-
-(fill 4 (19::Int)) <|> 727834 => 592402
-
 --}
 
-v = 19
-m = [26, 24, 6, 22::Int] -- 727834
-bases = [24, 28, 14, 7::Int]
-
--- solves 5 lights problem
 solution5 :: Int -> Int
 solution5 n = 727834 |> (31 !+ n)
 
-solveBases = map solution5 bases == [1, 13, 11, 8]
+solvesBases :: Bool
+solvesBases = map solution5 bases == [1, 13, 11, 8]
+
+bases :: [Int]
+bases = [24, 28, 14, 7]
 
 class (Listable a) => Vector a where
   (<|>) :: a -> a -> a
