3be72e66496f219ba068132306d4bea8d37ce94d
Jon Zingale <jonzingale@gmail.com>
2022-09-27 16:01:55 -0600

    closer to some data

--- Elo/Elo.hs
+++ Elo/Elo.hs
@@ -7,27 +7,27 @@ This module computes Elo and is a helper to FairTournament.hs
 type Elo = Float
 type Rank = Float
 
--- Where for M a b, a < b in initial rank
--- dependent types?
-data Match = M Elo Elo deriving (Show, Eq)
+-- Where for M a b, a > b in initial rank. Dependent types?
+data Pairing = M Elo Elo deriving (Show, Eq)
 
--- win_probability (M 400 1000) => (0.18242553,0.8175745)
-win_probability :: Match -> (Float, Float)
+-- win_probability (M 1000 400) => (0.8175745, 0.18242553)
+win_probability :: Pairing -> (Float, Float)
 win_probability (M a b) =
-  let prob_a = 1 / (1 + exp((b-a)/a)) in
-  (prob_a, 1 - prob_a)
+  let prob_b = 1 / (1 + exp((a-b)/b)) in
+  (1 - prob_b, prob_b)
 
-eloDiff :: Match -> (Rank, Rank)
+-- eloDiff (M 1000 400) => (5.837616, 26.162384)
+eloDiff :: Pairing -> (Rank, Rank)
 eloDiff match =
   let (x, y) = win_probability match in
   let f t = 32 * (1 - t) in
   (f x, f y)
 
-diffA :: Match -> Rank
-diffA = fst.eloDiff
-
-diffB :: Match -> Rank
-diffB = snd.eloDiff  
+-- Pairing -> Expected Player Wins? -> Open? -> Elo
+eloGain :: Pairing -> Bool -> Bool -> Elo
+eloGain mt True True = fst.eloDiff $ mt -- stronger player wins
+eloGain mt False True = snd.eloDiff $ mt -- weaker player wins
+eloGain mt _ False = fst.eloDiff $ (M 1 1) -- handicap case
 
 {--
 Amateur ranks are effectively linear.
@@ -45,28 +45,10 @@ Amateur ranks are effectively linear.
 1000 10 11 kyu
 500  5 16 kyu
 100  1 20 kyu
-
-| rank == kyu = (21 - rank) * 100
-| otherwise = 100 * rank + 2000
 --}
-rankToElo :: String -> Elo
-rankToElo r =
-  case r of
-    "6d" -> 2850
-    "3d" -> 2300
-    "2.5d" -> 2250
-    "2d" -> 2200
-    "3k" -> 1800
-    "3.2k" -> 1780
-    "3.3k" -> 1770
-    "4k" -> 1700
-    "5k" -> 1600
-    "5.8k" -> 1520
-    "6k" -> 1500
-    "6.1k" -> 1490
-    "6.7k" -> 1430
-    "7k" -> 1400
-    "7.6k" -> 1340
-    "9k" -> 1200
-    "20k" -> 100
-    "20.5k" -> 50
+
+rankToElo :: String -> Float
+rankToElo rank
+  | last rank == 'k' = (21 - toRank rank) * 100
+  | otherwise = 100 * (toRank rank) + 2000
+  where toRank r = read.init $ r
--- Elo/SpringRank/GoParser.hs
+++ Elo/SpringRank/GoParser.hs
@@ -8,32 +8,48 @@ import Data.Either.Extra (fromRight)
 import GHC.Generics (Generic)
 import Control.Monad (liftM)
 import Data.Char (ord)
+import Data.List (init)
 import Data.Csv
+import Elo (rankToElo, eloGain, Pairing(M))
 
 {--
 Data here is given as matchings where the first player listed won the bout
 against the second player listed.
 
-[name1, go_ranking1, name2, go_ranking2]
+[name1, go_ranking1, name2, go_ranking2, open?]
 
 The parser should prepare the data so that go_rankings are converted to elo,
 and the gained elo, via the match, is accounted for. Additionally, each
 player is to be given a unique integer id (index for the adjacency matrix).
 
-[id1, name1, go_ranking1, elo1, id2, name2, go_ranking2, elo2, elo_gain]
+[id1, name1, go_ranking1, elo1, id2, name2, go_ranking2, elo2, elo_gain, open?]
 --}
 
-example = getMatches "data/2018_matches.dat"
+example = getMatches "SpringRank/data/2018_matches.dat"
 
 type Tournament = [Match]
 type EitherData = Either String (Vector Match)
 
 data Match = BadRecord | Match {
+  n1 :: !String,
+  r1 :: !String,
+  n2 :: !String,
+  r2 :: !String,
+  o1 :: !String
+} deriving (Generic, Show)
+
+data Table = Table {
+  id1 :: !Int,
   name1 :: !String,
   rank1 :: !String,
+  elo1 :: !Float,
+  id2 :: !Int,
   name2 :: !String,
-  rank2 :: !String
-} deriving (Generic, Show)
+  rank2 :: !String,
+  elo2 :: !Float,
+  eloDiff :: !Float,
+  open :: !Bool
+} deriving (Show)
 
 instance FromRecord Match
 
@@ -44,3 +60,32 @@ records = toList.(fromRight empty).parseCsv
 
 getMatches :: FilePath -> IO(Tournament)
 getMatches file = liftM records $ BL.readFile file
+
+ex2 = genTable "SpringRank/data/2018_matches.dat"
+
+genTable :: FilePath -> IO [Table]
+genTable file = do
+  matches <- getMatches file
+  return $ map toTable matches
+  where
+    toTable (Match n1 r1 n2 r2 open) =
+      (Table
+        (playerId n1) n1 r1 (rankToElo r1)
+        (playerId n2) n2 r2 (rankToElo r2)
+        (diffElo (rankToElo r1) (rankToElo r2) open)
+        (read open)
+      )
+    playerId s = 1
+    diffElo a b o
+      | a < b = eloGain (M b a) False (read o)
+      | otherwise = eloGain (M a b) True (read o)
+
+
+
+
+
+
+
+
+
+
