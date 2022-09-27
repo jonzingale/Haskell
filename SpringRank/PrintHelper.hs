26e07bfc67482da32ee074c62495e3e36bcf0aa3
Jon Zingale <jonzingale@gmail.com>
2022-09-27 13:24:51 -0600

    print helpers

---
+++ SpringRank/PrintHelper.hs
@@ -0,0 +1,15 @@
+module PrintHelper where
+
+{--
+1. round(2)
+2. sort_by_rank
+
+--}
+
+sortRank :: Ord a => [(b, a)] -> [(b, a)]
+sortRank [] = []
+sortRank ((b,a):bas) =
+    sortRank(gte (b,a) bas) ++ [(b,a)] ++ sortRank(lt (b,a) bas)
+    where
+        lt (x,y) zs = filter (\(q,r) -> r < y) zs
+        gte (x,y) zs = filter (\(q,r) -> r >= y) zs
