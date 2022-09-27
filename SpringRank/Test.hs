31885d3d04b338808e03af319eba7387986069cd
Jon Zingale <jonzingale@gmail.com>
2022-09-27 13:24:31 -0600

    more testing

--- SpringRank/Test.hs
+++ SpringRank/Test.hs
@@ -1,9 +1,18 @@
 module Test where
 import SpringRank (springRank)
+import PrintHelper (sortRank)
 
 normalize :: [(Int, Double)] -> [(Int, Double)]
 normalize ls = let k = 1 / (maximum.map snd $ ls) in
   [(l, k * val) | (l, val) <- ls]
+
+nmgo_test = do
+  es1 <- springRank "data/2018_estimated_elo.dat"
+  es2 <- springRank "data/2018_adjusted_elo.dat"
+  print $ sortRank $ normalize es1
+  print ""
+  print $ sortRank $ normalize es2
+
 
 rasmus_test = do
   edge <- springRank "data/edge.dat"
