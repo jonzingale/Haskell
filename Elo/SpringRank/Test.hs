e498a551a2df259b48ba25887f7e6a43a28c2414
Jon Zingale <jonzingale@gmail.com>
2022-10-04 09:44:18 -0600

    analysis of 2018 and 2019 tournaments

--- Elo/SpringRank/Test.hs
+++ Elo/SpringRank/Test.hs
@@ -11,11 +11,32 @@ normalize :: [(a, Double)] -> [(a, Double)]
 normalize ls = let k = 1 / (maximum.map snd $ ls) in
   [(l, k * val) | (l, val) <- ls]
 
-nmgo_test = do
-  es <- springRank "SpringRank/data/2018_matches.dat"
-  ps <- genPlayers "SpringRank/data/2018_matches.dat"
+nmgo_2018_test = do
+  let datum = "SpringRank/data/2018_matches.dat"
+  es <- springRank datum
+  ps <- genPlayers datum
 
   let adjustedElo = zipWith (*) (map elo ps) (map snd es)
   let tournamentRank = zip (map name ps) adjustedElo
 
-  print.sortRank $ tournamentRank
+  return.sortRank.normalize $ tournamentRank
+
+nmgo_2019H_test = do
+  let datum = "SpringRank/data/2019H_matches.dat"
+  es <- springRank datum
+  ps <- genPlayers datum
+
+  let adjustedElo = zipWith (*) (map elo ps) (map snd es)
+  let tournamentRank = zip (map name ps) adjustedElo
+
+  return.sortRank.normalize $ tournamentRank
+
+nmgo_2019O_test = do
+  let datum = "SpringRank/data/2019O_matches.dat"
+  es <- springRank datum
+  ps <- genPlayers datum
+
+  let adjustedElo = zipWith (*) (map elo ps) (map snd es)
+  let tournamentRank = zip (map name ps) adjustedElo
+
+  return.sortRank.normalize $ tournamentRank
