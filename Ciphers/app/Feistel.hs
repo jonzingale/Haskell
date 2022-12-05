80794b16c1aff1a52d408d6030ea2eaba335b37e
Jon Zingale <jonzingale@gmail.com>
2022-12-05 15:32:56 -0700

    toward ignoring warnings

--- Ciphers/app/Feistel.hs
+++ Ciphers/app/Feistel.hs
@@ -1,12 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -Wincomplete-uni-patterns #-}
 
 module Feistel where
-import qualified Data.ByteString as BL
+import qualified Data.ByteString as B
 import Data.ByteString.Base64 (decode)
 import Data.Word
 import Data.Bits
 
 {--
+
 :set +s
 
 Todo:
@@ -31,7 +33,7 @@ feistelRound           Feistel app/Feistel.hs:(58,1)-(59,42)    2.4    0.0
 --}
 
 type Feistel = [(Text, Text)]
-type Text = BL.ByteString
+type Text = B.ByteString
 type Key = Text
 
 -- length 64 keys
@@ -39,7 +41,6 @@ key1, key2, key3 :: Text
 Right key1 = decode "kTSFoLQRrR+hWJlLjAwXqOH5Z3ZLDWray5mBgNK7lLuHdTwab8m/v96ykTSFoLQR"
 Right key2 = decode "5npvqoiq4jgn4hvwgV6HvWEAGRTV5h7vThEjDsrydgrHSrHtDthrsgrykTSFoLQR"
 Right key3 = decode "6GWYJR2jbersEgghjJ2esgbnyuMrJEHSRthyNUjesrtThsftHtyHeWsykTSFoLQR"
-Right key4 = decode "kTSFoLQRrR+hWJlLjAwXqOH5Z3ZLDWray5mBgNK7lLuHdTwab8m/v96ykTSFoLQR"
 
 keys :: [Text]
 keys = [key3, key2, key1]
@@ -56,24 +57,24 @@ swap (a, b) = (b, a)
 
 feistelRound :: Text -> (Text, Text) -> (Text, Text)
 feistelRound k (l, r) = (r, f (f k r) l)
-  where f k' = BL.pack . BL.zipWith xor k'
+  where f k' = B.pack . B.zipWith xor k'
 
 feistelToText :: Feistel -> Text
-feistelToText xs = foldr (fuseBody.fuseHead) BL.empty xs
+feistelToText xs = foldr (fuseBody.fuseHead) B.empty xs
   where
-    fuseHead (p, q) = BL.append p q
-    fuseBody a b = BL.append a b
+    fuseHead (p, q) = B.append p q
+    fuseBody a b = B.append a b
 
 -- break message up into padded ByteStrings of length n (key size)
 chunk :: Int -> Text -> [(Text, Text)]
 chunk bsize txt
-    | BL.length txt < bsize = [halfs . BL.append txt . pad $ txt]
-    | BL.length txt <= bsize = [halfs txt]
-    | otherwise = (halfs $ BL.take bsize txt) : chunk bsize (BL.drop bsize txt)
+    | B.length txt < bsize = [halfs . B.append txt . pad $ txt]
+    | B.length txt <= bsize = [halfs txt]
+    | otherwise = (halfs $ B.take bsize txt) : chunk bsize (B.drop bsize txt)
     where
-      halfs = BL.splitAt (bsize `div` 2)
-      pad t = padByteString $ bsize - BL.length t
+      halfs = B.splitAt (bsize `div` 2)
+      pad t = padByteString $ bsize - B.length t
 
-padByteString :: Int -> BL.ByteString
-padByteString n = BL.pack . take n $ repeat padChar
+padByteString :: Int -> B.ByteString
+padByteString n = B.pack . take n $ repeat padChar
   where padChar = fromIntegral n :: Word8
