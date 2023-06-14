b781ec3d26fcca5c1cc22ebba65a56c3363f25a4
Jon Zingale <jonzingale@gmail.com>
2023-06-13 22:04:04 -0600

    project Euler 27

--- ProjectEuler/27/Solution27.hs
+++ ProjectEuler/27/Solution27.hs
@@ -33,3 +33,22 @@ n^2 + a*n + b, where |a|<1000 and |b|≤1000
   this let this be the new values to beat.
 --}
 
+{--
+n^2 + an + b, where |a| < 1000 and |b| < 1000
+
+Find the product of the coefficients, a and b, for the quadratic expression that
+produces the maximum number of primes for consecutive values of n,
+starting with n = 0.
+--}
+
+
+
+
+
+
+
+
+
+
+
+
