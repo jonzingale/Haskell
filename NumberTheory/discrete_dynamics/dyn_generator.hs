21a6d062b091141c04a6591c99a6426e84ee95e9
Jon Zingale <jonzingale@gmail.com>
2024-09-01 09:12:00 -0600

    first approximation tensor product for adjacency matrices

--- NumberTheory/discrete_dynamics/dyn_generator.hs
+++ NumberTheory/discrete_dynamics/dyn_generator.hs
@@ -1,5 +1,5 @@
 module DynGenerator where
-import Data.Matrix --  identity, permMatrix, fromLists, toLists
+import Data.Matrix --  identity, permMatrix, fromLists, toLists, nrows, ncols
 
 {--
 NOTES:
@@ -9,8 +9,6 @@ Dyn representations as 1,0 valued adjacency matrices whose rows sum to 1.
 (*) <$> [[0,1],[1,0]] <*> [[0,1],[1,0]] => error
 
 cabal install --lib matrix-0.3.6.0
-
-Look at splitting and joining blocks for building tensor?
 --}
 
 {--
@@ -19,11 +17,28 @@ TODO:
 - tensors? none in library!
 --}
 
-ex0 = identity 2
-ex1 = matrix 4 4 $ \(i,j) -> 2*i - j
-ex2 = fromLists [[0,1],[1,0]]
+ex02 = zero 2 2
+exi2 = identity 2
+ex12 = fromLists [[0,1],[1,0]]
 
-tensor :: Num a => [a] -> [a] -> [a]
-tensor =  \x y -> (*) <$> x <*> y
+ex0 = zero 4 4
+exi = identity 4
+ex1 = fromLists [[0,1,1,0],[0,1,1,1],[1,1,1,0],[0,1,1,0]]
 
-val = fromList 4 4 $ tensor (toList ex2) (toList ex0)
+-- sanity check
+tensorExample = (ex02 <|> exi2) <-> (exi2 <|> ex02)
+test = tensor ex12 exi2 == tensorExample
+
+-- Note: square matrices only
+tensor :: Num a => Matrix a -> Matrix a -> Matrix a
+tensor m1 m2 =
+  let n = nrows m1 in
+  let ms = toLists m1 in
+  let mparts = map (map (\i -> scaleMatrix i m2)) ms in
+  process mparts n
+  where
+    -- could probably be written better, another foldr on (<->)?
+    process (p:[]) i =
+      foldr (<|>) (last p) (take (i-1) p)
+    process (p:ps) i =
+      foldr (<|>) (last p) (take (i-1) p) <-> process ps i
